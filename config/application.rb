59d0d5f9ae5a866f49e37940f1be3cccf0b941f2
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2014-12-05 14:55:51 +0000

    Disable loading of ActiveModel
    It's not necessary for this application.

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "active_model/railtie"
+# require "active_model/railtie"
 # require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
