f46a4b6c35349e6b260532a220939b85cd0a87af
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-02-04 12:35:13 +0000

    Add test for url-arbiter validation error.

--- urlarbiter/url_arbiter_test.go
+++ urlarbiter/url_arbiter_test.go
@@ -38,12 +38,27 @@ var _ = Describe("URLArbiter", func() {
 
 		response, err := arbiter.Register("/foo/bar", "foo_publishing")
 		Expect(err).To(Equal(urlarbiter.ConflictPathAlreadyReserved))
-		Expect(response.Errors).ToNot(BeEmpty())
+		Expect(response.Errors).To(HaveLen(1))
+		Expect(response.Errors["path"]).To(Equal([]string{"is already reserved by the 'foo_publisher' app"}))
+	})
+
+	It("responds with an unprocessable entity error on validation errors", func() {
+		testServer := buildTestServer(422, `{
+"path":"/foo/bar",
+"publishing_app":"",
+"errors":{"publishing_app":["can't be blank"]}
+}`)
+		arbiter := urlarbiter.NewURLArbiter(testServer.URL)
+		response, err := arbiter.Register("/foo/bar", "")
+		Expect(err).To(Equal(urlarbiter.UnprocessableEntity))
+		Expect(response.Errors).To(HaveLen(1))
+		Expect(response.Errors["publishing_app"]).To(Equal([]string{"can't be blank"}))
 	})
 })
 
 func buildTestServer(status int, body string) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-type", "application/json")
 		w.WriteHeader(status)
 		fmt.Fprintln(w, body)
 	}))
