e009104271ca18e56a14b52dcf48fcabf16143fd
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2015-03-25 07:57:49 +0000

    Test url-arbiter should set response content-type
    https://trello.com/c/8zCaPXNh

--- integration_tests/draft_content_item_requests_test.go
+++ integration_tests/draft_content_item_requests_test.go
@@ -45,7 +45,7 @@ var _ = Describe("Draft Content Item Requests", func() {
 			trackRequest(URLArbiterRequestLabel),
 			ghttp.VerifyRequest("PUT", "/paths/vat-rates"),
 			ghttp.VerifyJSON(`{"publishing_app": "mainstream_publisher"}`),
-			ghttp.RespondWithPtr(&urlArbiterResponseCode, &urlArbiterResponseBody),
+			ghttp.RespondWithPtr(&urlArbiterResponseCode, &urlArbiterResponseBody, http.Header{"Content-Type": []string{"application/json"}}),
 		))
 
 		testDraftContentStore = ghttp.NewServer()
