5bbb18c935a168fdbac3adad50bb9793d9b470b4
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-07-29 13:04:44 +0100

    Support the draft endpoint.

--- app/controllers/content_items_controller.rb
+++ app/controllers/content_items_controller.rb
@@ -1,28 +1,52 @@
 class ContentItemsController < ApplicationController
-  def live_content_item
+  def put_live_content_item
+    with_url_arbitration do
+      with_502_suppression do
+        draft_content_store.put_content_item(content_item)
+      end
+
+      live_response = live_content_store.put_content_item(content_item)
+
+      render json: content_item, content_type: live_response.headers[:content_type]
+    end
+  end
+
+  def put_draft_content_item
+    with_url_arbitration do
+      draft_response = with_502_suppression do
+        draft_content_store.put_content_item(content_item)
+      end
+
+      if draft_response
+        render json: content_item, content_type: draft_response.headers[:content_type]
+      else
+        render json: content_item
+      end
+    end
+  end
+
+private
+
+  def with_url_arbitration(&block)
     url_arbiter.reserve_path(
       base_path,
       publishing_app: content_item[:publishing_app]
     )
 
-    begin
-      draft_content_store.put_content_item(content_item)
-    rescue GdsApi::HTTPServerError => e
-      unless e.code == 502 && ENV["SUPPRESS_DRAFT_STORE_502_ERROR"]
-        raise e
-      end
-    end
-
-    response = live_content_store.put_content_item(content_item)
-
-    render json: content_item, content_type: response.headers[:content_type]
+    block.call
   rescue GOVUK::Client::Errors::UnprocessableEntity => e
     render json: e.response, status: 422
   rescue GOVUK::Client::Errors::Conflict => e
     render json: e.response, status: 409
   end
 
-private
+  def with_502_suppression(&block)
+    block.call
+  rescue GdsApi::HTTPServerError => e
+    unless e.code == 502 && ENV["SUPPRESS_DRAFT_STORE_502_ERROR"]
+      raise e
+    end
+  end
 
   def url_arbiter
     PublishingAPI.services(:url_arbiter)
--- spec/requests/live_content_item_requests_spec.rb
+++ spec/requests/live_content_item_requests_spec.rb
@@ -86,4 +86,42 @@ RSpec.describe "Content item requests", :type => :request do
       put_content_item(body: content_item_with_access_limiting.to_json)
     end
   end
+
+  describe "PUT /draft-content" do
+    check_url_registration_happens
+    check_url_registration_failures
+    check_200_response
+    check_400_on_invalid_json
+    check_content_type_header
+    check_draft_content_store_502_suppression
+
+    def put_content_item(body: content_item.to_json)
+      put "/draft-content/vat-rates", body
+    end
+
+    it "sends to draft content store after registering the URL" do
+      expect(PublishingAPI.services(:url_arbiter)).to receive(:reserve_path).ordered
+      expect(PublishingAPI.services(:draft_content_store)).to receive(:put_content_item)
+        .with(content_item)
+        .and_return(stub_json_response)
+        .ordered
+
+      put_content_item
+    end
+
+    it "does not send anything to the live content store" do
+      expect(PublishingAPI.services(:live_content_store)).to receive(:put_content_item).never
+      expect(WebMock).not_to have_requested(:any, /draft-content-store.*/)
+
+      put_content_item
+    end
+
+    it "strips access limiting metadata from the document" do
+      expect(PublishingAPI.services(:draft_content_store)).to receive(:put_content_item)
+        .with(content_item)
+        .and_return(stub_json_response)
+
+      put_content_item(body: content_item_with_access_limiting.to_json)
+    end
+  end
 end
