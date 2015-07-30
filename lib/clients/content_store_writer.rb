9286de4ef935a279893c4ae31340c05b9c85f974
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-07-30 10:14:00 +0100

    Inherit ContentStoreWriter from GdsApi::ContentStore.
    This lives here and not in the GDS API adapters
    because no other application should be writing
    to the content store.

--- lib/clients/content_store_writer.rb
+++ lib/clients/content_store_writer.rb
@@ -1,20 +1,29 @@
-require "gds_api/base"
+require "gds_api/content_store"
 require "active_support/core_ext/hash/keys"
 
-class ContentStoreWriter < GdsApi::Base
+# This lives here and not in the GDS API adapters
+# because no other application should be writing
+# to the content store.
+class ContentStoreWriter < GdsApi::ContentStore
   def put_content_item(base_path:, content_item:)
-    put_json!("#{endpoint}/content#{base_path}", content_item)
+    put_json!(content_item_url(base_path), content_item)
   end
 
   def put_publish_intent(base_path:, publish_intent:)
-    put_json!("#{endpoint}/publish-intent#{base_path}", publish_intent)
+    put_json!(publish_intent_url(base_path), publish_intent)
   end
 
   def get_publish_intent(base_path)
-    get_json!("#{endpoint}/publish-intent#{base_path}").to_hash.deep_symbolize_keys
+    get_json!(publish_intent_url(base_path)).to_hash.deep_symbolize_keys
   end
 
   def delete_publish_intent(base_path)
-    delete_json!("#{endpoint}/publish-intent#{base_path}")
+    delete_json!(publish_intent_url(base_path))
+  end
+
+private
+
+  def publish_intent_url(base_path)
+    "#{endpoint}/publish-intent#{base_path}"
   end
 end
