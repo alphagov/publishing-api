1b9e4eacae7302cb0b6fcd28e1a841ffbfc31dca
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-08-12 13:45:41 +0100

    Tidy up route constraints.
    These were initially copied from the content store
    routing, but in some ways are not necessary,
    particularly with some changes between Rails versions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   with_options format: false do |r|
-    r.get '/healthcheck', :to => proc { [200, {}, ['OK']] }
+    r.put "/draft-content/*base_path", to: "content_items#put_draft_content_item"
+    r.put "/content/*base_path", to: "content_items#put_live_content_item"
 
-    r.constraints base_path: %r[/.*] do
-      put "/draft-content/*base_path", to: "content_items#put_draft_content_item"
-      put "/content/*base_path", to: "content_items#put_live_content_item"
-
-      put "/publish-intent/*base_path", to: "publish_intents#create_or_update"
-      get "/publish-intent/*base_path", to: "publish_intents#show"
-      delete "/publish-intent/*base_path", to: "publish_intents#destroy"
-    end
+    r.put "/publish-intent/*base_path", to: "publish_intents#create_or_update"
+    r.get "/publish-intent/*base_path", to: "publish_intents#show"
+    r.delete "/publish-intent/*base_path", to: "publish_intents#destroy"
   end
+
+  get '/healthcheck', :to => proc { [200, {}, ['OK']] }
 end
