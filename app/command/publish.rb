c70c931149e58652668a384916b533d73e3b41e0
David Heath <david.heath@digital.cabinet-office.gov.uk>
2015-10-06 12:16:35 +0100

    Send notification on message queue when publishing
    also refactored to use the ContentItemPresenter

--- app/command/publish.rb
+++ app/command/publish.rb
@@ -1,7 +1,12 @@
 class Command::Publish < Command::BaseCommand
+  attr_reader :live_item
+
   def call
-    live_item = LiveContentItem.create_or_replace(draft_item.attributes.except("access_limited"))
-    Adapters::ContentStore.new(services: services).call(live_item.base_path, live_payload(live_item))
+    @live_item = LiveContentItem.create_or_replace(draft_item.attributes.except("access_limited"))
+
+    Adapters::ContentStore.new(services: services).call(live_item.base_path, live_payload)
+    queue_publisher.send_message(live_payload)
+
     Command::Success.new(content_id: content_id)
   end
 
@@ -18,9 +23,15 @@ private
     @link_set ||= LinkSet.find_by(content_id: content_id)
   end
 
-  def live_payload(live_item)
-    live_item.as_json.tap do |item|
-      item["links"] = link_set.links if link_set.present?
+  def link_set_hash
+    if link_set.present?
+      {"links" => link_set.links}
+    else
+      {}
     end
+  end
+
+  def live_payload
+    Presenters::ContentItemPresenter.new(@live_item).present.merge(link_set_hash)
   end
 end
--- spec/requests/publish_requests_spec.rb
+++ spec/requests/publish_requests_spec.rb
@@ -4,10 +4,13 @@ RSpec.describe "POST /v2/publish", type: :request do
   context "a draft content item exists" do
     let(:draft_content_item) { create(:draft_content_item) }
     let(:draft_content_item_attributes) { draft_content_item.attributes.deep_symbolize_keys }
-    let(:expected_live_content_item_attributes) {
+    let(:expected_live_content_item_derived_representation) {
       draft_content_item_attributes
         .merge(draft_content_item_attributes[:metadata])
-        .except(:metadata)
+        .except(:metadata, :access_limited)
+    }
+    let(:expected_live_content_item_hash) {
+      expected_live_content_item_derived_representation.deep_stringify_keys.merge("links" => link_set.links)
     }
 
     let(:content_id) { draft_content_item.content_id }
@@ -25,15 +28,22 @@ RSpec.describe "POST /v2/publish", type: :request do
       post request_path, body
     end
 
-    creates_a_content_item_representation(LiveContentItem, expected_attributes_proc: ->() { expected_live_content_item_attributes })
+    creates_a_content_item_representation(LiveContentItem, expected_attributes_proc: ->() { expected_live_content_item_derived_representation })
     logs_event("Publish", expected_payload_proc: ->{ payload.merge("content_id" => content_id) })
 
     it "sends item to live content store including links" do
       expect(PublishingAPI.service(:live_content_store)).to receive(:put_content_item)
         .with(
           base_path: draft_content_item.base_path,
-          content_item: hash_including("content_id" => content_id, "links" => link_set.links)
+          content_item: expected_live_content_item_hash
         )
+      do_request
+    end
+
+    it "sends the item combined with the current link set on the message queue" do
+      expect(PublishingAPI.service(:queue_publisher)).to receive(:send_message)
+        .with(expected_live_content_item_hash)
+
       do_request
     end
   end
