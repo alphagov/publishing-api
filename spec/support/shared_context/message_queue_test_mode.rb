96e7f05c30fbf7be769a18dea5753ea1fc143ccf
David Heath <david.heath@digital.cabinet-office.gov.uk>
2015-10-08 15:02:16 +0100

    Avoid polluting test instance variable namespace
    we can use local variables and share them through the closure rather
    than assigning instance variables.
    
    The `@queue` assignment remains as this forms part of the api between
    the tests and the shared context.

--- spec/support/shared_context/message_queue_test_mode.rb
+++ spec/support/shared_context/message_queue_test_mode.rb
@@ -1,19 +1,22 @@
 RSpec.shared_context "using the message queue in test mode" do
+  old_publisher = nil
+  rabbitmq_config = nil
+
   before :all do
-    @config = YAML.load_file(Rails.root.join("config", "rabbitmq.yml"))[Rails.env].symbolize_keys
-    @old_publisher = PublishingAPI.service(:queue_publisher)
-    PublishingAPI.register_service(name: :queue_publisher, client: QueuePublisher.new(@config))
+    rabbitmq_config = YAML.load_file(Rails.root.join("config", "rabbitmq.yml"))[Rails.env].symbolize_keys
+    old_publisher = PublishingAPI.service(:queue_publisher)
+    PublishingAPI.register_service(name: :queue_publisher, client: QueuePublisher.new(rabbitmq_config))
   end
 
   after :all do
-    PublishingAPI.register_service(name: :queue_publisher, client: @old_publisher)
+    PublishingAPI.register_service(name: :queue_publisher, client: old_publisher)
   end
 
   around :each do |example|
-    conn = Bunny.new(@config)
+    conn = Bunny.new(rabbitmq_config)
     conn.start
     read_channel = conn.create_channel
-    ex = read_channel.topic(@config.fetch(:exchange), passive: true)
+    ex = read_channel.topic(rabbitmq_config.fetch(:exchange), passive: true)
     @queue = read_channel.queue("", :exclusive => true)
     @queue.bind(ex, routing_key: '#')
 
