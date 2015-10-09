9774c76918d7209f4b09a3d5a81d1602a041b4ea
Chris Patuzzo <chris@patuzzo.co.uk>
2015-10-09 10:19:41 +0100

    Remove setting instance variables as a side-effect

--- app/commands/v2/publish.rb
+++ app/commands/v2/publish.rb
@@ -1,18 +1,19 @@
 module Commands
   module V2
     class Publish < BaseCommand
-      attr_reader :live_item, :link_set
-
       def call
         validate!
-        @live_item = LiveContentItem.create_or_replace(draft_item.attributes.except("access_limited")) do |live_item|
+
+        live_content_item = LiveContentItem.create_or_replace(draft_item.attributes.except("access_limited")) do |live_item|
           raise CommandError.new(code: 400, message: "This item is already published") if live_item.version == draft_item.version
         end
-        @link_set = LinkSet.find_by(content_id: content_id)
 
-        Adapters::ContentStore.new.call(live_item.base_path, live_payload)
+        link_set = LinkSet.find_by(content_id: content_id)
 
-        send_to_message_queue!
+        item_for_content_store = live_payload(live_content_item, link_set)
+        Adapters::ContentStore.new.call(live_content_item.base_path, item_for_content_store)
+
+        send_to_message_queue!(item_for_content_store)
 
         Success.new(content_id: content_id)
       end
@@ -43,23 +44,28 @@ module Commands
       end
 
       def draft_item
-        DraftContentItem.find_by(content_id: content_id) or raise CommandError.new(code: 404, message: "Item with content_id #{content_id} does not exist")
+        if (draft_content_item = DraftContentItem.find_by(content_id: content_id))
+          draft_content_item
+        else
+          message = "Item with content_id #{content_id} does not exist"
+          raise CommandError.new(code: 404, message: message)
+        end
       end
 
-      def link_set_hash
+      def link_set_hash(link_set)
         if link_set.present?
-          {links: link_set.links.deep_symbolize_keys}
+          {links: link_set.links}
         else
           {}
         end
       end
 
-      def live_payload
-        Presenters::ContentItemPresenter.new(@live_item).present.merge(link_set_hash)
+      def live_payload(live_item, link_set)
+        Presenters::ContentItemPresenter.new(live_item).present.merge(link_set_hash(link_set))
       end
 
-      def send_to_message_queue!
-        message_payload = live_payload.merge(update_type: update_type)
+      def send_to_message_queue!(item_for_content_store)
+        message_payload = item_for_content_store.merge(update_type: update_type)
         PublishingAPI.service(:queue_publisher).send_message(message_payload)
       end
     end
