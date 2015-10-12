92c3a87f79e6d141b0028ab3b75bb3164fcb8b6d
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-10-12 16:12:05 +0100

    Add downstream calls from PUT links endpoint.
    Sends to draft and live content stores as appropriate.
    Does not do URL arbitration.

--- app/commands/v2/put_link_set.rb
+++ app/commands/v2/put_link_set.rb
@@ -9,6 +9,14 @@ module Commands
           link_set.links = merge_links(link_set.links, link_params.fetch(:links))
         end
 
+        if (draft_content_item = DraftContentItem.find_by(content_id: link_params.fetch(:content_id)))
+          Adapters::DraftContentStore.call(draft_content_item.base_path, content_store_payload(draft_content_item))
+        end
+
+        if (live_content_item = LiveContentItem.find_by(content_id: link_params.fetch(:content_id)))
+          Adapters::ContentStore.call(live_content_item.base_path, content_store_payload(live_content_item))
+        end
+
         Success.new(links: link_set.links)
       end
 
@@ -37,6 +45,12 @@ module Commands
         base_links
           .merge(new_links)
           .reject {|_, links| links.empty? }
+      end
+
+      def content_store_payload(content_item)
+        content_item_hash = LinkSetMerger.merge_links_into(content_item)
+        content_item_hash = content_item_hash.merge(update_type: "links")
+        Presenters::ContentItemPresenter.present(content_item_hash)
       end
     end
   end
