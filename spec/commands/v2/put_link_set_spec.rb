b5f3ebf71aa0a02594ce8182d0d6a470d033145a
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-10-12 10:59:50 +0100

    Extract common setup for PutLinkSet spec.

--- spec/commands/v2/put_link_set_spec.rb
+++ spec/commands/v2/put_link_set_spec.rb
@@ -1,6 +1,26 @@
 require "rails_helper"
 
 RSpec.describe Commands::V2::PutLinkSet do
+  let(:content_id) { "a5f715f9-a0b3-4186-823d-d31f6af4b060" }
+  let(:link_params) {
+    {
+      content_id: content_id,
+      links: {
+        organisations: ["some-new-uuid"],
+      }
+    }
+  }
+
+  before do
+    @existing_link_set = FactoryGirl.create(:link_set,
+      content_id: content_id,
+      links: {
+        organisations: ["some-original-uuid"],
+        topics: ["some-topic-uuid"],
+      }
+    )
+  end
+
   it "validates the link params" do
     link_params_with_missing_links = {}
 
@@ -10,21 +30,9 @@ RSpec.describe Commands::V2::PutLinkSet do
   end
 
   it "updates the LinkSet on disk" do
-    link_set = FactoryGirl.create(:link_set, links: {
-      organisations: ["some-original-uuid"],
-      topics: ["some-topic-uuid"],
-    })
-
-    link_params = {
-      content_id: link_set.content_id,
-      links: {
-        organisations: ["some-new-uuid"],
-      }
-    }
-
     described_class.call(link_params)
 
-    expect(LinkSet.last.links).to eq(
+    expect(@existing_link_set.reload.links).to eq(
       organisations: ["some-new-uuid"],
       topics: ["some-topic-uuid"],
     )
