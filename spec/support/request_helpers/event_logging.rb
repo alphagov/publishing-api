6a0a6fa9fe4fedb384ad06749f0afa3a9d98eb6e
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-10-12 10:58:42 +0100

    Fail early on event logging tests if request fails.

--- spec/support/request_helpers/event_logging.rb
+++ spec/support/request_helpers/event_logging.rb
@@ -4,6 +4,8 @@ module RequestHelpers
       it "logs a '#{event_class_name}' event in the event log" do
         do_request
 
+        expect(response.status).to eq(200)
+
         expect(Event.count).to eq(1)
         expect(Event.first.action).to eq(event_class_name)
         expect(Event.first.user_uid).to eq(nil)
