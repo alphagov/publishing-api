0393165f64a73283ce620f8a1589a028571a32e3
Chris Patuzzo <chris@patuzzo.co.uk>
2015-10-14 11:44:54 +0100

    Extract ImmutableBasePath shared helper...
    We have also added coverage for checking that you
    can override the immutability by setting a flag.
    
    This is required in order to maintain backwards
    compatibility with v1 of the API.

--- spec/models/live_content_item_spec.rb
+++ spec/models/live_content_item_spec.rb
@@ -7,16 +7,6 @@ RSpec.describe LiveContentItem do
     expect(described_class.first.title).to eq("New title")
   end
 
-  describe 'validations' do
-    before do
-      create(:live_content_item, content_id: '123', base_path: '/foo')
-    end
-    it 'validates base_path' do
-      live = build(:live_content_item, content_id: '123', base_path: '/bar')
-      expect(live).not_to be_valid
-    end
-  end
-
   let(:new_attributes) {
     {
       content_id: content_id,
@@ -26,4 +16,5 @@ RSpec.describe LiveContentItem do
 
   it_behaves_like Replaceable
   it_behaves_like DefaultAttributes
+  it_behaves_like ImmutableBasePath
 end
