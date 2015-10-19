db377a3706b43f8e5f7542040a65a09e8d517c6e
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-10-19 11:21:00 +0100

    Add sidekiq configs.

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,17 @@
+redis_config = {
+  host: ENV["REDIS_HOST"] || "127.0.0.1",
+  port: ENV["REDIS_PORT"] || 6379,
+  namespace: "publishing_api",
+}
+
+Sidekiq.configure_server do |config|
+  config.redis = redis_config
+  config.error_handlers << Proc.new {|ex, context_hash| Airbrake.notify(ex, context_hash) }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = redis_config
+end
+
+require 'sidekiq/logging/json'
+Sidekiq.logger.formatter = Sidekiq::Logging::Json::Logger.new
