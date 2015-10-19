5b4530651683196171c4073bc067e54bfbabdf9f
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-10-19 11:49:25 +0100

    Add background downstream ops to linkset command.

--- app/commands/v2/put_link_set.rb
+++ app/commands/v2/put_link_set.rb
@@ -13,11 +13,20 @@ module Commands
         end
 
         if (draft_content_item = DraftContentItem.find_by(content_id: link_params.fetch(:content_id)))
-          Adapters::DraftContentStore.call(draft_content_item.base_path, draft_content_store_payload(draft_content_item))
+          ContentStoreWorker.perform_async(
+            content_store: Adapters::DraftContentStore,
+            base_path: draft_content_item.base_path,
+            payload: draft_content_store_payload(draft_content_item),
+          )
         end
 
         if (live_content_item = LiveContentItem.find_by(content_id: link_params.fetch(:content_id)))
-          Adapters::ContentStore.call(live_content_item.base_path, live_content_store_payload(live_content_item))
+          ContentStoreWorker.perform_async(
+            content_store: Adapters::ContentStore,
+            base_path: live_content_item.base_path,
+            payload: live_content_store_payload(live_content_item),
+          )
+
           PublishingAPI.service(:queue_publisher).send_message(message_bus_payload(live_content_item))
         end
 
