740f3fb487f30e50532a52303022920bfcc5d278
Chris Patuzzo <chris@patuzzo.co.uk>
2015-10-26 15:15:38 +0000

    Validate DraftContentItem#rendering_app

--- app/models/draft_content_item.rb
+++ app/models/draft_content_item.rb
@@ -22,6 +22,7 @@ class DraftContentItem < ActiveRecord::Base
   validates :format, presence: true
   validates :publishing_app, presence: true
   validates :title, presence: true, if: :renderable_content?
+  validates :rendering_app, presence: true, dns_hostname: true, if: :renderable_content?
 
 private
   def self.query_keys
