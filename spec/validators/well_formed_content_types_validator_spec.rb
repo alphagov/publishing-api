c73c8625a3e7b02bfdc98ff0e5d154220ec80622
Chris Patuzzo <chris@patuzzo.co.uk>
2015-11-18 10:59:38 +0000

    Use real mime types in the test examples

--- spec/validators/well_formed_content_types_validator_spec.rb
+++ spec/validators/well_formed_content_types_validator_spec.rb
@@ -30,56 +30,56 @@ RSpec.describe WellFormedContentTypesValidator do
   end
 
   it "accepts well-formed content types" do
-    value = [{ content_type: "html", content: "<p>content</p>" }]
+    value = [{ content_type: "text/html", content: "<p>content</p>" }]
     subject.validate_each(record, attribute, value)
     expect(record.errors).to be_empty
 
-    value = { foo: [{ content_type: "html", content: "<p>content</p>" }] }
+    value = { foo: [{ content_type: "text/html", content: "<p>content</p>" }] }
     subject.validate_each(record, attribute, value)
     expect(record.errors).to be_empty
 
-    value = [{ "content_type" => "html", "content" => "<p>content</p>" }]
+    value = [{ "content_type" => "text/html", "content" => "<p>content</p>" }]
     subject.validate_each(record, attribute, value)
     expect(record.errors).to be_empty
   end
 
   it "rejects values where the content key is missing" do
-    value = [{ content_type: "html" }]
+    value = [{ content_type: "text/html" }]
     subject.validate_each(record, attribute, value)
     expect(record.errors).to be_present
-    expect(record.errors).to eq(some_attribute: ["the 'html' content type does not contain content"])
+    expect(record.errors).to eq(some_attribute: ["the 'text/html' content type does not contain content"])
 
     record.errors.clear
 
-    value = { foo: [{ content_type: "html" }] }
+    value = { foo: [{ content_type: "text/html" }] }
     subject.validate_each(record, attribute, value)
     expect(record.errors).to be_present
-    expect(record.errors).to eq(some_attribute: ["the 'html' content type does not contain content"])
+    expect(record.errors).to eq(some_attribute: ["the 'text/html' content type does not contain content"])
 
     record.errors.clear
 
-    value = [{ content_type: "text" }, { content_type: "html", content: "<p>content</p>" }]
+    value = [{ content_type: "text/plain" }, { content_type: "text/html", content: "<p>content</p>" }]
     subject.validate_each(record, attribute, value)
     expect(record.errors).to be_present
-    expect(record.errors).to eq(some_attribute: ["the 'text' content type does not contain content"])
+    expect(record.errors).to eq(some_attribute: ["the 'text/plain' content type does not contain content"])
   end
 
-  context "when the 'must_include' option is set to 'html'" do
-    let(:options) { { must_include: "html" } }
+  context "when the 'must_include' option is set to 'text/html'" do
+    let(:options) { { must_include: "text/html" } }
 
-    it "rejects values that do not have a content type of 'html'" do
-      value = [{ content_type: "text", content: "content" }]
+    it "rejects values that do not have a content type of 'text/html'" do
+      value = [{ content_type: "text/plain", content: "content" }]
       subject.validate_each(record, attribute, value)
       expect(record.errors).to be_present
-      expect(record.errors).to eq(some_attribute: ["the 'html' content type is mandatory and it is missing"])
+      expect(record.errors).to eq(some_attribute: ["the 'text/html' content type is mandatory and it is missing"])
     end
   end
 
   context "when the 'must_include' option is not set" do
     let(:options) { {} }
 
-    it "accepts values that do not have a content type of 'html'" do
-      value = [{ content_type: "text", content: "content" }]
+    it "accepts values that do not have a content type of 'text/html'" do
+      value = [{ content_type: "text/plain", content: "content" }]
       subject.validate_each(record, attribute, value)
       expect(record.errors).to be_empty
     end
@@ -87,36 +87,36 @@ RSpec.describe WellFormedContentTypesValidator do
 
   it "rejects values that have duplicate content types" do
     value = [
-      { content_type: "html", content: "<p>content</p>" },
-      { content_type: "html", content: "<p>content</p>" },
+      { content_type: "text/html", content: "<p>content</p>" },
+      { content_type: "text/html", content: "<p>content</p>" },
     ]
     subject.validate_each(record, attribute, value)
     expect(record.errors).to be_present
-    expect(record.errors).to eq(some_attribute: ["there are 2 instances of the 'html' content type - there should be 1"])
+    expect(record.errors).to eq(some_attribute: ["there are 2 instances of the 'text/html' content type - there should be 1"])
 
     record.errors.clear
 
     value = [
-      { content_type: "html", content: "<p>content</p>" },
-      { content_type: "text", content: "content" },
-      { content_type: "text", content: "content" },
+      { content_type: "text/html", content: "<p>content</p>" },
+      { content_type: "text/plain", content: "content" },
+      { content_type: "text/plain", content: "content" },
     ]
     subject.validate_each(record, attribute, value)
     expect(record.errors).to be_present
-    expect(record.errors).to eq(some_attribute: ["there are 2 instances of the 'text' content type - there should be 1"])
+    expect(record.errors).to eq(some_attribute: ["there are 2 instances of the 'text/plain' content type - there should be 1"])
   end
 
   context "when the value is invalid for more than one reason" do
-    let(:options) { { must_include: "html" } }
-    let(:value) { [{ content_type: "text" }] }
+    let(:options) { { must_include: "text/html" } }
+    let(:value) { [{ content_type: "text/plain" }] }
 
     it "communicates all of those reasons back to the programmer in one go" do
       subject.validate_each(record, attribute, value)
       expect(record.errors).to be_present
 
       error_messages = record.errors.fetch(attribute)
-      expect(error_messages).to include("the 'text' content type does not contain content")
-      expect(error_messages).to include("the 'html' content type is mandatory and it is missing")
+      expect(error_messages).to include("the 'text/plain' content type does not contain content")
+      expect(error_messages).to include("the 'text/html' content type is mandatory and it is missing")
     end
   end
 
