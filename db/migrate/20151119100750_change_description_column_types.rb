1e9ef5e54a94fb6708d36582eb8e17d6ee4dac23
Chris Patuzzo <chris@patuzzo.co.uk>
2015-11-20 13:30:48 +0000

    Store descriptions in the format the app expects…
    I have checked that …
    
    ```
    DraftContentItem.find_each { |i| puts i.description }
    ```
    
    … works as expected

--- db/migrate/20151119100750_change_description_column_types.rb
+++ db/migrate/20151119100750_change_description_column_types.rb
@@ -4,10 +4,13 @@ class ChangeDescriptionColumnTypes < ActiveRecord::Migration
       [:draft_content_items, DraftContentItem],
       [:live_content_items, LiveContentItem],
     ].each do |table, model|
-      add_column table, :new_description, :json
+      add_column table, :new_description, :json, default: { value: nil }
 
       # http://www.postgresql.org/docs/9.3/static/functions-json.html#FUNCTIONS-JSON-TABLE
-      ActiveRecord::Base.connection.execute("update #{table} set new_description = to_json(description)")
+      ActiveRecord::Base.connection.execute(%{
+        update #{table} set new_description = concat('{"value":', to_json(description), '}')::json
+        where description is not null
+      })
 
       rename_column table, :description, :old_description
       rename_column table, :new_description, :description
