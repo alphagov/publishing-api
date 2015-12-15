76721bcbe56ef8bddebf874b8e364880a5208b78
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2015-12-15 12:51:30 +0000

    Expose redrafted status and live_version
    If an item has been published then subsequently edited - ie it has a live
    version number less than the draft version number - then publication_state is
    reported as "redrafted".
    
    Also expose live_version attribute if the item has been published.

--- app/presenters/queries/content_item_presenter.rb
+++ app/presenters/queries/content_item_presenter.rb
@@ -3,12 +3,14 @@ module Presenters
     class ContentItemPresenter
       def self.present(content_item)
         version = Version.find_by(target: content_item)
-        new(content_item, version).present
+        live_version = Version.find_by(target: content_item.live_content_item)
+        new(content_item, version, live_version).present
       end
 
-      def initialize(content_item, version)
+      def initialize(content_item, version, live_version)
         self.content_item = content_item
         self.version = version
+        self.live_version = live_version
       end
 
       def present
@@ -17,15 +19,19 @@ module Presenters
           .merge(
             version: version.number,
             publication_state: publication_state,
-          )
+          ).tap { |h| h[:live_version] = live_version.number if live_version.present? }
       end
 
     private
 
-      attr_accessor :content_item, :version
+      attr_accessor :content_item, :version, :live_version
 
       def publication_state
-        content_item.live_content_item.present? ? 'live' : 'draft'
+        if live_version.nil?
+          'draft'
+        else
+          version.number > live_version.number ? 'redrafted' : 'live'
+        end
       end
     end
   end
--- spec/presenters/queries/content_item_presenter_spec.rb
+++ spec/presenters/queries/content_item_presenter_spec.rb
@@ -3,23 +3,58 @@ require 'rails_helper'
 RSpec.describe Presenters::Queries::ContentItemPresenter do
   describe "present" do
     let(:content_id) { SecureRandom.uuid }
-
-    before do
-      content_item = FactoryGirl.create(:draft_content_item, content_id: content_id)
-      FactoryGirl.create(:version, target: content_item, number: 101)
-      @result = Presenters::Queries::ContentItemPresenter.present(content_item)
-    end
+    let(:content_item) { FactoryGirl.create(:draft_content_item, content_id: content_id) }
+    let!(:version) { FactoryGirl.create(:version, target: content_item, number: 101) }
+    let(:result) { Presenters::Queries::ContentItemPresenter.present(content_item) }
 
     it "presents content item attributes as a hash" do
-      expect(@result.fetch(:content_id)).to eq(content_id)
+      expect(result.fetch(:content_id)).to eq(content_id)
     end
 
     it "exposes the version number of the content item" do
-      expect(@result.fetch(:version)).to eq(101)
+      expect(result.fetch(:version)).to eq(101)
     end
 
-    it "exposes the publication state of the content item" do
-      expect(@result.fetch(:publication_state)).to eq("draft")
+    context "with no published version" do
+      it "shows the publication state of the content item as draft" do
+        expect(result.fetch(:publication_state)).to eq("draft")
+      end
+
+      it "does not include live_version" do
+        expect(result).not_to have_key(:live_version)
+      end
+    end
+
+    context "with a published version and no subsequent draft" do
+      let(:live_content_item) { FactoryGirl.create(:live_content_item, content_id: content_id, draft_content_item: content_item) }
+
+      before do
+        FactoryGirl.create(:version, target: live_content_item, number: 101)
+      end
+
+      it "shows the publication state of the content item as live" do
+        expect(result.fetch(:publication_state)).to eq("live")
+      end
+
+      it "exposes the live version number" do
+        expect(result.fetch(:live_version)).to eq(101)
+      end
+    end
+
+    context "with a published version and a subsequent draft" do
+      let(:live_content_item) { FactoryGirl.create(:live_content_item, content_id: content_id, draft_content_item: content_item) }
+
+      before do
+        FactoryGirl.create(:version, target: live_content_item, number: 100)
+      end
+
+      it "shows the publication state of the content item as redrafted" do
+        expect(result.fetch(:publication_state)).to eq("redrafted")
+      end
+
+      it "exposes the live version number" do
+        expect(result.fetch(:live_version)).to eq(100)
+      end
     end
   end
 end
