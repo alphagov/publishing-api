0068c592bf576db80fc9f2f135a4fb9a308ce728
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-12-29 10:44:02 +0000

    Commit missed schema version number.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126135748) do
+ActiveRecord::Schema.define(version: 20151207154727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
