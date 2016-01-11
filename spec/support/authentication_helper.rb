5b83f5d8d20f1d0a561d2842684069ecd6b867f2
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2016-01-11 16:10:49 +0000

    Fake login during tests.
    We need two different types of fake login, one for controller specs and one for
    requests specs. This is because controller specs interact with Rack::Test
    directly, so we have to stub the Warden request environment explicitly,
    whereas the request specs go through a higher-level abstraction and we can
    simply set GDS::SSO.test_user.

---
+++ spec/support/authentication_helper.rb
@@ -0,0 +1,28 @@
+module AuthenticationHelper
+  module ControllerMixin
+    def login_as(user)
+      request.env['warden'] = double(
+        :authenticate! => true,
+        :authenticated? => true,
+        :user => user
+      )
+    end
+
+    def login_as_stub_user
+      user = create(:user, permissions: ['signin'])
+      login_as(user)
+    end
+  end
+
+
+  module RequestMixin
+    def login_as(user)
+      GDS::SSO.test_user = user
+    end
+
+    def login_as_stub_user
+      user = create(:user, permissions: ['signin'])
+      login_as(user)
+    end
+  end
+end
