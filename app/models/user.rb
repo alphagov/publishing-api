36b593b2966df412c9599a457c4057f881f9d003
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2016-01-11 11:57:08 +0000

    Set User.app_name from email by default.
    We will be using app_name to restrict the items an app sees by default on the
    get_content endpoint, by filtering on publishing_app. GDS-SSO does not send
    this data; email and name are not sufficiently constrained to automatically use
    either. As a starting point, use the user part of the email address on first
    access, this can be changed manually per-app if necessary.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
   include GDS::SSO::User
 
   serialize :permissions, Array
+
+  def set_app_name!
+    if app_name.blank? && email.present?
+      self.app_name = email.split('@')[0]
+      save!
+    end
+  end
 end
