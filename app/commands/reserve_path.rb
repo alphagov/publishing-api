cd0ca72c07a5d0f03331e121fb1fde2286b3d07a
Chris Patuzzo <chris@patuzzo.co.uk>
2016-02-10 17:32:44 +0000

    Move #base_path method out of BaseCommand…
    The /v2 commands don’t all require a base path and
    so we shouldn’t push this method into the
    superclass as it is not shared across all commands.

--- app/commands/reserve_path.rb
+++ app/commands/reserve_path.rb
@@ -4,5 +4,11 @@ module Commands
       PathReservation.reserve_base_path!(base_path, payload[:publishing_app])
       Success.new(payload)
     end
+
+  private
+
+    def base_path
+      payload.fetch(:base_path)
+    end
   end
 end
