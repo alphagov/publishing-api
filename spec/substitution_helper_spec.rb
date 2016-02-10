68bd5cd079e325d7561ed74b2ddfe56d85c6e4ff
Chris Patuzzo <chris@patuzzo.co.uk>
2016-02-10 17:19:39 +0000

    Update the SubstitutionHelper to use new object model…
    This now “withdraws” a content item if it is
    blocking another one. This only happens if that
    content item has a format that is considered
    “substitutable” – e.g. gone, redirect

---
+++ spec/substitution_helper_spec.rb
@@ -0,0 +1,95 @@
+require "rails_helper"
+
+RSpec.describe SubstitutionHelper do
+  let(:existing_format) { "guide" }
+  let(:new_format) { "guide" }
+  let(:existing_base_path) { "/vat-rates" }
+
+  let!(:existing_item) {
+    FactoryGirl.create(:draft_content_item, :with_location, :with_translation,
+      format: existing_format,
+      base_path: existing_base_path,
+    )
+  }
+
+  let!(:live_item) {
+    FactoryGirl.create(:live_content_item, :with_location, :with_translation,
+      format: existing_format,
+      base_path: existing_base_path,
+    )
+  }
+  let!(:french_item) {
+    FactoryGirl.create(:draft_content_item, :with_location, :with_translation,
+      format: existing_format,
+      base_path: existing_base_path,
+      locale: "fr",
+    )
+  }
+  let!(:item_elsewhere) {
+    FactoryGirl.create(:draft_content_item, :with_location, :with_translation,
+      format: existing_format,
+      base_path: "/somewhere-else",
+    )
+  }
+
+
+  before do
+    subject.clear!(
+      new_item_format: new_format,
+      new_item_content_id: new_content_id,
+      base_path: existing_base_path,
+      locale: "en",
+      state: "draft",
+    )
+  end
+
+  context "when the content_id is the same as the existing item" do
+    let(:new_content_id) { existing_item.content_id }
+
+    it "does not withdraw the existing item" do
+      state = State.find_by!(content_item: existing_item)
+      expect(state.name).not_to eq("withdrawn")
+    end
+  end
+
+  context "when the content_id differs from the existing item" do
+    let(:new_content_id) { SecureRandom.uuid }
+
+    context "when the existing item has a format that is substitutable" do
+      let(:existing_format) { "gone" }
+
+      it "withdraws the existing item" do
+        state = State.find_by!(content_item: existing_item)
+        expect(state.name).to eq("withdrawn")
+      end
+
+      it "doesn't withdraw any other items" do
+        expect(State.find_by!(content_item: live_item).name).not_to eq("withdrawn")
+        expect(State.find_by!(content_item: french_item).name).not_to eq("withdrawn")
+        expect(State.find_by!(content_item: item_elsewhere).name).not_to eq("withdrawn")
+      end
+    end
+
+    context "when the new item has a format that is substitutable" do
+      let(:new_format) { "gone" }
+
+      it "withdraws the existing item" do
+        state = State.find_by!(content_item: existing_item)
+        expect(state.name).to eq("withdrawn")
+      end
+
+      it "doesn't withdraw any other items" do
+        expect(State.find_by!(content_item: live_item).name).not_to eq("withdrawn")
+        expect(State.find_by!(content_item: french_item).name).not_to eq("withdrawn")
+        expect(State.find_by!(content_item: item_elsewhere).name).not_to eq("withdrawn")
+      end
+    end
+
+    context "when neither item has a format that is substitutable" do
+      it "does not withdraw the existing item" do
+        state = State.find_by!(content_item: existing_item)
+        expect(state.name).not_to eq("withdrawn")
+      end
+    end
+  end
+end
