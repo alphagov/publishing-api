03f8dfe1da1db0362fe0c0f11c71c7e4e17a0312
Chris Patuzzo <chris@patuzzo.co.uk>
2016-02-10 17:40:23 +0000

    Fix the data sanitizer

--- spec/lib/tasks/data_sanitizer_spec.rb
+++ spec/lib/tasks/data_sanitizer_spec.rb
@@ -1,9 +1,36 @@
 require "rails_helper"
 
 RSpec.describe Tasks::DataSanitizer do
-  let!(:non_limited_draft) { FactoryGirl.create(:draft_content_item) }
-  let!(:limited_draft) { FactoryGirl.create(:access_limited_draft_content_item) }
-  let!(:live_content_item) { FactoryGirl.create(:live_content_item) }
+  let!(:non_limited_draft) do
+    FactoryGirl.create(
+      :draft_content_item,
+      :with_location,
+      :with_translation,
+      :with_semantic_version,
+      base_path: "/non-limited-draft",
+    )
+  end
+
+  let!(:limited_draft) do
+    FactoryGirl.create(
+      :access_limited_draft_content_item,
+      :with_location,
+      :with_translation,
+      :with_semantic_version,
+      base_path: "/limited-draft",
+    )
+  end
+
+  let!(:live_content_item) do
+    FactoryGirl.create(
+      :live_content_item,
+      :with_location,
+      :with_translation,
+      :with_semantic_version,
+      base_path: "/live-item",
+    )
+  end
+
   let(:stdout) { double(:stdout, puts: nil)}
 
   before do
@@ -13,33 +40,31 @@ RSpec.describe Tasks::DataSanitizer do
   it "deletes all access limited drafts" do
     Tasks::DataSanitizer.delete_access_limited(stdout)
 
-    expect(DraftContentItem.exists?(limited_draft.id)).to eq(false)
-    expect(DraftContentItem.exists?(non_limited_draft.id)).to eq(true)
-    expect(LiveContentItem.exists?(live_content_item.id)).to eq(true)
+    expect(ContentItem.exists?(limited_draft.id)).to eq(false)
+    expect(ContentItem.exists?(non_limited_draft.id)).to eq(true)
+    expect(ContentItem.exists?(live_content_item.id)).to eq(true)
   end
 
   it "deletes access limited drafts from the draft content store" do
     expect(PublishingAPI.service(:draft_content_store)).to receive(:delete_content_item)
-      .with(limited_draft.base_path)
+      .with("/limited-draft")
 
     Tasks::DataSanitizer.delete_access_limited(stdout)
   end
 
-  it "resets drafts to live if available, wiping access limiting" do
-    FactoryGirl.create(:live_content_item,
-      base_path: limited_draft.base_path,
+  it "removes the limited draft" do
+    FactoryGirl.create(
+      :live_content_item,
+      :with_location,
+      base_path: "/live-item",
       content_id: limited_draft.content_id,
-      draft_content_item: limited_draft,
       title: "A live content item",
     )
 
-    Tasks::DataSanitizer.delete_access_limited(stdout)
+    expect {
+      Tasks::DataSanitizer.delete_access_limited(stdout)
+    }.to change(ContentItem, :count).by(-1)
 
-    expect(DraftContentItem.exists?(limited_draft.id)).to eq(true)
-
-    limited_draft.reload
-
-    expect(limited_draft.title).to eq("A live content item")
-    expect(limited_draft.access_limited).to eq({})
+    expect(ContentItem.exists?(limited_draft.id)).to eq(false)
   end
 end
