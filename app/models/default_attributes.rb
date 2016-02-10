f5a046f091a09885557c5e9ab341e08148014537
Chris Patuzzo <chris@patuzzo.co.uk>
2016-02-10 17:39:57 +0000

    Never default timestamps to nil

--- app/models/default_attributes.rb
+++ app/models/default_attributes.rb
@@ -5,7 +5,7 @@ module DefaultAttributes
     def assign_attributes_with_defaults(attributes)
       new_attributes = self.class.column_defaults.symbolize_keys
         .merge(attributes.symbolize_keys)
-        .except(:id, :version)
+        .except(:id, :version, :created_at, :updated_at)
       assign_attributes(new_attributes)
     end
   end
