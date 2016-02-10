072cbb78aa9b055355e4da539fbea027cf1fc315
Chris Patuzzo <chris@patuzzo.co.uk>
2016-02-10 17:35:04 +0000

    Mark AccessLimit polymorphic field as deprecated

--- app/models/access_limit.rb
+++ app/models/access_limit.rb
@@ -1,10 +1,15 @@
 class AccessLimit < ActiveRecord::Base
+  # These are to be removed after deploy
   belongs_to :target, polymorphic: true
+  deprecated_columns :target_id, :target_type
+  # These are to be removed after deploy
+
+  belongs_to :content_item
 
   validate :user_uids_are_strings
 
   def self.viewable?(target, user_uid: nil)
-    if (access_limit = self.find_by(target: target))
+    if (access_limit = self.find_by(content_item: target))
       user_uid.present? && access_limit.users.include?(user_uid)
     else
       true
