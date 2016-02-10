d6a481d24162eb798a4ba1b3d2788261f50d43a5
Chris Patuzzo <chris@patuzzo.co.uk>
2016-02-10 17:31:28 +0000

    Re-work the PutDraftContentWithLinks command (v1)

--- app/commands/put_draft_content_with_links.rb
+++ app/commands/put_draft_content_with_links.rb
@@ -1,59 +1,36 @@
 module Commands
-  class PutDraftContentWithLinks < PutContentWithLinks
+  class PutDraftContentWithLinks < BaseCommand
     def call
-      if content_item[:content_id]
-        draft_content_item = create_or_update_draft_content_item!
-        create_or_update_links!
-      end
+      if payload[:content_id]
+        V2::PutContent.call(payload)
+        V2::PutLinkSet.call(payload.slice(:content_id, :links))
+      else
+        PathReservation.reserve_base_path!(base_path, payload[:publishing_app])
 
-      PathReservation.reserve_base_path!(base_path, content_item[:publishing_app])
-
-      if downstream
-        if (access_limit = AccessLimit.find_by(target: draft_content_item))
-          attributes = content_item.merge(
-            access_limited: {
-              users: access_limit.users
-            }
-          )
-        else
-          attributes = content_item
+        if downstream
+          content_store_payload = Presenters::DownstreamPresenter::V1.present(payload, update_type: false)
+          Adapters::DraftContentStore.put_content_item(base_path, content_store_payload)
         end
-
-        payload = Presenters::DownstreamPresenter::V1.present(
-          attributes,
-          update_type: false,
-        )
-        Adapters::DraftContentStore.put_content_item(base_path, payload)
       end
 
-      Success.new(content_item)
+      Success.new(payload.except(:base_path))
     end
 
   private
-
-    def create_or_update_draft_content_item!
-      DraftContentItem.create_or_replace(content_item_attributes) do |item|
-        SubstitutionHelper.clear_draft!(item)
-
-        if item.valid?
-          version = Version.find_or_initialize_by(target: item)
-          version.increment
-          version.save!
-
-          if access_limit_params && (users = access_limit_params[:users])
-            AccessLimit.create(
-              target: item,
-              users: users
-            )
-          end
-        end
-
-        item.assign_attributes_with_defaults(content_item_attributes)
-      end
+    def base_path
+      payload.fetch(:base_path)
     end
 
-    def access_limit_params
-      payload[:access_limited]
+    def draft_content_item
+      @draft_content_item ||= ContentItemFilter.new(scope: ContentItem.where(content_id: payload[:content_id]))
+        .filter(
+          locale: locale,
+          state: "draft",
+        ).first
+    end
+
+    def locale
+      payload.fetch(:locale, ContentItem::DEFAULT_LOCALE)
     end
   end
 end
