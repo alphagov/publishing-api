a02d900c73be7ccb777fb93bc7fc4d2415da42f8
Chris Patuzzo <chris@patuzzo.co.uk>
2016-02-11 12:38:28 +0000

    Improve test coverage for Queries::GetPublishIntent

--- app/queries/get_publish_intent.rb
+++ app/queries/get_publish_intent.rb
@@ -1,14 +1,32 @@
 module Queries
   module GetPublishIntent
-    def self.call(base_path)
-      response = content_store.get_publish_intent(base_path)
-      response.to_hash.deep_symbolize_keys
-    end
+    class << self
+      def call(base_path)
+        response = content_store.get_publish_intent(base_path)
+        response.to_hash.deep_symbolize_keys
+      rescue GdsApi::TimedOutException
+        raise_error(500, "The live content store timed out requesting publish intent")
+      rescue GdsApi::HTTPNotFound
+        raise_error(404, "Could not find a publish intent for #{base_path}")
+      end
 
-  private
+    private
 
-    def self.content_store
-      PublishingAPI.service(:live_content_store)
+      def content_store
+        PublishingAPI.service(:live_content_store)
+      end
+
+      def raise_error(code, message)
+        raise CommandError.new(
+          code: code,
+          error_details: {
+            error: {
+              code: code,
+              message: message,
+            },
+          },
+        )
+      end
     end
   end
 end
--- spec/queries/get_publish_intent_spec.rb
+++ spec/queries/get_publish_intent_spec.rb
@@ -1,15 +1,49 @@
 require "rails_helper"
 
 RSpec.describe Queries::GetPublishIntent do
-  let(:base_path) { "/foo" }
+  let(:timeout) { false }
 
   before do
-    stub_request(:get, %r{.*content-store.*/publish-intent#{base_path}})
-      .to_return(body: { foo: "bar" }.to_json)
+    request_stub = stub_request(:get, %r{.*content-store.*/publish-intent#{base_path}})
+
+    if timeout
+      request_stub.to_timeout
+    else
+      request_stub.to_return(status: status, body: body.to_json)
+    end
   end
 
-  it "returns the body of the response from the content store" do
-    result = subject.call(base_path)
-    expect(result).to eq(foo: "bar")
+  context "when the content store responds with a 200" do
+    let(:base_path) { "/vat-rates" }
+    let(:status) { 200 }
+    let(:body) { { foo: "bar" } }
+
+    it "returns the body of the response from the content store" do
+      result = subject.call(base_path)
+      expect(result).to eq(body)
+    end
+  end
+
+  context "when the content store responds with a 404" do
+    let(:base_path) { "/missing" }
+    let(:status) { 404 }
+    let(:body) { {} }
+
+    it "raises a command error" do
+      expect {
+        subject.call(base_path)
+      }.to raise_error(CommandError, /could not find/i)
+    end
+  end
+
+  context "when the content store times out" do
+    let(:base_path) { "/timeout" }
+    let(:timeout) { true }
+
+    it "raises a command error" do
+      expect {
+        subject.call(base_path)
+      }.to raise_error(CommandError, /content store timed out/i)
+    end
   end
 end
