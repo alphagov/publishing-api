884b3ab6ab3df5f6abe87b8e0a4567b816c3e390
Chris Patuzzo <chris@patuzzo.co.uk>
2016-02-11 11:35:22 +0000

    Improve test coverage for GetLinkSet…
    Previously, it wasn’t checking that links are
    included in the response, which seems to be the
    primary reason for using this command.

--- spec/queries/get_link_set_spec.rb
+++ spec/queries/get_link_set_spec.rb
@@ -1,25 +1,76 @@
 require "rails_helper"
 
 RSpec.describe Queries::GetLinkSet do
-  before do
-    foo = FactoryGirl.create(:link_set, content_id: "foo")
-    FactoryGirl.create(:version, target: foo, number: 2)
-    FactoryGirl.create(:link_set, content_id: "bar")
-  end
+  let(:content_id) { SecureRandom.uuid }
 
-  it "returns the link set for a given content_id" do
-    expect(subject.call("foo").fetch(:content_id)).to eq("foo")
-  end
+  context "when the link set exists" do
+    let!(:link_set) do
+      FactoryGirl.create(:link_set, content_id: content_id)
+    end
 
-  it "returns the version of the link set" do
-    expect(subject.call("foo").fetch(:version)).to eq(2)
+    before do
+      FactoryGirl.create(:version, target: link_set, number: 5)
+    end
+
+    context "and it has some links" do
+      let(:parent) { [SecureRandom.uuid] }
+      let(:related) { [SecureRandom.uuid, SecureRandom.uuid] }
+
+      before do
+        FactoryGirl.create(
+          :link,
+          link_set: link_set,
+          link_type: "parent",
+          target_content_id: parent.first
+        )
+
+        FactoryGirl.create(
+          :link,
+          link_set: link_set,
+          link_type: "related",
+          target_content_id: related.first
+        )
+
+        FactoryGirl.create(
+          :link,
+          link_set: link_set,
+          link_type: "related",
+          target_content_id: related.last
+        )
+      end
+
+      it "returns the content_id, version and links grouped by link_type" do
+        result = subject.call(content_id)
+
+        expect(result).to eq(
+          content_id: content_id,
+          version: 5,
+          links: {
+            parent: parent,
+            related: related,
+          }
+        )
+      end
+    end
+
+    context "and it doesn't have any links" do
+      it "returns the content_id, version and an empty links hash" do
+        result = subject.call(content_id)
+
+        expect(result).to eq(
+          content_id: content_id,
+          version: 5,
+          links: {},
+        )
+      end
+    end
   end
 
   context "when the link set does not exist" do
-    it "returns an error object" do
+    it "raises a command error" do
       expect {
-        subject.call("missing")
-      }.to raise_error(CommandError, /with content_id: missing/)
+        subject.call(content_id)
+      }.to raise_error(CommandError, /could not find link set/i)
     end
   end
 end
