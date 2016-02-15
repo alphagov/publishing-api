7b558fa56d58cd29b547b5d06fed4db2e1019f56
Graham Pengelly <graham@three-tier.com>
2016-02-15 11:05:07 +0000

    Remove `transmitted_at` from Content Store calls
    This commit removes `transmitted_at` from the Content Store calls and
    related tests. This supports PR https://github.com/alphagov/content-store/pull/189
    
    `transmitted_at` has been left on DownstreamPresenter::V1

--- spec/pacts/content_store/put_endpoint_spec.rb
+++ spec/pacts/content_store/put_endpoint_spec.rb
@@ -9,16 +9,11 @@ RSpec.describe "PUT endpoint pact with the Content Store", pact: true do
 
   let(:client) { ContentStoreWriter.new("http://localhost:3093") }
   let(:body) { Presenters::ContentStorePresenter.present(content_item) }
-  let(:request_time) { Time.at(2000000000) }
 
-  around do |example|
-    Timecop.freeze(request_time) { example.run }
-  end
-
-  context "when a content item exists that has an older transmitted_at than the request" do
+  context "a content item exists" do
     before do
       content_store
-        .given("a content item exists with base_path /vat-rates and transmitted_at 1000000000000000000")
+        .given("a content item exists with base_path /vat-rates")
         .upon_receiving("a request to create a content item")
         .with(
           method: :put,
@@ -37,104 +32,9 @@ RSpec.describe "PUT endpoint pact with the Content Store", pact: true do
         )
     end
 
-    it "accepts in-order messages to the content store" do
+    it "accepts the messages to the content store" do
       response = client.put_content_item(base_path: "/vat-rates", content_item: body)
       expect(response.code).to eq(200)
-    end
-  end
-
-  context "when a content item exists that has a newer transmitted_at than the request" do
-    before do
-      content_store
-        .given("a content item exists with base_path /vat-rates and transmitted_at 3000000000000000000")
-        .upon_receiving("a request to create a content item")
-        .with(
-          method: :put,
-          path: "/content/vat-rates",
-          body: body,
-          headers: {
-            "Content-Type" => "application/json"
-          },
-        )
-        .will_respond_with(
-          status: 409,
-          body: {},
-          headers: {
-            "Content-Type" => "application/json; charset=utf-8"
-          },
-        )
-    end
-
-    it "rejects out-of-order messages to the content store" do
-      expect {
-        client.put_content_item(base_path: "/vat-rates", content_item: body)
-      }.to raise_error(GdsApi::HTTPConflict)
-    end
-  end
-
-  describe "V1" do
-    let(:attributes) { content_item_params }
-    let(:body) do
-      Presenters::DownstreamPresenter::V1.present(
-        attributes, update_type: false
-      )
-    end
-
-    context "when a content item exists that has an older transmitted_at than the request" do
-      before do
-        content_store
-          .given("a content item exists with base_path /vat-rates and transmitted_at 1000000000000000000")
-          .upon_receiving("a request to create a content item originating from v1 endpoint")
-          .with(
-            method: :put,
-            path: "/content/vat-rates",
-            body: body,
-            headers: {
-              "Content-Type" => "application/json"
-            },
-          )
-          .will_respond_with(
-            status: 200,
-            body: {},
-            headers: {
-              "Content-Type" => "application/json; charset=utf-8"
-            },
-          )
-      end
-
-      it "accepts in-order messages to the content store" do
-        response = client.put_content_item(base_path: "/vat-rates", content_item: body)
-        expect(response.code).to eq(200)
-      end
-    end
-
-    context "when a content item exists that has a newer transmitted_at than the request" do
-      before do
-        content_store
-          .given("a content item exists with base_path /vat-rates and transmitted_at 3000000000000000000")
-          .upon_receiving("a request to create a content item originating from v1 endpoint")
-          .with(
-            method: :put,
-            path: "/content/vat-rates",
-            body: body,
-            headers: {
-              "Content-Type" => "application/json"
-            },
-          )
-          .will_respond_with(
-            status: 409,
-            body: {},
-            headers: {
-              "Content-Type" => "application/json; charset=utf-8"
-            },
-          )
-      end
-
-      it "rejects out-of-order messages to the content store" do
-        expect {
-          client.put_content_item(base_path: "/vat-rates", content_item: body)
-        }.to raise_error(GdsApi::HTTPConflict)
-      end
     end
   end
 end
--- spec/support/request_helpers/downstream_requests.rb
+++ spec/support/request_helpers/downstream_requests.rb
@@ -2,35 +2,29 @@ module RequestHelpers
   module DownstreamRequests
     def sends_to_draft_content_store
       it "sends to draft content store" do
-        Timecop.freeze do
-          expect(PublishingAPI.service(:draft_content_store)).to receive(:put_content_item)
-            .with(
-              base_path: base_path,
-              content_item: content_item_for_draft_content_store
-                .merge(transmitted_at: DateTime.now.to_s(:nanoseconds))
-            )
+        expect(PublishingAPI.service(:draft_content_store)).to receive(:put_content_item)
+          .with(
+            base_path: base_path,
+            content_item: content_item_for_draft_content_store,
+          )
 
-          do_request
+        do_request
 
-          expect(response.status).to eq(200), response.body
-        end
+        expect(response.status).to eq(200), response.body
       end
     end
 
     def sends_to_live_content_store
       it "sends to live content store" do
-        Timecop.freeze do
-          expect(PublishingAPI.service(:live_content_store)).to receive(:put_content_item)
-            .with(
-              base_path: base_path,
-              content_item: content_item_for_live_content_store
-                .merge(transmitted_at: DateTime.now.to_s(:nanoseconds))
-            )
+        expect(PublishingAPI.service(:live_content_store)).to receive(:put_content_item)
+          .with(
+            base_path: base_path,
+            content_item: content_item_for_live_content_store,
+          )
 
-          do_request
+        do_request
 
-          expect(response.status).to eq(200), response.body
-        end
+        expect(response.status).to eq(200), response.body
       end
     end
 
