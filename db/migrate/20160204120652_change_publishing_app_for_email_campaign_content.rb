2b6cf54cae162e03e8ab11d0611139287cf81846
Chris Patuzzo <chris@patuzzo.co.uk>
2016-02-19 14:15:18 +0000

    Update EmailCampaignMigration to use the new object model

--- db/migrate/20160204120652_change_publishing_app_for_email_campaign_content.rb
+++ db/migrate/20160204120652_change_publishing_app_for_email_campaign_content.rb
@@ -8,22 +8,21 @@ class ChangePublishingAppForEmailCampaignContent < ActiveRecord::Migration
       reservation.save(validate: false)
     end
 
-    DraftContentItem.where(publishing_app: "email-campaign-frontend").each do |content_item|
+    ContentItem.where(publishing_app: "email-campaign-frontend").each do |content_item|
       content_item.update_attributes!(publishing_app: "share-sale-publisher")
 
-      ContentStoreWorker.perform_async(
-        content_store: Adapters::DraftContentStore,
-        draft_content_item_id: content_item.id,
-      )
-    end
+      state = State.find_by!(content_item: content_item)
 
-    LiveContentItem.where(publishing_app: "email-campaign-frontend").each do |content_item|
-      content_item.update_attributes!(publishing_app: "share-sale-publisher")
+      if state.name == "draft"
+        content_store = Adapters::DraftContentStore
+      elsif state.name == "published"
+        content_store = Adapters::ContentStore
+      end
 
       ContentStoreWorker.perform_async(
-        content_store: Adapters::ContentStore,
-        live_content_item_id: content_item.id,
-      )
+        content_store: content_store,
+        content_item_id: content_item.id,
+      ) if content_store
     end
   end
 end
