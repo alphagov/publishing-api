b49daf4c74837e40d17fe4d6e0c44f572ae9b9cc
Graham Pengelly <graham@three-tier.com>
2016-02-26 14:11:44 +0000

    Initialise statsd and expose on Application
    This will allow `payload_version` conflicts to be monitored

---
+++ config/initializers/statsd.rb
@@ -0,0 +1,10 @@
+require "statsd"
+
+# Statsd "the process" listens on a port on the provided host for UDP
+# messages. Given that it's UDP, it's fire-and-forget and will not
+# block your application. You do not need to have a statsd process
+# running locally on your development environment.
+statsd_client = Statsd.new("localhost")
+statsd_client.namespace = "govuk.app.publishing-api"
+
+PublishingAPI::Application.statsd = statsd_client
