ad7b68b668b6ccc530b4de95a87cf64995520ca7
Paul Bowsher <paul.bowsher@gmail.com>
2016-02-26 17:46:10 +0000

    Lint whole project in CI

--- jenkins.sh
+++ jenkins.sh
@@ -38,15 +38,10 @@ git merge --no-commit origin/master || git merge --abort
 export RAILS_ENV=test
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment --without development
 
-# Lint changes introduced in this branch, but not for master
-if [[ ${GIT_BRANCH} != "origin/master" ]]; then
-  bundle exec govuk-lint-ruby \
-    --diff \
-    --cached \
-    --format html --out rubocop-${GIT_COMMIT}.html \
-    --format clang \
-    app config Gemfile lib spec
-fi
+bundle exec govuk-lint-ruby \
+  --format html --out rubocop-${GIT_COMMIT}.html \
+  --format clang \
+  app config Gemfile lib spec
 
 bundle exec rake db:drop db:create db:schema:load
 
