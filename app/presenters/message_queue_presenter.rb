d78f1c23e5a72752988aded6e0ed202803712195
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2016-04-18 10:57:10 +0100

    Add govuk_request_id to RabbitMQ message.

--- app/presenters/message_queue_presenter.rb
+++ app/presenters/message_queue_presenter.rb
@@ -2,7 +2,10 @@ module Presenters
   class MessageQueuePresenter
     def self.present(content_item, event, update_type:)
       attributes = DownstreamPresenter.present(content_item, event)
-      attributes.merge(update_type: update_type)
+      attributes.merge(
+        update_type: update_type,
+        govuk_request_id: GdsApi::GovukHeaders.headers[:govuk_request_id],
+      )
     end
   end
 end
--- spec/requests/logging_requests_spec.rb
+++ spec/requests/logging_requests_spec.rb
@@ -2,26 +2,32 @@ require "rails_helper"
 require "sidekiq/testing"
 
 RSpec.describe "Logging requests", type: :request do
-  let(:content_id) { SecureRandom.uuid }
-  let(:draft_content_item_params) { content_item_params.except(:links) }
-
-  around do |example|
-    Sidekiq::Testing.fake! do
-      example.run
-      Sidekiq::Worker.clear_all
-    end
-  end
+  let(:govuk_request_id) { "12345-67890" }
 
   it "adds a request uuid to the content store worker job" do
-    put("/v2/content/#{content_id}", draft_content_item_params.to_json,
-      "HTTP_GOVUK_REQUEST_ID" => "12345-67890"
-    )
-    GdsApi::GovukHeaders.clear_headers # Simulate workers running in a separate thread
-    Sidekiq::Worker.drain_all # Run all workers
+    Sidekiq::Testing.fake! do
+      put("/v2/content/#{SecureRandom.uuid}", content_item_params.except(:links).to_json,
+        "HTTP_GOVUK_REQUEST_ID" => govuk_request_id,
+      )
+      GdsApi::GovukHeaders.clear_headers # Simulate workers running in a separate thread
+      Sidekiq::Worker.drain_all # Run all workers
+      Sidekiq::Worker.clear_all
+    end
 
     expect(WebMock).to have_requested(:put, /draft-content-store.*content/)
       .with(headers: {
-        "GOVUK-Request-Id" => "12345-67890"
+        "GOVUK-Request-Id" => govuk_request_id,
       })
+  end
+
+  it "adds a request uuid to the message bus" do
+    draft_content_item = FactoryGirl.create(:draft_content_item)
+
+    expect(PublishingAPI.service(:queue_publisher)).to receive(:send_message)
+      .with(hash_including(govuk_request_id: govuk_request_id))
+
+    post("/v2/content/#{draft_content_item.content_id}/publish", { update_type: "minor" }.to_json,
+      "HTTP_GOVUK_REQUEST_ID" => "12345-67890"
+    )
   end
 end
