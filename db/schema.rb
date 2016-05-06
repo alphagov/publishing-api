85db96379375acdabe31f52e2a7278c558372e6e
Brendan Butler <brendan.butler@digital.cabinet-office.gov.uk>
2016-05-06 13:49:55 +0100

    Remove redundant Special Education Needs policy
    - There are two copies of this policy. This removes one of them as per
      this [Zendesk ticket](https://govuk.zendesk.com/agent/tickets/1299583).

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505133601) do
+ActiveRecord::Schema.define(version: 20160506124002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
