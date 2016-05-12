b8b70e42492e2a4a22d62ec18b6407cce32336d6
Chris Patuzzo <chris@patuzzo.co.uk>
2016-05-12 14:18:10 +0100

    Allow for order parameter to specify results sort order
    This matches the interface used by Rummager as
    documented here:
    https://github.com/alphagov/rummager/blob/master/docs/unified-search-api.md

--- app/pagination.rb
+++ app/pagination.rb
@@ -4,7 +4,7 @@ class Pagination
 
   def initialize(options = {})
     @options = options
-    @order = { public_updated_at: :desc }
+    @order = order_from_options(options)
     @page = options[:page] || 1
     @per_page = options.fetch(:per_page, PER_PAGE).to_i
 
@@ -25,5 +25,56 @@ private
 
   def offset_from_page
     options.fetch(:offset, (page.to_i - 1) * per_page).to_i
+  end
+
+  def order_from_options(options)
+    order = options.fetch(:order, "-public_updated_at")
+
+    if order.start_with?("-")
+      field = order[1..-1].to_sym
+      direction = :desc
+    else
+      field = order.to_sym
+      direction = :asc
+    end
+
+    raise_unless_valid_order_field(field)
+
+    { field => direction }
+  end
+
+  def raise_unless_valid_order_field(field)
+    return if valid_order_fields.include?(field)
+
+    message = "Invalid order field: #{field}."
+    message += " Valid order fields: [#{valid_order_fields.join(', ')}]"
+
+    raise CommandError.new(
+      code: 422,
+      message: "Invalid order field: #{field}",
+      error_details: {
+        error: {
+          code: 422,
+          message: message,
+        }
+      }
+    )
+  end
+
+  # These fields have indexes and so we can use them to order results. If we
+  # need to add an order field, ensure that it has an index or query performance
+  # will be hampered.
+  def valid_order_fields
+    [
+      :content_id,
+      :document_type,
+      :format,
+      :public_updated_at,
+      :publishing_app,
+      :rendering_app,
+      :base_path,
+      :locale,
+      :updated_at,
+    ]
   end
 end
