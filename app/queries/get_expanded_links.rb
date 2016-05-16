5b4126a34911eaefa67ee0eb7ae58f45fbe79d3d
Tijmen Brommet <tijmen@gmail.com>
2016-05-16 15:57:09 +0100

    Extract GET expanded links into Query object
    This is more consistent with the other GET endpoints in this controller.

---
+++ app/queries/get_expanded_links.rb
@@ -0,0 +1,14 @@
+module Queries
+  class GetExpandedLinks
+    def self.call(content_id)
+      link_set = LinkSet.find_by(content_id: content_id)
+
+      expanded_link_set = Presenters::Queries::ExpandedLinkSet.new(
+        link_set: link_set,
+        fallback_order: [:draft, :published]
+      )
+
+      expanded_link_set.links
+    end
+  end
+end
