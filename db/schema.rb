5108c0bf2ca7d65036a00c5fd31d2452c88b847e
Paul Bowsher <paul.bowsher@gmail.com>
2016-05-24 22:09:07 +0100

    Create index on linkable.base_path
    This is queried twice in `PutContent` as a `Linkables.exists?` call.
    Without an index these queries takes 40ms each.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517134907) do
+ActiveRecord::Schema.define(version: 20160524210649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20160517134907) do
     t.datetime "updated_at"
   end
 
+  add_index "linkables", ["base_path"], name: "index_linkables_on_base_path", using: :btree
   add_index "linkables", ["document_type"], name: "index_linkables_on_document_type", using: :btree
 
   create_table "links", force: :cascade do |t|
