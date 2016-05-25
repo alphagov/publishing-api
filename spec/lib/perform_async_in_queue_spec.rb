fdbc364c59690c5e30fdd90ed9da339e6ad88f16
Paul Bowsher <paul.bowsher@gmail.com>
2016-05-25 16:45:49 +0100

    Helper to enqueue in named queue
    Includable module that mimics Whitehall’s `perform_async_in_queue`

---
+++ spec/lib/perform_async_in_queue_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe PerformAsyncInQueue do
+  describe '.perform_async_in_queue' do
+    let!(:worker_class) { ExampleWorker = Class.new { include PerformAsyncInQueue } }
+
+    it 'calls client_push with the correct queue name' do
+      expect(Sidekiq::Client)
+        .to receive(:enqueue_to)
+        .with('foo', ExampleWorker, 'bar')
+
+      ExampleWorker.perform_async_in_queue('foo', 'bar')
+    end
+  end
+end
