43c1b6beb923582070fdb0429d69c80b3954edf3
Paul Bowsher <paul.bowsher@gmail.com>
2016-06-06 16:33:13 +0100

    Delete HTML publications from publishing api
    Due to the various stages of migration of HTML publications, it's best to clear out the partially migrated content items and start from scratch. Example reasons we need to do this:
    
    - Some HTML publications had different content ids across their various editions
    - Some HTML publications had their locale set to en when it wasn't, which causes `patch_link_set` calls to send the wrong locale to the Content Store
    
    These are not currently used anywhere in production code. I've included a safety check to prevent this migration being run twice accidentally in production.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160601134855) do
+ActiveRecord::Schema.define(version: 20160606142809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
