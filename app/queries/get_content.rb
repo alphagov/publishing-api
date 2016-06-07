57259b0c973ebb6279afe28cc6f00bbb3e28eca7
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-06-07 11:00:49 +0000

    Remove state scope from GetContent query

--- app/queries/get_content.rb
+++ app/queries/get_content.rb
@@ -5,7 +5,6 @@ module Queries
 
       content_items = ContentItem.where(content_id: content_id)
       content_items = Translation.filter(content_items, locale: locale)
-      content_items = State.filter(content_items, name: %w(draft published))
 
       response = Presenters::Queries::ContentItemPresenter.present_many(content_items).first
 
