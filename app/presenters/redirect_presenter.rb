1c2dacf7fb756aa10cad77f808bdea6cbfddd299
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2016-06-07 13:16:25 +0000

    RedirectHelper calls RedirectPresenter.
    For the sake of consistency.

--- app/presenters/redirect_presenter.rb
+++ app/presenters/redirect_presenter.rb
@@ -1,12 +1,12 @@
 module RedirectPresenter
-  def self.present(base_path:, publishing_app:, destination:, public_updated_at:)
+  def self.present(base_path:, publishing_app:, destination:, public_updated_at:, redirects: nil)
     {
       document_type: "redirect",
       schema_name: "redirect",
       base_path: base_path,
       publishing_app: publishing_app,
       public_updated_at: public_updated_at.iso8601,
-      redirects: [
+      redirects: redirects || [
         {
           path: base_path,
           type: "exact",
