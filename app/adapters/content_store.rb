48a1ee0279880e65e5f28a6dc6724b0ba3b94fb1
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-06-08 10:30:43 +0000

    Freeze live content store state fallback order

--- app/adapters/content_store.rb
+++ app/adapters/content_store.rb
@@ -2,7 +2,7 @@
 # to
 module Adapters
   class ContentStore
-    DEPENDENCY_FALLBACK_ORDER = [:published]
+    DEPENDENCY_FALLBACK_ORDER = [:published].freeze
 
     def self.put_content_item(base_path, content_item)
       CommandError.with_error_handling do
