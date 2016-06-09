118e88c17a875b6fcbad7e9a8b201b36e12ab229
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2016-06-09 12:23:59 +0000

    Get format value to send downstream.
    Content store is still expecting the format field, so we need to get it
    from document_type.

--- app/models/web_content_item.rb
+++ app/models/web_content_item.rb
@@ -12,7 +12,7 @@ class WebContentItem
   end
 
   CONTENT_ITEM_METHODS = [
-    :content_id, :description, :analytics_identifier, :title, :public_updated_at
+    :content_id, :description, :analytics_identifier, :title, :public_updated_at, :document_type
   ]
 
   def_delegators :@content_item, *CONTENT_ITEM_METHODS
