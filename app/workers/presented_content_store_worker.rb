e3646f97fed37b63272c359b6aa9ae6cc42461ac
Paul Bowsher <paul.bowsher@gmail.com>
2016-06-16 18:25:25 +0100

    Don't perform safety check for every job
    Only perform the state == draft check if we would send to the live
    content store. This saves an additional database query if we aren't
    attempting to do anything potentially dangerous anyway.

--- app/workers/presented_content_store_worker.rb
+++ app/workers/presented_content_store_worker.rb
@@ -27,14 +27,15 @@ private
 
   def send_to_content_store
     if content_item_id
-      content_item_is_draft = (State.where(content_item_id: content_item_id).pluck(:name) == ["draft"])
       content_store_is_live = (content_store == Adapters::ContentStore)
 
-      if content_item_is_draft && content_store_is_live
-        raise CommandError.new(
-          code: 500,
-          message: "Will not send a draft content item to the live content store",
-        )
+      if content_store_is_live
+        if State.where(content_item_id: content_item_id).pluck(:name) == ["draft"]
+          raise CommandError.new(
+            code: 500,
+            message: "Will not send a draft content item to the live content store",
+          )
+        end
       end
 
       base_path = presented_payload.fetch(:base_path)
