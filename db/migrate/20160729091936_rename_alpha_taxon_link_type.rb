bdfdfccd902de5713e6ccdecfac731af3db21d3b
Mo Baig <mo@mobaig.com>
2016-07-29 14:05:09 +0100

    Add data migration to move alpha_taxons links to taxons link type
    We've renamed the alpha_taxons link type to taxons. This updates the
    links for content currently tagged to alpha_taxons.

---
+++ db/migrate/20160729091936_rename_alpha_taxon_link_type.rb
@@ -0,0 +1,18 @@
+class RenameAlphaTaxonLinkType < ActiveRecord::Migration
+  def up
+    alpha_taxon_links = Link.where(link_type: "alpha_taxons")
+    link_sets = LinkSet.where(id: alpha_taxon_links.pluck(:link_set_id).uniq)
+    link_sets.each do |link_set|
+      target_content_ids = link_set.links.where(link_type: "alpha_taxons").pluck(:target_content_id)
+      Commands::V2::PatchLinkSet.call(
+        {
+          content_id: link_set.content_id,
+          links: {
+            alpha_taxons: [],
+            taxons: target_content_ids,
+          }
+        }
+      )
+    end
+  end
+end
