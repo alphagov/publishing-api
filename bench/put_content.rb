ec47ca9f6d9083fcccda125bfe9f4c15832bb16f
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2016-08-25 14:31:34 +0000

    Benchmark redrafting items in put_content.
    Use --redraft to test putting new draft content on top of existing
    published items.

--- bench/put_content.rb
+++ bench/put_content.rb
@@ -15,6 +15,7 @@ ActiveSupport::Notifications.subscribe "sql.active_record" do |name, started, fi
 end
 
 new_item = (ARGV.first == '--new-item')
+redraft = (ARGV.first == '--redraft')
 
 content_id = SecureRandom.uuid
 title = Faker::Company.catch_phrase
@@ -47,14 +48,34 @@ content_items = 100.times.map do
 end
 
 begin
-  StackProf.run(mode: :wall, out: "tmp/put_content_wall.dump") do
-    puts Benchmark.measure {
-      content_items.each do |item|
-        Commands::V2::PutContent.call(item)
-        print "."
-      end
-      puts ""
-    }
+  if redraft
+    puts "Creating published items..."
+    content_items.each do |item|
+      Commands::V2::PutContent.call(item)
+      Commands::V2::Publish.call(content_id: item[:content_id], update_type: 'major')
+    end
+    $queries = 0
+
+    puts "Publishing..."
+    StackProf.run(mode: :wall, out: "tmp/put_content_wall.dump") do
+      puts Benchmark.measure {
+        content_items.each do |item|
+          Commands::V2::PutContent.call(item.merge(title: Faker::Company.catch_phrase))
+          print "."
+        end
+        puts ""
+      }
+    end
+  else
+    StackProf.run(mode: :wall, out: "tmp/put_content_wall.dump") do
+      puts Benchmark.measure {
+        content_items.each do |item|
+          Commands::V2::PutContent.call(item)
+          print "."
+        end
+        puts ""
+      }
+    end
   end
 
   puts "#{$queries} SQL queries"
