1d398c3033db1104bd002d05b7d443dfd9795d4c
Douglas Roper <dougdroper@gmail.com>
2016-08-25 11:05:07 +0100

    Refactor unpublish command
    Split call method into smaller methods so that it is easier to see what
    is happening in the method.
    
    Move content_item into its own method so that it doesn't need to passed
    in as an argument everywhere.
    
    Split the validation into 4 separate methods so that it is easier to
    read.

--- app/commands/v2/unpublish.rb
+++ app/commands/v2/unpublish.rb
@@ -2,22 +2,67 @@ module Commands
   module V2
     class Unpublish < BaseCommand
       def call
+        validate
+        State.supersede(previous_item) if previous_item
+        transision_state
+        delete_linkable
+
+        after_transaction_commit do
+          send_downstream
+        end
+
+        Success.new(content_id: content_id)
+      end
+
+    private
+
+      def transision_state
+        raise_invalid_type unless valid_type?
+        method = {
+          "withdrawal" => method(:withdraw),
+          "redirect" => method(:redirect),
+          "vanish" => method(:vanish),
+          "gone" => method(:gone),
+        }[type].call
+      end
+
+      def valid_type?
+        %w(withdrawal redirect gone vanish).include?(type)
+      end
+
+      def type
+        payload.fetch(:type)
+      end
+
+      def raise_invalid_type
+        message = "#{type} is not a valid unpublishing type"
+        raise_command_error(422, message, fields: {})
+      end
+
+      def content_item
+        @content_item ||= find_unpublishable_content_item
+      end
+
+      def content_id
+        @content_id ||= payload.fetch(:content_id)
+      end
+
+      def validate_allow_discard_draft!
         if payload[:allow_draft] && payload[:discard_drafts]
           message = "allow_draft and discard_drafts cannot be used together"
           raise_command_error(422, message, fields: {})
         end
+      end
 
-        content_id = payload.fetch(:content_id)
-        content_item = find_unpublishable_content_item(content_id)
-
+      def validate_content_item_presence!
         unless content_item.present?
           message = "Could not find a content item to unpublish"
           raise_command_error(404, message, fields: {})
         end
+      end
 
-        check_version_and_raise_if_conflicting(content_item, previous_version_number)
-
-        if draft_present?(content_id) && !payload[:allow_draft]
+      def validate_draft_presence!
+        if draft_exists? && !payload[:allow_draft]
           if payload[:discard_drafts] == true
             DiscardDraft.call(
               {
@@ -33,50 +78,30 @@ module Commands
             raise_command_error(422, message, fields: {})
           end
         end
-
-        previous_item = lookup_previous_item(content_item)
-        State.supersede(previous_item) if previous_item
-
-        case type = payload.fetch(:type)
-        when "withdrawal"
-          withdraw(content_item)
-        when "redirect"
-          redirect(content_item)
-        when "gone"
-          gone(content_item)
-        when "vanish"
-          vanish(content_item)
-        else
-          message = "#{type} is not a valid unpublishing type"
-          raise_command_error(422, message, fields: {})
-        end
-
-        delete_linkable(content_item)
-
-        after_transaction_commit do
-          send_downstream(content_item)
-        end
-
-        Success.new(content_id: content_id)
       end
 
-    private
+      def validate
+        validate_allow_discard_draft!
+        validate_content_item_presence!
+        check_version_and_raise_if_conflicting(content_item, previous_version_number)
+        validate_draft_presence!
+      end
 
-      def withdraw(content_item)
+      def withdraw
         State.unpublish(content_item,
           type: "withdrawal",
           explanation: payload.fetch(:explanation),
         )
       end
 
-      def redirect(content_item)
+      def redirect
         State.unpublish(content_item,
           type: "redirect",
           alternative_path: payload.fetch(:alternative_path),
         )
       end
 
-      def gone(content_item)
+      def gone
         State.unpublish(content_item,
           type: "gone",
           alternative_path: payload[:alternative_path],
@@ -84,15 +109,15 @@ module Commands
         )
       end
 
-      def vanish(content_item)
+      def vanish
         State.unpublish(content_item, type: "vanish")
       end
 
-      def delete_linkable(content_item)
+      def delete_linkable
         Linkable.find_by(content_item: content_item).try(:destroy)
       end
 
-      def send_downstream(content_item)
+      def send_downstream
         return unless downstream
 
         DownstreamDraftWorker.perform_async_in_queue(
@@ -118,7 +143,7 @@ module Commands
         payload[:previous_version].to_i if payload[:previous_version]
       end
 
-      def find_unpublishable_content_item(content_id)
+      def find_unpublishable_content_item
         allowed_states = %w(published unpublished)
 
         if payload[:allow_draft]
@@ -130,22 +155,21 @@ module Commands
         content_item if content_item && (payload[:allow_draft] || !Unpublishing.is_substitute?(content_item))
       end
 
-      def lookup_previous_item(content_item)
-        previous_items = ContentItemFilter.similar_to(
+      def previous_item
+        raise "There should only be one previous published or unpublished item" if previous_items.size > 1
+        previous_items.first
+      end
+
+      def previous_items
+        @previous_items ||= ContentItemFilter.similar_to(
           content_item,
           state: %w(published unpublished),
           base_path: nil,
           user_version: nil,
         ).to_a
-
-        if previous_items.size > 1
-          raise "There should only be one previous published or unpublished item"
-        end
-
-        previous_items.first
       end
 
-      def draft_present?(content_id)
+      def draft_exists?
         filter = ContentItemFilter.new(scope: ContentItem.where(content_id: content_id))
         filter.filter(locale: locale, state: "draft").exists?
       end
