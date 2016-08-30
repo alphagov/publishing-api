4bbf0a879f46fc034236824d3c9e3235c89a861b
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2016-08-30 13:06:27 +0000

    Publish conflict items that were only drafts
    In f9e19be we are removing items created by publisher that have a base
    path conflict. From publisher there were some instances where we had a
    draft conflict where the old item was published:
    
    ```
      # base_path: /done-apply-queens-award-enterprise, content_store: draft
      # keeping id: 832418, content_id: c3cd8556-4a95-4c6d-ab2d-d1f6cd280d37, state: draft, updated_at: 2016-06-28 13:41:00 UTC, publishing_app: publisher, document_type: completed_transaction
      # deleting id: 13146, content_id: 0e4bc22b-ff05-49a1-b837-f18be6fd6bb2, state: published, updated_at: 2016-02-29 09:24:10 UTC, publishing_app: publisher, document_type: placeholder, details match item to keep: no
      # deleting id: 226059, content_id: 0e4bc22b-ff05-49a1-b837-f18be6fd6bb2, state: draft, updated_at: 2016-02-29 09:24:10 UTC, publishing_app: publisher, document_type: placeholder, details match item to keep: no
    
      # base_path: /investment-for-growth-north-east, content_store: draft
      # keeping id: 667316, content_id: c8d9daa3-6e3a-4422-bde4-b85b7a54c526, state: draft, updated_at: 2016-05-06 14:08:15 UTC, publishing_app: publisher, document_type: placeholder
      # deleting id: 36282, content_id: 4032908f-511e-408e-86b0-696088cc5b15, state: published, updated_at: 2016-02-29 09:24:10 UTC, publishing_app: publisher, document_type: placeholder, details match item to keep: no
      # deleting id: 251342, content_id: 4032908f-511e-408e-86b0-696088cc5b15, state: draft, updated_at: 2016-02-29 09:24:10 UTC, publishing_app: publisher, document_type: placeholder, details match item to keep: no
    
      # base_path: /spirit-enterprise-fund-north-east-england, content_store: draft
      # keeping id: 667321, content_id: 41d78758-1ecd-401e-99d3-2325982a6da7, state: draft, updated_at: 2016-05-06 14:08:17 UTC, publishing_app: publisher, document_type: placeholder
      # deleting id: 37530, content_id: c48d1d7c-6faf-404d-b8d3-6784639d36ac, state: published, updated_at: 2016-02-29 09:24:10 UTC, publishing_app: publisher, document_type: placeholder, details match item to keep: no
      # deleting id: 252763, content_id: c48d1d7c-6faf-404d-b8d3-6784639d36ac, state: draft, updated_at: 2016-02-29 09:24:10 UTC, publishing_app: publisher, document_type: placeholder, details match item to keep: no
    ```
    
    Chatting to @boffbowsh it was determined the items were correct to be removed
    however the draft items should be published, which is what this migration does.

---
+++ db/migrate/20160830105752_publish_newer_placeholders.rb
@@ -0,0 +1,31 @@
+class PublishNewerPlaceholders < ActiveRecord::Migration
+  def up
+    to_publish = [
+      ["c3cd8556-4a95-4c6d-ab2d-d1f6cd280d37", "en"],
+      ["c8d9daa3-6e3a-4422-bde4-b85b7a54c526", "en"],
+      ["41d78758-1ecd-401e-99d3-2325982a6da7", "en"]
+    ]
+
+    to_publish.each do |(content_id, locale)|
+      next if already_published?(content_id, locale)
+      Commands::V2::Publish.call(
+        content_id: content_id,
+        locale: locale,
+        update_type: "minor",
+      )
+    end
+  end
+
+  def already_published?(content_id, locale)
+    results = connection.execute("
+      SELECT COUNT(*)
+      FROM content_items
+      INNER JOIN translations ON translations.content_item_id = content_items.id
+      INNER JOIN states ON states.content_item_id = content_items.id
+      WHERE states.name = 'published'
+      AND translations.locale = #{connection.quote(locale)}
+      AND content_items.content_id = #{connection.quote(content_id)}
+    ")
+    results[0]["count"] != "0"
+  end
+end
