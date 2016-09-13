26a8720712d2742b126f73291bb397756b6f5ce7
Douglas Roper <dougdroper@gmail.com>
2016-09-13 12:16:52 +0100

    remove migration

--- db/migrate/20160825141026_add_links_to_travel_advice.rb
+++
@@ -1,7 +0,0 @@
-class AddLinksToTravelAdvice < ActiveRecord::Migration
-  def change
-    Link.where("passthrough_hash IS NOT NULL").each do |link|
-      link.update_attributes!(target_content_id: link.passthrough_hash[:content_id])
-    end
-  end
-end
