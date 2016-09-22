a0227ec49b339988aab1c199e48a8f1577385f4a
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2016-09-22 17:01:03 +0100

    Add query to check if a ContentItem is publishable

---
+++ spec/queries/check_for_content_item_preventing_draft_from_being_published_spec.rb
@@ -0,0 +1,173 @@
+require "rails_helper"
+
+RSpec.describe Queries::CheckForContentItemPreventingDraftFromBeingPublished do
+  let(:content_id) { SecureRandom.uuid }
+  let(:base_path) { "/vat-rates" }
+  let(:document_type) { "guide" }
+
+  describe ".call" do
+    subject { described_class.call(content_id, base_path, document_type) }
+
+    shared_examples "check succeeds" do
+      it "returns nil" do
+        expect(subject).to eq(nil)
+      end
+    end
+
+    context "with a single content item" do
+      before do
+        FactoryGirl.create(:content_item,
+          content_id: content_id,
+          base_path: base_path,
+          document_type: document_type,
+          user_facing_version: 1,
+          locale: "en",
+        )
+      end
+
+      include_examples "check succeeds"
+    end
+
+    context "with two content items of different locales" do
+      before do
+        FactoryGirl.create(:content_item,
+          content_id: content_id,
+          base_path: base_path + ".en",
+          document_type: document_type,
+          user_facing_version: 1,
+          locale: "en",
+        )
+
+        FactoryGirl.create(:content_item,
+          content_id: content_id,
+          base_path: base_path + ".es",
+          document_type: document_type,
+          user_facing_version: 1,
+          locale: "es",
+        )
+      end
+
+      include_examples "check succeeds"
+    end
+
+    context "with a unpublished item, of type \"substitute\", and a draft at the same base path" do
+      before do
+        FactoryGirl.create(:unpublished_content_item,
+          content_id: SecureRandom.uuid,
+          base_path: base_path,
+          document_type: document_type,
+          unpublishing_type: "substitute",
+          user_facing_version: 1,
+          locale: "en",
+        )
+
+        FactoryGirl.create(:content_item,
+          content_id: content_id,
+          base_path: base_path,
+          document_type: document_type,
+          user_facing_version: 1,
+          locale: "en",
+        )
+      end
+
+      include_examples "check succeeds"
+    end
+
+    context "with a published item, with a substitutable document_type, and a draft at the same base path" do
+      before do
+        FactoryGirl.create(:content_item,
+          content_id: SecureRandom.uuid,
+          base_path: base_path,
+          document_type: "unpublishing",
+          state: "published",
+          user_facing_version: 1,
+          locale: "en",
+        )
+
+        FactoryGirl.create(:content_item,
+          content_id: content_id,
+          base_path: base_path,
+          document_type: document_type,
+          user_facing_version: 1,
+          locale: "en",
+        )
+      end
+
+      include_examples "check succeeds"
+    end
+
+    context "with a draft with a substitutable document_type, and a published item at the same base path" do
+      let(:document_type) { "unpublishing" }
+
+      before do
+        FactoryGirl.create(:content_item,
+          content_id: SecureRandom.uuid,
+          base_path: base_path,
+          document_type: "guide",
+          state: "published",
+          user_facing_version: 1,
+          locale: "en",
+        )
+
+        FactoryGirl.create(:content_item,
+          content_id: content_id,
+          base_path: base_path,
+          document_type: document_type,
+          user_facing_version: 1,
+          locale: "en",
+        )
+      end
+
+      include_examples "check succeeds"
+    end
+
+    context "with a unpublished item, and a draft at the same base path" do
+      before do
+        @blocking_content_item = FactoryGirl.create(:gone_unpublished_content_item,
+          content_id: SecureRandom.uuid,
+          base_path: base_path,
+          document_type: document_type,
+          user_facing_version: 1,
+          locale: "en",
+        )
+
+        FactoryGirl.create(:content_item,
+          content_id: content_id,
+          base_path: base_path,
+          document_type: document_type,
+          user_facing_version: 1,
+          locale: "en",
+        )
+      end
+
+      it "fails, returning the id of the content item" do
+        expect(subject).to eq(@blocking_content_item.id)
+      end
+    end
+
+    context "with a published item, and a draft at the same base path" do
+      before do
+        @blocking_content_item = FactoryGirl.create(:content_item,
+          content_id: SecureRandom.uuid,
+          base_path: base_path,
+          document_type: document_type,
+          state: "published",
+          user_facing_version: 1,
+          locale: "en",
+        )
+
+        FactoryGirl.create(:content_item,
+          content_id: content_id,
+          base_path: base_path,
+          document_type: document_type,
+          user_facing_version: 1,
+          locale: "en",
+        )
+      end
+
+      it "fails, returning the id of the content item" do
+        expect(subject).to eq(@blocking_content_item.id)
+      end
+    end
+  end
+end
