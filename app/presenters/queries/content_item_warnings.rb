5113577c1b276d4433c66c420ca5a9b57200686d
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2016-09-26 14:15:15 +0100

    Add ContentItemWarnings query
    Quite simple at the moment as there is only one query, but this is where
    other warnings can be hooked in.

---
+++ app/presenters/queries/content_item_warnings.rb
@@ -0,0 +1,23 @@
+module Presenters
+  module Queries
+    module ContentItemWarnings
+      def self.call(content_id, state, base_path, document_type)
+        return unless state == "draft"
+
+        blocking_content_item_id = ::Queries::CheckForContentItemPreventingDraftFromBeingPublished.call(
+          content_id,
+          base_path,
+          document_type,
+        )
+
+        warnings = {}
+
+        if blocking_content_item_id
+          warnings["content_item_blocking_publish"] = "There is an item of content that prevents this draft from being published"
+        end
+
+        warnings
+      end
+    end
+  end
+end
