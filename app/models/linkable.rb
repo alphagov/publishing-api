f075196f1d3ac01df1c524b8375f74dae67a083b
Douglas Roper <dougdroper@gmail.com>
2016-10-13 14:08:55 +0100

    Removes linkable model, spec and factory

--- app/models/linkable.rb
+++
@@ -1,8 +0,0 @@
-class Linkable < ApplicationRecord
-  belongs_to :content_item
-
-  validates :base_path, presence: true, uniqueness: true
-  validates :state, presence: true
-  validates :content_item, presence: true
-  validates :document_type, presence: true
-end
