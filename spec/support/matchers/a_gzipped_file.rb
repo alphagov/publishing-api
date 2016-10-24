2810de43ae763ee1883d38363026d0754d5a3c55
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2016-10-24 19:36:33 +0100

    Rspec matcher to check zipped file
    Used for checking the files that we are sending to S3.
    
    The output of it could be improved, but I couldn't work out how to do
    that and the `failure_message` methods don't seem to have an effect in
    the context we are using this.

---
+++ spec/support/matchers/a_gzipped_file.rb
@@ -0,0 +1,20 @@
+# Usage:
+#
+# expect(file).to be a_gzipped_file
+# expect(file).to be a_gzipped_file.containing("Hello World")
+# expect(class).to receive(:method).with(a_gzipped_file)
+RSpec::Matchers.define :a_gzipped_file do
+  match do |actual|
+    begin
+      @file = Zlib::GzipReader.open(actual)
+      return true unless @expected_contents
+      values_match?(@expected_contents, @file.read)
+    rescue Zlib::GzipFile::Error
+      false
+    end
+  end
+
+  chain :containing do |expected_contents|
+    @expected_contents = expected_contents
+  end
+end
