14929d7456eab82a88f7a7802930024e4f47705e
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2016-11-29 19:11:50 +0000

    Use content_id and locale to represent downstream
    This updates the interface used to call the Downstream*Worker from
    needing a content_item_id to a content_id and locale.

--- app/commands/v2/represent_downstream.rb
+++ app/commands/v2/represent_downstream.rb
@@ -9,55 +9,73 @@ module Commands
         filter = ContentItemFilter.new(scope: scope)
 
         if draft
-          items_for_draft_store(filter).pluck(:id, :content_id).each do |(content_item_id, content_id)|
-            downstream_draft(content_item_id, content_id)
+          content_ids = content_ids_for_draft_store(filter)
+          content_ids_with_locales(content_ids, draft_states).each do |(content_id, locale)|
+            downstream_draft(content_id, locale)
           end
         end
 
-        items_for_live_store(filter).pluck(:id, :content_id).each_with_index do |(content_item_id, content_id), index|
+        content_ids = content_ids_for_live_store(filter)
+        content_ids_with_locales(content_ids, live_states).each_with_index do |(content_id, locale), index|
           sleep 60 if (index + 1) % 10_000 == 0
-          downstream_live(content_item_id, content_id)
+          downstream_live(content_id, locale)
         end
       end
 
     private
 
-      def items_for_draft_store(filter)
-        Queries::GetLatest.call(
-          filter.filter(state: %w{draft published unpublished})
-        )
+      def content_ids_with_locales(content_ids, states)
+        content_ids.inject([]) do |memo, content_id|
+          memo + Queries::LocalesForContentItem.call(content_id, states).map { |locale| [content_id, locale] }
+        end
       end
 
-      def items_for_live_store(filter)
-        filter.filter(state: %w{published unpublished})
+      def content_ids_for_draft_store(filter)
+        Queries::GetLatest.call(filter.filter(state: draft_states)).distinct.pluck(:content_id)
       end
 
-      def downstream_draft(content_item_id, content_id)
+      def content_ids_for_live_store(filter)
+        filter.filter(state: live_states).distinct.pluck(:content_id)
+      end
+
+      def draft_states
+        %w{draft published unpublished}
+      end
+
+      def live_states
+        %w{published unpublished}
+      end
+
+      def downstream_draft(content_id, locale)
         event_payload = {
           content_id: content_id,
+          locale: locale,
           message: "Representing downstream draft",
         }
 
         EventLogger.log_command(self.class, event_payload) do |event|
           DownstreamDraftWorker.perform_async_in_queue(
             DownstreamDraftWorker::LOW_QUEUE,
-            content_item_id: content_item_id,
+            content_id: content_id,
+            locale: locale,
             payload_version: event.id,
             update_dependencies: false,
           )
         end
       end
 
-      def downstream_live(content_item_id, content_id)
+      def downstream_live(content_id, locale)
         event_payload = {
           content_id: content_id,
-          message: "Representing downstream publish",
+          locale: locale,
+          message: "Representing downstream live",
         }
 
         EventLogger.log_command(self.class, event_payload) do |event|
           DownstreamLiveWorker.perform_async_in_queue(
             DownstreamLiveWorker::LOW_QUEUE,
-            content_item_id: content_item_id,
+            content_id: content_id,
+            locale: locale,
             payload_version: event.id,
             message_queue_update_type: "links",
             update_dependencies: false,
