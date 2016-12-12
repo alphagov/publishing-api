0752b14e6aa313705d433059d59b712c1d9099bb
Douglas Roper <dougdroper@gmail.com>
2016-12-12 16:07:58 +0000

    Do not use joins for locales_for_content_items

--- app/queries/locales_for_content_items.rb
+++ app/queries/locales_for_content_items.rb
@@ -13,31 +13,23 @@ module Queries
       include_substitutes = false
     )
       content_items_table = ContentItem.arel_table
-      translations_table = Translation.arel_table
-      states_table = State.arel_table
 
-      scope = translations_table
+      scope = content_items_table
         .project(
           content_items_table[:content_id],
-          translations_table[:locale]
+          content_items_table[:locale]
         )
         .distinct
-        .join(content_items_table).on(
-          content_items_table[:id].eq(translations_table[:content_item_id])
-        )
-        .join(states_table).on(
-          content_items_table[:id].eq(states_table[:content_item_id])
-        )
         .where(content_items_table[:content_id].in(content_ids))
-        .where(states_table[:name].in(states))
-        .order(content_items_table[:content_id].asc, translations_table[:locale].asc)
+        .where(content_items_table[:state].in(states))
+        .order(content_items_table[:content_id].asc, content_items_table[:locale].asc)
 
       unless include_substitutes
         unpublishings_table = Unpublishing.arel_table
 
         scope = scope.outer_join(unpublishings_table).on(
           content_items_table[:id].eq(unpublishings_table[:content_item_id])
-            .and(states_table[:name].eq("unpublished"))
+            .and(content_items_table[:state].eq("unpublished"))
           )
           .where(
             unpublishings_table[:type].eq(nil).or(
