8985d93b374b5be0823f1782c7e8aa3c25c77711
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2016-12-12 15:46:17 +0000

    Add RoutesAndRedirectsValidator to ContentItem

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -8,15 +8,4 @@ RSpec.describe Location do
       expect(subject).to be_valid
     end
   end
-
-  describe "routes and redirects" do
-    subject { FactoryGirl.build(:location) }
-    let(:content_item) { FactoryGirl.build(:content_item, base_path: "/vat-rates") }
-
-    before do
-      subject.content_item = content_item
-    end
-
-    it_behaves_like RoutesAndRedirectsValidator
-  end
 end
