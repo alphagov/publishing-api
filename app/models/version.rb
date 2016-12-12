aa45b19d1da9f8b7094d1af2fe263088c42d8019
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2016-12-12 16:19:39 +0000

    Move draft and live versions into ContentItem
    We no longer handle this on a lock version, which I don't think will
    have any consequences.

--- app/models/version.rb
+++ app/models/version.rb
@@ -2,7 +2,6 @@
 module Version
   def self.included(subclass)
     subclass.validate :numbers_must_increase
-    subclass.validate :draft_cannot_be_behind_live, if: :content_item_target?
   end
 
   def increment
@@ -18,30 +17,5 @@ private
     mismatch = "(#{number} <= #{number_was})"
     message = "cannot be less than or equal to the previous number #{mismatch}"
     errors.add(:number, message)
-  end
-
-  def draft_and_live_versions
-    result = Queries::DraftAndLiveVersions.call(target, self.class)
-    if result["draft"] == self.number
-      draft_version = self.number
-      live_version = result["live"]
-    elsif result["live"] == self.number
-      draft_version = result["draft"]
-      live_version = self.number
-    end
-
-    [draft_version, live_version]
-  end
-
-  def draft_cannot_be_behind_live
-    draft_version, live_version = draft_and_live_versions
-
-    return unless draft_version && live_version
-
-    if draft_version < live_version
-      mismatch = "(#{draft_version} < #{live_version})"
-      message = "draft #{self.class.name} cannot be behind the live #{self.class.name} #{mismatch}"
-      errors.add(:number, message)
-    end
   end
 end
