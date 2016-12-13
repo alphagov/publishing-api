ef1c754aad3a5e7d484fdc86e561e19b2378c8d4
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2016-12-13 09:17:50 +0000

    Various test fixes

--- spec/lib/tasks/version_validator_spec.rb
+++ spec/lib/tasks/version_validator_spec.rb
@@ -31,9 +31,9 @@ RSpec.describe Tasks::VersionValidator do
 
   context "when two items of the same content_id have identical versions" do
     before do
-      version = UserFacingVersion.last
-      version.number = 1
-      version.save!(validate: false)
+      item = ContentItem.last
+      item.user_facing_version = 1
+      item.save!(validate: false)
     end
 
     it "outputs that the content item has an invalid version sequence" do
@@ -44,9 +44,9 @@ RSpec.describe Tasks::VersionValidator do
 
     context "but the content items have different locales" do
       before do
-        translation = Translation.last
-        translation.locale = "fr"
-        translation.save!(validate: false)
+        item = ContentItem.last
+        item.locale = 'fr'
+        item.save!(validate: false)
       end
 
       it "does not output any problems" do
@@ -59,9 +59,9 @@ RSpec.describe Tasks::VersionValidator do
 
   context "when the version sequence does not begin at zero" do
     before do
-      version = UserFacingVersion.first
-      version.number = 3
-      version.save!(validate: false)
+      item = ContentItem.first
+      item.user_facing_version = 3
+      item.save!(validate: false)
     end
 
     it "outputs that the content item has an invalid version sequence" do
