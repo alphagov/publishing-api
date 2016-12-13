2bde535a336f12990c3e147c0dc9272260e3b495
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2016-12-13 18:52:34 +0000

    Replace ContentItemFilter in RequeueContent
    This whole class might be something we can remove as it looks out of
    date and thus I assume no longer used (or if it's used it's incorrect).

--- lib/requeue_content.rb
+++ lib/requeue_content.rb
@@ -7,11 +7,11 @@ class RequeueContent
 
   def call
     if number_of_items.present?
-      ContentItemFilter.filter(state: :published).limit(number_of_items).each do |content_item|
+      ContentItem.where(state: :published).limit(number_of_items).each do |content_item|
         publish_to_queue(content_item)
       end
     else
-      ContentItemFilter.filter(state: :published).find_each do |content_item|
+      ContentItem.where(state: :published).find_each do |content_item|
         publish_to_queue(content_item)
       end
     end
