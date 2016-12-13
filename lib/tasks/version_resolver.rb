0b97e4b53bceb028eb0ce98668a8809e7d7685da
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2016-12-13 12:13:51 +0000

    Version validator and resolver for pillars
    Swaps over the version handling to work on the ContentItem table.

--- lib/tasks/version_resolver.rb
+++ lib/tasks/version_resolver.rb
@@ -1,18 +1,18 @@
 module Tasks
   class VersionResolver
     def self.resolve
-      VersionValidator.validate.each do |content_id, invalid_version_id, versions, version_ids|
-        index = version_ids.index(invalid_version_id)
+      VersionValidator.validate.each do |content_id, invalid_content_item_id, versions, content_item_ids|
+        index = content_item_ids.index(invalid_content_item_id)
         # Get the subset of versions needing update ie [2, 2, 3] from [1, 2, 2, 3]
-        version_ids[index..-1].each do |version_id|
+        content_item_ids[index..-1].each do |content_item_id|
           # Only update this version if it's the same number as the last
           if versions[index] == versions[index - 1]
-            invalid_version = UserFacingVersion.find(version_id)
-            invalid_version.number += 1
+            invalid_version = ContentItem.find(content_item_id)
+            invalid_version.user_facing_version += 1
             invalid_version.save(validate: false)
 
-            if version_id == version_ids.last
-              updated_version_numbers = UserFacingVersion.where(id: version_ids).map(&:number).sort
+            if content_item_id == content_item_ids.last
+              updated_version_numbers = ContentItem.where(id: content_item_ids).map(&:user_facing_version).sort
               puts "Resolved versions for #{content_id} from #{versions} to #{updated_version_numbers}"
             end
           end
