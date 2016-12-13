163e2a10280a78ad0ced9d0ec5e28c8f36c8f24e
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2016-12-13 19:06:33 +0000

    Blank out migrations that will fail to run
    These won't work again so code is removed in case someone accidentally
    executes them.

--- db/migrate/20160429095656_change_withdrawn_to_unpublished.rb
+++ db/migrate/20160429095656_change_withdrawn_to_unpublished.rb
@@ -1,40 +1,6 @@
 class ChangeWithdrawnToUnpublished < ActiveRecord::Migration
+  # Migration has been removed as it will no longer execute due to changes in
+  # the codebase
   def up
-    create_table :unpublishings do |t|
-      t.references :content_item, null: false
-      t.string :type, null: false
-      t.string :explanation
-      t.string :alternative_url
-      t.timestamps
-    end
-
-    add_index :unpublishings, :content_item_id
-    add_index :unpublishings, [:content_item_id, :type]
-
-    withdrawn_content_items = ContentItemFilter.filter(state: "withdrawn")
-
-    puts "Updating #{withdrawn_content_items.count} withdrawn content items"
-
-    withdrawn_content_items.find_each do |withdrawn_ci|
-      user_facing_version = UserFacingVersion.find_by(content_item: withdrawn_ci)
-      location = Location.find_by(content_item: withdrawn_ci)
-      translation = Translation.find_by(content_item: withdrawn_ci)
-
-      puts "Creating unpublishing for [ #{user_facing_version.number} | withdrawn | #{location.base_path} | #{translation.locale} ]"
-
-      Unpublishing.create!(
-        content_item: withdrawn_ci,
-        type: "substitute",
-        explanation: "Automatically unpublished to make way for another content item",
-      )
-    end
-
-    puts "Renaming 'withdrawn' to 'unpublished'"
-    State.where(name: "withdrawn").update_all(name: "unpublished")
-  end
-
-  def down
-    drop_table :unpublishings
-    State.where(name: "unpublished").update_all(name: "withdrawn")
   end
 end
