7af84a4adb151ae7b320532d32481b3e5d9c3145
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2016-12-13 11:18:16 +0000

    Fix spelling of 'locale'

--- app/substitution_helper.rb
+++ app/substitution_helper.rb
@@ -30,7 +30,7 @@ module SubstitutionHelper
           if state == "draft"
             Commands::V2::DiscardDraft.call({
                 content_id: blocking_item.content_id,
-                locae: locale,
+                locale: locale,
               },
               downstream: downstream,
               nested: nested,
