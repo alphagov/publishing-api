579aae1eda4a32a02102409d3f649ee58632ce4f
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2016-12-20 16:44:12 +0000

    Populate content_store field in content_items
    This moves the migration from
    https://github.com/alphagov/publishing-api/pull/659/commits/2711caa4adc93cab623182b51b825dd721d11ff2
    into this commit of it's own. It seemed to be less confusing to generate
    this as a fresh migration since it a) makes the timestamps easier to
    follow and b) can be run twice without problems.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219143746) do
+ActiveRecord::Schema.define(version: 20161220161528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
