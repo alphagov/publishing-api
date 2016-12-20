99e468f71638fc8d81667c852d3f06cc06326394
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2016-12-20 18:13:42 +0000

    Unique content_store for content_id & locale constraint
    Runs without a transaction so the index can be created concurrently, we
    only run a single statement because of this as it's difficult to debug a
    failing query.
    
    Replacement for:
    https://github.com/alphagov/publishing-api/pull/659/files#diff-421539f99eeaf240d79f84e6c7e31419
    which should be rollbacked before this.

---
+++ db/migrate/20161220180541_unique_content_store_for_content_id_and_locale.rb
@@ -0,0 +1,12 @@
+class UniqueContentStoreForContentIdAndLocale < ActiveRecord::Migration[5.0]
+  disable_ddl_transaction!
+
+  def change
+    # No 2 content items with the same content_id and locale can be on the
+    # same content_store at same time
+    add_index :content_items,
+      [:content_id, :locale, :content_store],
+      unique: true,
+      algorithm: :concurrently
+  end
+end
