0cbfff24d020cd05f511daee35f73bba6ea35d7c
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2016-12-21 08:21:04 +0000

    Add migration to populate documents table

---
+++ db/migrate/20161221074633_fill_documents.rb
@@ -0,0 +1,9 @@
+class FillDocuments < ActiveRecord::Migration[5.0]
+  def up
+    execute 'INSERT INTO documents (content_id, locale)
+             SELECT content_id, locale FROM content_items GROUP BY content_id, locale'
+  end
+
+  def down
+  end
+end
---
+++ db/migrate/20161221074658_create_editions.rb
@@ -0,0 +1,28 @@
+class CreateEditions < ActiveRecord::Migration[5.0]
+  def up
+    add_column :content_items, :document_id, :integer
+    add_foreign_key :content_items, :documents
+
+    execute 'UPDATE content_items SET document_id = (
+               SELECT id FROM documents
+               WHERE documents.content_id = content_items.content_id AND
+                  documents.locale = content_items.locale
+             )'
+
+    remove_column :content_items, :content_id
+    remove_column :content_items, :locale
+  end
+
+  def down
+    add_column :content_items, :content_id, :string
+    add_column :content_items, :locale, :string
+
+    execute 'UPDATE content_items
+             SET content_id = t.content_id, locale = t.locale
+             FROM (SELECT id, content_id, locale FROM documents) t
+             WHERE t.id = content_items.document_id'
+
+    remove_foreign_key :content_items, :documents
+    remove_column :content_items, :document_id
+  end
+end
