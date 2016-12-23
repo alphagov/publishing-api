5b124cb706734e9429d70e131ebf193a02eb9d6e
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2016-12-23 13:37:53 +0000

    Ensure access limits are not sent for live items.
    Although the publish command destroys any access limit items, we have
    seen occasions when the access_limited hash has been populated in the
    live content store. This ensures that it will never be sent for
    published items, and notifies airbrake if it encounters this situation.

--- spec/presenters/downstream_presenter_spec.rb
+++ spec/presenters/downstream_presenter_spec.rb
@@ -197,6 +197,33 @@ RSpec.describe Presenters::DownstreamPresenter do
       end
     end
 
+    context "for an access-limited item" do
+      let!(:access_limit) {
+        FactoryGirl.create(:access_limit, content_item: content_item)
+      }
+
+      context "in draft" do
+        let(:content_item) { FactoryGirl.create(:draft_content_item) }
+
+        it "populates the access_limited hash" do
+          expect(result[:access_limited][:users].length).to eq(1)
+        end
+      end
+
+      context "in live" do
+        let(:content_item) { FactoryGirl.create(:live_content_item) }
+
+        it "does not send an access_limited hash" do
+          expect(result).not_to include(:access_limited)
+        end
+
+        it "notifies Airbrake" do
+          expect(Airbrake).to receive(:notify)
+          result
+        end
+      end
+    end
+
     describe "rendering govspeak" do
       let(:details) do
         {
