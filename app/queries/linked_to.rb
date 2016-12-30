ff69521f322bd97e046376b3ccaa79e3d1264573
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2016-12-30 17:54:04 +0000

    Refactor linked_to query
    Aim was to make this easier to follow by making the recursion cases
    clearer.

--- app/queries/linked_to.rb
+++ app/queries/linked_to.rb
@@ -14,39 +14,46 @@ module Queries
     end
 
     def call
-      first_level = links_targetting(content_id)
-      expanded = first_level.flat_map do |(content_id, type)|
-        expand_recursive_links([content_id], [type])
-      end
-      content_ids = expanded.inject([]) do |memo, hash|
-        only_item = hash[:link_type_path].length == 1
-        valid_recursion = expansion_rules.valid_link_recursion?(hash[:link_type_path].reverse)
-        only_item || valid_recursion ? memo + hash[:content_ids] : memo
-      end
-      content_ids.uniq
+      expand_links
     end
 
   private
 
     attr_reader :content_id, :expansion_rules
 
-    def links_targetting(content_id, types = nil)
+    def links_targetting(content_id, types)
       links_where = { target_content_id: content_id }
       links_where[:link_type] = types if types.present?
       LinkSet.joins(:links).where(links: links_where).pluck(:content_id, :link_type)
     end
 
-    def expand_recursive_links(content_id_path, type_path)
-      next_content_id = content_id_path.last
-      allowed_types = expansion_rules.next_reverse_recursive_types(type_path)
-      cycle = content_id_path.uniq != content_id_path
-      current = { content_ids: content_id_path, link_type_path: type_path }
-      return [current] if allowed_types.empty? || cycle
-      links = links_targetting(next_content_id, allowed_types)
-      expanded_links = links.flat_map do |(content_id, type)|
-        expand_recursive_links(content_id_path + [content_id], type_path + [type])
+    def expand_links(content_ids_path = [], link_type_path = [])
+      links_for = content_ids_path.last || content_id
+      allowed_link_types = allowed_link_types(link_type_path)
+      in_cycle = content_ids_path.uniq != content_ids_path
+      fetch_links = content_ids_path.empty? || (!in_cycle && !allowed_link_types.empty?)
+
+      links = fetch_links ? links_targetting(links_for, allowed_link_types) : []
+
+      if links.empty?
+        valid_link_path?(link_type_path) ? content_ids_path : []
+      else
+        expanded_links = links.flat_map do |(content_id, link_type)|
+          expand_links(content_ids_path + [content_id], link_type_path + [link_type])
+        end
+
+        expanded_links.uniq
       end
-      [current] + expanded_links
+    end
+
+    def allowed_link_types(link_type_path)
+      link_type_path.empty? ? [] : expansion_rules.next_reverse_recursive_types(link_type_path)
+    end
+
+    def valid_link_path?(link_type_path)
+      only_item = link_type_path.length == 1
+      valid_recursion = expansion_rules.valid_link_recursion?(link_type_path.reverse)
+      only_item || valid_recursion
     end
   end
 end
