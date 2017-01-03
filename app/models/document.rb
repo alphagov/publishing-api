bdbfe83d2bf618ed1b4e9b9adcd5a0859e906f18
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-01-03 10:05:02 +0000

    Add 'draft' and 'live' methods to documents

--- app/models/document.rb
+++ app/models/document.rb
@@ -7,4 +7,12 @@ class Document < ApplicationRecord
     in: I18n.available_locales.map(&:to_s),
     message: 'must be a supported locale'
   }
+
+  def draft
+    content_items.find_by(state: "draft")
+  end
+
+  def live
+    content_items.find_by(state: %w(published unpublished))
+  end
 end
