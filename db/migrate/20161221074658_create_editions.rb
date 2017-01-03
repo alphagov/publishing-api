69617d0d223ae416bfcbcd6b94291d33610376bb
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-01-03 12:23:51 +0000

    Add migrations to change columns to become UUIDs

--- db/migrate/20161221074658_create_editions.rb
+++ db/migrate/20161221074658_create_editions.rb
@@ -1,10 +1,10 @@
 class CreateEditions < ActiveRecord::Migration[5.0]
   def up
-    execute 'UPDATE content_items SET document_id = (
+    execute "UPDATE content_items SET document_id = (
                SELECT id FROM documents
                WHERE documents.content_id = content_items.content_id AND
                   documents.locale = content_items.locale
-             )'
+             )"
 
     change_column :content_items, :document_id, :integer, null: false
 
@@ -15,12 +15,15 @@ class CreateEditions < ActiveRecord::Migration[5.0]
   end
 
   def down
-    add_column :content_items, :content_id, :string, null: false
-    add_column :content_items, :locale, :string, null: false
+    add_column :content_items, :content_id, :uuid
+    add_column :content_items, :locale, :string
 
-    execute 'UPDATE content_items
+    execute "UPDATE content_items
              SET content_id = t.content_id, locale = t.locale
              FROM (SELECT id, content_id, locale FROM documents) t
-             WHERE t.id = content_items.document_id'
+             WHERE t.id = content_items.document_id"
+
+    change_column :content_items, :content_id, :uuid, null: false
+    change_column :content_items, :locale, :string, null: false
   end
 end
