c626dbae353c91325381c6c4f98ce68c58b61b7f
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-01-04 13:48:28 +0000

    Create initial document model

--- spec/presenters/queries/link_set_presenter_spec.rb
+++ spec/presenters/queries/link_set_presenter_spec.rb
@@ -5,17 +5,17 @@ RSpec.describe Presenters::Queries::LinkSetPresenter do
     let(:content_id) { SecureRandom.uuid }
     let(:link_set) { FactoryGirl.create(:link_set, content_id: content_id) }
 
-    before do
+    subject(:result) do
       FactoryGirl.create(:lock_version, target: link_set, number: 101)
       @result = Presenters::Queries::LinkSetPresenter.present(link_set)
     end
 
     it "returns link set attributes as a hash" do
-      expect(@result.fetch(:content_id)).to eq(content_id)
+      expect(result.fetch(:content_id)).to eq(content_id)
     end
 
     it "exposes the lock_version of the link set" do
-      expect(@result.fetch(:version)).to eq(101)
+      expect(result.fetch(:version)).to eq(101)
     end
   end
 
