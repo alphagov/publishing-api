4dc379a6e730d4b447ca63dfe8dd9de8433ca057
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-01-04 14:13:27 +0000

    Populate documents from content items

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,10 @@
 class Document < ApplicationRecord
+  has_many :content_items
 
+  validates :content_id, presence: true
+
+  validates :locale, inclusion: {
+    in: I18n.available_locales.map(&:to_s),
+    message: 'must be a supported locale'
+  }
 end
