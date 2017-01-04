b1b9f56d866b358d9964fe2a9dadf6455fb30fa1
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-01-04 14:13:27 +0000

    Populate documents from content items

---
+++ db/migrate/20161221074640_link_documents_to_content_items.rb
@@ -0,0 +1,6 @@
+class LinkDocumentsToContentItems < ActiveRecord::Migration[5.0]
+  def change
+    add_column :content_items, :document_id, :integer
+    add_foreign_key :content_items, :documents
+  end
+end
