448020e77ff84434c47779ecf897e239b9e2cf2b
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-01-09 14:49:50 +0000

    Add indexes concurrently

--- db/migrate/20170106122400_link_documents_and_editions.rb
+++ db/migrate/20170106122400_link_documents_and_editions.rb
@@ -2,12 +2,5 @@ class LinkDocumentsAndEditions < ActiveRecord::Migration[5.0]
   def change
     add_column :content_items, :document_id, :integer
     add_foreign_key :content_items, :documents
-<<<<<<< 33fbf71cae7dd1fc2d4513db05afc23693e87061
-=======
-
-    add_index :content_items, [:document_id, :user_facing_version], unique: true
-    add_index :content_items, [:document_id, :content_store], unique: true
-    add_index :content_items, [:document_id, :state]
->>>>>>> Reorganise migrations for simplicity
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -72,9 +72,9 @@ ActiveRecord::Schema.define(version: 20170111161439) do
     t.integer  "user_facing_version",  default: 1,              null: false
     t.string   "base_path"
     t.string   "content_store"
-    t.string   "locale",                                        null: false
     t.uuid     "content_id",                                    null: false
-    t.integer  "document_id"
+    t.string   "locale",                                        null: false
+    t.integer  "document_id",                                   null: false
     t.index ["base_path", "content_store"], name: "index_content_items_on_base_path_and_content_store", unique: true, using: :btree
     t.index ["content_id", "locale", "content_store"], name: "index_content_items_on_content_id_and_locale_and_content_store", unique: true, using: :btree
     t.index ["content_id", "locale", "user_facing_version"], name: "index_unique_ufv_content_id_locale", unique: true, using: :btree
