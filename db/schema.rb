5960673496e4a36997a5d743ee241be3df3e4543
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-01-10 12:38:47 +0000

    Add index on document_id

--- db/schema.rb
+++ db/schema.rb
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 20170111161439) do
     t.index ["content_id", "locale", "user_facing_version"], name: "index_unique_ufv_content_id_locale", unique: true, using: :btree
     t.index ["content_id", "state", "locale"], name: "index_content_items_on_content_id_and_state_and_locale", using: :btree
     t.index ["content_id"], name: "index_content_items_on_content_id", using: :btree
+    t.index ["document_id"], name: "index_content_items_on_document_id", using: :btree
     t.index ["document_id", "content_store"], name: "index_content_items_on_document_id_and_content_store", unique: true, using: :btree
     t.index ["document_id", "state"], name: "index_content_items_on_document_id_and_state", using: :btree
     t.index ["document_id", "user_facing_version"], name: "index_content_items_on_document_id_and_user_facing_version", unique: true, using: :btree
