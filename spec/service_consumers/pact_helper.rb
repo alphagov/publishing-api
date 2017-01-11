a158f4697bcb14076d6f2b2cb4f250265460161d
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-01-11 14:26:25 +0000

    Remove unnecessary assignment

--- spec/service_consumers/pact_helper.rb
+++ spec/service_consumers/pact_helper.rb
@@ -408,7 +408,7 @@ Pact.provider_states_for "GDS API Adapters" do
         lock_version: 1
       )
 
-      link_set = FactoryGirl.create(:link_set,
+      FactoryGirl.create(:link_set,
         content_id: "bed722e6-db68-43e5-9079-063f623335a7",
         stale_lock_version: 3,
       )
