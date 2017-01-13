160f0fb4be7961028ba1ad4e7611cc345d0b4ac3
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-01-13 16:47:03 +0000

    Improve query performance

--- db/migrate/20170106122300_populate_documents.rb
+++ db/migrate/20170106122300_populate_documents.rb
@@ -2,8 +2,10 @@ class PopulateDocuments < ActiveRecord::Migration[5.0]
   def up
     execute "INSERT INTO documents (content_id, locale)
              SELECT content_id, locale FROM content_items
-               WHERE document_id IS NULL
-                AND (content_id, locale) NOT IN (SELECT content_id, locale FROM documents)
-               GROUP BY content_id, locale"
+             WHERE NOT EXISTS (
+               SELECT 1 FROM documents
+               WHERE documents.content_id = content_items.content_id AND documents.locale = content_items.locale
+             )
+             GROUP BY content_id, locale"
   end
 end
