3cfb8cf5c9ad030a607d39873e95a1f36f0838af
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-01-16 14:38:14 +0000

    Update timestamp on move_love_version_data
    This fixes errors that can occur from it expecting to run before
    20170106122800 which has already been deployed.
    
    This migration can be safely ran again if a developer has already run
    the previous version in their local environment.

--- db/migrate/20170116143200_move_lock_version_data.rb
+++ db/migrate/20170116143200_move_lock_version_data.rb
@@ -1,12 +1,12 @@
 class MoveLockVersionData < ActiveRecord::Migration[5.0]
   def up
     execute "UPDATE link_sets
-             SET lock_version = t.number
+             SET stale_lock_version = t.number
              FROM (SELECT target_id, number FROM lock_versions) t
              WHERE link_sets.id = t.target_id"
 
     execute "UPDATE documents
-             SET lock_version = COALESCE(t.max_number, 1)
+             SET stale_lock_version = COALESCE(t.max_number, 1)
              FROM (SELECT document_id, MAX(number) AS max_number
                    FROM content_items LEFT JOIN lock_versions
                    ON content_items.id = lock_versions.target_id
