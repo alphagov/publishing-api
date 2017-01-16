913cdbb70e55454823537ee40fbfbe3e54e431f7
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-01-16 19:25:02 +0000

    Don't reset document_id
    Without this field protected from reset it will be set to nil when a
    content item is updated.

---
+++ app/models/default_attributes.rb
@@ -0,0 +1,21 @@
+module DefaultAttributes
+  extend ActiveSupport::Concern
+
+  ATTRIBUTES_PROTECTED_FROM_RESET = [
+    :id,
+    :document_id,
+    :created_at,
+    :updated_at,
+    :first_published_at,
+    :last_edited_at,
+  ].freeze
+
+  included do
+    def assign_attributes_with_defaults(attributes)
+      new_attributes = self.class.column_defaults.symbolize_keys
+        .merge(attributes.symbolize_keys)
+        .except(*ATTRIBUTES_PROTECTED_FROM_RESET)
+      assign_attributes(new_attributes)
+    end
+  end
+end
