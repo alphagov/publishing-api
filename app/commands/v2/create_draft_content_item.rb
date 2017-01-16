8601b19faf41f79abae51803264c56a303941cf7
Douglas Roper <dougdroper@gmail.com>
2017-01-16 16:25:56 +0000

    Move logic in create draft content item
    Move the logic to determine what the draft item user facing version and
    lock version should be to the previously_published_item.
    
    Removes the logic of creating redirects since redirects can be created
    on updates too

--- app/commands/v2/create_draft_content_item.rb
+++ app/commands/v2/create_draft_content_item.rb
@@ -1,9 +1,10 @@
 module Commands
   module V2
     class CreateDraftContentItem
-      def initialize(put_content, payload)
+      def initialize(put_content, payload, previously_published_item)
         @put_content = put_content
         @payload = payload
+        @previously_published_item = previously_published_item
       end
 
       def call
@@ -14,13 +15,7 @@ module Commands
 
     private
 
-      NoPreviousPublishedItem = Class.new do
-        def user_facing_version
-          0
-        end
-      end
-
-      attr_reader :payload, :put_content
+      attr_reader :payload, :put_content, :previously_published_item
 
       def content_item
         @content_item ||= create_content_item
@@ -36,23 +31,12 @@ module Commands
         ContentItem.create!(attributes)
       end
 
-      def previously_published_item
-        @previously_published_item ||=
-          ContentItem.find_by(
-            content_id: payload.fetch(:content_id),
-            state: %w(published unpublished),
-            locale: locale,
-        ) || NoPreviousPublishedItem.new
-      end
-
       def lock_version_number_for_new_draft
-        return 1 if previously_published_item.class == NoPreviousPublishedItem
-        lock_version = LockVersion.find_by!(target: previously_published_item)
-        lock_version.number + 1
+        previously_published_item.lock_version_number
       end
 
       def user_facing_version_number_for_new_draft
-        previously_published_item.user_facing_version + 1
+        previously_published_item.user_facing_version
       end
 
       def locale
@@ -63,7 +47,7 @@ module Commands
         LockVersion.create!(target: content_item, number: lock_version_number_for_new_draft)
         ensure_link_set_exists
 
-        previously_published_item_actions
+        set_first_published_at
       end
 
       def ensure_link_set_exists
@@ -71,41 +55,12 @@ module Commands
         LockVersion.find_or_create_by!(target: link_set, number: 1)
       end
 
-      def previously_published_item_actions
-        return if previously_published_item.class == NoPreviousPublishedItem
-
-        set_first_published_at
-        previous_base_path = previously_published_item.base_path
-
-        if path_has_changed?(previous_base_path)
-          create_redirect(
-            from_path: previous_base_path,
-            to_path: payload[:base_path],
-            routes: previously_published_item.routes,
-          )
-        end
-      end
-
       def set_first_published_at
+        return unless previously_published_item.set_first_published_at?
         return if content_item.first_published_at
         content_item.update_attributes(
           first_published_at: previously_published_item.first_published_at,
         )
-      end
-
-      def create_redirect(from_path:, to_path:, routes:)
-        RedirectHelper.create_redirect(
-          publishing_app: payload[:publishing_app],
-          old_base_path: from_path,
-          new_base_path: to_path,
-          routes: routes,
-          callbacks: put_content.callbacks,
-        )
-      end
-
-      def path_has_changed?(previous_base_path)
-        return false unless put_content.content_with_base_path?
-        previous_base_path != payload[:base_path]
       end
 
       def content_item_attributes_from_payload
