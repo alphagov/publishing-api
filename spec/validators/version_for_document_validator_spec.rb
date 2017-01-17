092dba48c58b97d8a5ed81674ee1d66a387702a8
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-01-17 16:00:35 +0000

    Rename VersionForLocaleValidator for Documents
    We can focus just on the conflict between document and version rather
    than using content_id and locale fields.

--- spec/validators/version_for_document_validator_spec.rb
+++ spec/validators/version_for_document_validator_spec.rb
@@ -1,14 +1,13 @@
 require "rails_helper"
 
-RSpec.describe VersionForLocaleValidator do
+RSpec.describe VersionForDocumentValidator do
   let(:version) { 5 }
-  let(:locale) { "en" }
+  let(:document) { FactoryGirl.create(:document) }
 
   let(:content_item) do
-    FactoryGirl.create(
-      :content_item,
+    FactoryGirl.build(:content_item,
+      document: document,
       user_facing_version: version,
-      locale: locale,
     )
   end
 
@@ -19,8 +18,8 @@ RSpec.describe VersionForLocaleValidator do
       it { is_expected.to be_nil }
     end
 
-    context "when locale is nil" do
-      before { content_item.locale = nil }
+    context "when document is nil" do
+      before { content_item.document_id = nil }
       it { is_expected.to be_nil }
     end
 
@@ -29,22 +28,19 @@ RSpec.describe VersionForLocaleValidator do
       it { is_expected.to be_nil }
     end
 
-    context "when version, locale and content_id are the same" do
+    context "when version and document are the same" do
       let!(:conflict_content_item) {
-        FactoryGirl.create(
-          :content_item,
-          content_id: content_item.content_id,
-          locale: "fr",
+        FactoryGirl.create(:content_item,
+          document: document,
           user_facing_version: version,
         )
       }
       let(:expected_error) do
-        "user_facing_version=#{version} and locale=fr for content item=" +
-          "#{content_item.content_id} conflicts with content item " +
-          "id=#{conflict_content_item.id}"
+        "user_facing_version=#{version} and document=#{document.id} " +
+          "conflicts with content item id=#{conflict_content_item.id}"
       end
+
       before do
-        content_item.locale = "fr"
         validate
       end
 
