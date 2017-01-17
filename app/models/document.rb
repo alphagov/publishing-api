cb70d963fe3349d6eaa478181beb144ad1f7d339
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-01-17 13:50:56 +0000

    published_or_unpublished relation scope on Document
    This replaces the `previous` method with an ActiveRecord relation for
    the edition with a published or unpublished state. The advantage of an
    ActiveRecord relation is that it caches the result.
    
    This relation is used to get around the inconsistent situation we have
    with the substitute type of unpublishing.

--- app/models/document.rb
+++ app/models/document.rb
@@ -5,14 +5,15 @@ class Document < ApplicationRecord
   has_one :draft, -> { where(content_store: "draft") }, class_name: ContentItem
   has_one :live, -> { where(content_store: "live") }, class_name: ContentItem
 
+  # Due to the scenario of unpublished type substitute we need a scope that
+  # can access published / unpublished which isn't tied to live content store
+  # FIXME - we should make this go away
+  has_one :published_or_unpublished, -> { where(state: %w(published unpublished)) }, class_name: ContentItem
+
   validates :content_id, presence: true, uuid: true
 
   validates :locale, inclusion: {
     in: I18n.available_locales.map(&:to_s),
     message: 'must be a supported locale'
   }
-
-  def previous
-    content_items.find_by(state: %w(published unpublished))
-  end
 end
