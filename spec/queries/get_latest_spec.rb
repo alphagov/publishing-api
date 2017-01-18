df587fdac883ab8dcce3faaa0554313bfd60bf54
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-01-18 20:09:57 +0000

    Use documents in GetLatest spec

--- spec/queries/get_latest_spec.rb
+++ spec/queries/get_latest_spec.rb
@@ -1,20 +1,23 @@
 require "rails_helper"
 
 RSpec.describe Queries::GetLatest do
-  let(:a) { SecureRandom.uuid }
-  let(:b) { SecureRandom.uuid }
-  let(:c) { SecureRandom.uuid }
+  let(:document_a) { FactoryGirl.create(:document) }
+  let(:document_b) { FactoryGirl.create(:document) }
+  let(:document_b_fr) do
+    FactoryGirl.create(:document, content_id: document_b.content_id, locale: "fr")
+  end
+  let(:document_c) { FactoryGirl.create(:document) }
 
   before do
-    FactoryGirl.create(:live_content_item, content_id: a, user_facing_version: 2, base_path: "/a2", state: "published")
-    FactoryGirl.create(:superseded_content_item, content_id: a, user_facing_version: 1, base_path: "/a1", state: "superseded")
-    FactoryGirl.create(:content_item, content_id: a, user_facing_version: 3, base_path: "/a3")
+    FactoryGirl.create(:live_content_item, document: document_a, user_facing_version: 2, base_path: "/a2")
+    FactoryGirl.create(:superseded_content_item, document: document_a, user_facing_version: 1, base_path: "/a1")
+    FactoryGirl.create(:draft_content_item, document: document_a, user_facing_version: 3, base_path: "/a3")
 
-    FactoryGirl.create(:live_content_item, content_id: b, user_facing_version: 1, base_path: "/b1", state: "published")
-    FactoryGirl.create(:content_item, content_id: b, user_facing_version: 2, locale: "fr", base_path: "/b2", state: "published")
+    FactoryGirl.create(:live_content_item, document: document_b, user_facing_version: 1, base_path: "/b1")
+    FactoryGirl.create(:live_content_item, document: document_b_fr, user_facing_version: 2, base_path: "/b2")
 
-    FactoryGirl.create(:live_content_item, content_id: c, user_facing_version: 1, base_path: "/c1", state: "published")
-    FactoryGirl.create(:content_item, content_id: c, user_facing_version: 2, base_path: "/c2", state: "draft")
+    FactoryGirl.create(:live_content_item, document: document_c, user_facing_version: 1, base_path: "/c1")
+    FactoryGirl.create(:draft_content_item, document: document_c, user_facing_version: 2, base_path: "/c2")
   end
 
   def base_paths(result)
@@ -26,7 +29,8 @@ RSpec.describe Queries::GetLatest do
     result = subject.call(scope)
     expect(base_paths(result)).to match_array(["/a3", "/b1", "/b2", "/c2"])
 
-    scope = scope.joins(:document).where('documents.content_id': [a, b])
+    scope = scope.joins(:document)
+      .where('documents.content_id': [document_a.content_id, document_b.content_id])
     result = subject.call(scope)
     expect(base_paths(result)).to match_array(["/a3", "/b1", "/b2"])
 
