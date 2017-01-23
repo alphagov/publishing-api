135890f015fd81273760e36b2e4346cdcfc6dc12
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-01-23 19:23:37 +0000

    Monkey patch ActiveRecord to kill ContentItem class
    These monkey patches are used so that we can continue using a
    polymorphic association on LockVersion to ContentItem/Edition without
    changing what is saved in the database currently - namely a
    `target_type` of "ContentItem".
    
    This allows us to swap from this branch to the currently released
    version without pain.
    
    This patching should live for a very short period of time as we will not
    need to maintain backwards compatibility once this is deployed and we
    also intend to drop the lock_version table.

--- app/models/content_item.rb
+++
@@ -1,4 +0,0 @@
-# Needed to work around the fact that we use a polymorphic type and to avoid
-# including a migration which changed the class name in the database, we simply
-# make sure it is still available under its old name.
-ContentItem = Edition
