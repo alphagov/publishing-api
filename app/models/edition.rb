e80c53b46bbafc8803f6342ea24d06aafedb9bee
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-01-23 08:28:59 +0000

    Move ContentItem definition into own file

--- app/models/edition.rb
+++ app/models/edition.rb
@@ -270,7 +270,3 @@ private
     renderable_content? && document_type != "contact"
   end
 end
-
-# Needed to work around the fact that we use a polymorphic type and to avoid including a migration which changed
-# the class name in the database, we simply make sure it is still available under its old name.
-ContentItem = Edition
