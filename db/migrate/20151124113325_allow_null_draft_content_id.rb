971a99a6498323c0d5fcd5900f60b2c083fa7e21
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-01-25 14:15:30 +0000

    Fix typo
    This migration fails due to the previous migration that created the
    foreign key referring to "draft_content_item" rather than
    "draft_content_items".

--- db/migrate/20151124113325_allow_null_draft_content_id.rb
+++ db/migrate/20151124113325_allow_null_draft_content_id.rb
@@ -1,6 +1,6 @@
 class AllowNullDraftContentId < ActiveRecord::Migration
   def change
-    remove_foreign_key :live_content_items, :draft_content_item
+    remove_foreign_key :live_content_items, :draft_content_items
     change_column_null :live_content_items, :draft_content_item_id, true
   end
 end
