540ee5918bc7abde0736b6947195b77744f878d1
Mat Moore <mat.moore@digital.cabinet-office.gov.uk>
2017-02-07 15:59:54 +0000

    Delete all the taxons links
    We're going to do a fresh import of the tagging from the finding things tagathon.
    https://trello.com/c/O1LI5ctS/472-clear-tag-associations-in-content-tagger
    
    See also eec6e7b06da223348e31b26179462fc165000d79

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131092110) do
+ActiveRecord::Schema.define(version: 20170207155436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
