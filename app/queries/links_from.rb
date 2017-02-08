d4499e239af76508f7030e75b77c0a5d630dde71
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-02-08 15:55:56 +0000

    Update LinksFrom for edition links

--- app/queries/links_from.rb
+++ app/queries/links_from.rb
@@ -1,12 +1,21 @@
 module Queries
   class LinksFrom
-    def self.call(content_id, allowed_link_types: nil, parent_content_ids: [])
+    def self.call(content_id, with_drafts:, allowed_link_types: nil, parent_content_ids: [])
       return {} if allowed_link_types && allowed_link_types.empty?
-      where = { "link_sets.content_id": content_id }
-      where[:link_type] = allowed_link_types if allowed_link_types
+
       links = Link
-        .joins(:link_set)
-        .where(where)
+        .left_outer_joins(:link_set)
+        .left_outer_joins(edition: :document)
+
+      links = links
+        .where("link_sets.content_id = ? OR documents.content_id = ?",
+               content_id, content_id)
+
+      # links = links.where(editions: { content_store: "live" }) unless with_drafts
+
+      links = links.where(link_type: allowed_link_types) if allowed_link_types
+
+      links = links
         .where.not(target_content_id: parent_content_ids)
         .order(link_type: :asc, position: :asc)
         .pluck(:link_type, :target_content_id)
