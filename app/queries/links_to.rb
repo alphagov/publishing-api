cd4222ecee7a2871f52745b124c9ce00a1d2d6d2
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-02-09 09:24:11 +0000

    Update LinksTo query to support edition-level links

--- app/queries/links_to.rb
+++ app/queries/links_to.rb
@@ -2,14 +2,18 @@ module Queries
   class LinksTo
     def self.call(content_id, allowed_link_types: nil, parent_content_ids: [])
       return {} if allowed_link_types && allowed_link_types.empty?
-      where = { "links.target_content_id": content_id }
-      where[:link_type] = allowed_link_types if allowed_link_types
+
       links = Link
-        .joins(:link_set)
-        .where(where)
+        .left_outer_joins(:link_set)
+        .left_outer_joins(edition: :document)
+        .where(target_content_id: content_id)
+
+      links = links.where(link_type: allowed_link_types) if allowed_link_types
+
+      links = links
         .where.not(target_content_id: parent_content_ids)
         .order(link_type: :asc, position: :asc)
-        .pluck(:link_type, :content_id)
+        .pluck(:link_type, "COALESCE(link_sets.content_id, documents.content_id)")
 
       grouped = links
         .group_by(&:first)
