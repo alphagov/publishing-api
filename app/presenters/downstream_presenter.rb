4cfe86b8255e9c0260109ff52c4b451290b88aa4
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-02-10 07:47:11 +0000

    Remove web content item deprecation notice

--- app/presenters/downstream_presenter.rb
+++ app/presenters/downstream_presenter.rb
@@ -4,11 +4,6 @@ module Presenters
 
     def self.present(web_content_item, draft: false)
       return {} unless web_content_item
-      if web_content_item.is_a?(Edition)
-        # TODO: Add deprecation notice here once we start to migrate other parts of
-        # the app to use WebContentItem. Adding a notice now would be too noisy
-        web_content_item = Edition.find(web_content_item.id)
-      end
 
       new(web_content_item, nil, draft: draft).present
     end
