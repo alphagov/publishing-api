62c9d3d247a819bfc553f97b483ae573d3a2348a
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-02-13 10:23:39 +0000

    Take unpublishings into account when checking inconsistencies

--- lib/content_consistency_checker.rb
+++ lib/content_consistency_checker.rb
@@ -39,22 +39,30 @@ private
   def check_edition(prefix, edition, content_store)
     return unless edition.base_path
 
-    content_item = item_from_content_store(edition.base_path, content_store)
+    content_item = item_from_content_store(edition.base_path, content_store, prefix)
+    return if content_item.nil?
 
-    if edition.gone? && content_item
-      errors << "#{prefix} content exists in the content store."
-    elsif content_item.nil?
-      errors << "#{prefix} content is missing from the content store."
-      return
-    end
+    unpublishing = edition.unpublishing
 
-    fields = [:rendering_app, :publishing_app, :schema_name, :document_type]
-    fields.each do |field|
-      edition_value = edition.send(field)
-      content_item_value = content_item[field.to_s]
+    if unpublishing && unpublishing.redirect?
+      if content_item["document_type"] != "redirect" ||
+          content_item["schema_name"] != "redirect"
+        errors << "#{prefix} content is not a redirect in the content store."
+      end
+    elsif unpublishing && unpublishing.gone?
+      if content_item["document_type"] != "gone" ||
+          content_item["schema_name"] != "gone"
+        errors << "#{prefix} content is not gone in the content store."
+      end
+    else
+      fields = [:rendering_app, :publishing_app, :schema_name, :document_type]
+      fields.each do |field|
+        edition_value = edition.send(field)
+        content_item_value = content_item[field.to_s]
 
-      if edition_value != content_item_value
-        errors << "#{prefix} edition #{field} (#{edition_value}) does not match content store (#{content_item_value})."
+        if edition_value != content_item_value
+          errors << "#{prefix} edition #{field} (#{edition_value}) does not match content store (#{content_item_value})."
+        end
       end
     end
   end
