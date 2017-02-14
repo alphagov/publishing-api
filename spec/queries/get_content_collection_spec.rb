9837fe577c756938309497b9f76565758a758dd3
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-02-14 11:54:14 +0000

    Make quote usage more consistent

--- spec/queries/get_content_collection_spec.rb
+++ spec/queries/get_content_collection_spec.rb
@@ -99,7 +99,7 @@ RSpec.describe Queries::GetContentCollection do
 
       expect(
         Queries::GetContentCollection.new(
-          document_types: 'topic',
+          document_types: "topic",
           fields: %w(base_path publication_state unpublishing),
         ).call
       ).to match_array(
@@ -431,7 +431,7 @@ RSpec.describe Queries::GetContentCollection do
     context "search in" do
       context "with a single nested field" do
         let(:search_in) { ["details.body"] }
-        let(:search_query) { 'doors' }
+        let(:search_query) { "doors" }
         it "finds the edition" do
           expect(subject.call.map(&:to_hash)).to eq([{ "base_path" => "/baz" }])
         end
@@ -439,7 +439,7 @@ RSpec.describe Queries::GetContentCollection do
 
       context "with multiple nested fields" do
         let(:search_in) { ["details.body", "details.internal_name"] }
-        let(:search_query) { 'newtopic' }
+        let(:search_query) { "newtopic" }
         it "finds the edition" do
           expect(subject.call.map(&:to_hash)).to eq([{ "base_path" => "/bar/foo" }])
         end
@@ -447,7 +447,7 @@ RSpec.describe Queries::GetContentCollection do
 
       context "with a mixture of nested and non-nested fields" do
         let(:search_in) { ["title", "details.internal_name"] }
-        let(:search_query) { 'baz' }
+        let(:search_query) { "baz" }
         it "finds the edition" do
           expect(subject.call.map(&:to_hash)).to eq([{ "base_path" => "/bar/foo" }, { "base_path" => "/baz" }])
         end
@@ -455,7 +455,7 @@ RSpec.describe Queries::GetContentCollection do
 
       context "with invalid top-level fields" do
         let(:search_in) { ["nonexistent_field"] }
-        let(:search_query) { 'baz' }
+        let(:search_query) { "baz" }
         it "raises a CommandError" do
           expect { subject.call }.to raise_error(CommandError)
         end
@@ -479,14 +479,14 @@ RSpec.describe Queries::GetContentCollection do
 
       context "with fields nested more than one level deep" do
         let(:search_in) { ["details.foo.bar"] }
-        let(:search_query) { 'baz' }
+        let(:search_query) { "baz" }
         it "raises a CommandError" do
           expect { subject.call }.to raise_error(CommandError)
         end
 
         context "with SQL injection in nested fields" do
           let(:search_in) { ["details.foo' = '') OR 1=1--"] }
-          let(:search_query) { 'baz' }
+          let(:search_query) { "baz" }
           it "returns an empty result" do
             expect(subject.call.to_a).to eq([])
           end
