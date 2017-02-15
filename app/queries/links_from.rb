3c756727d54e6ba7e1ef646d95d1bc255cb7b4ac
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-02-15 10:44:27 +0000

    Fix draft/live edition-level links

--- app/queries/links_from.rb
+++ app/queries/links_from.rb
@@ -1,6 +1,6 @@
 module Queries
   class LinksFrom
-    def self.call(content_id, with_drafts:, locales:, allowed_link_types: nil, parent_content_ids: [])
+    def self.call(content_id, with_drafts:, locale:, allowed_link_types: nil, parent_content_ids: [])
       return {} if allowed_link_types && allowed_link_types.empty?
 
       links = Link
@@ -11,17 +11,36 @@ module Queries
         .where("link_sets.content_id = ? OR documents.content_id = ?",
                content_id, content_id)
 
+      # here we query on state rather than content_store because of the join
+      # which means that superseeded editions and documents would both have a
+      # content_store of NULL
+      if with_drafts
+        has_draft = "EXISTS (SELECT 1
+                             FROM editions AS e
+                             WHERE content_store = 'draft'
+                               AND e.document_id = documents.id)"
+        links = links.where("editions.state IS NULL
+                             OR CASE
+                               WHEN #{has_draft}
+                                 THEN editions.state = 'draft'
+                               ELSE editions.state IN ('published', 'unpublished')
+                             END")
+       else
+         links = links.where(editions: { state: [nil, "unpublished", "published"] })
+       end
+
+      if locale.nil?
+        links = links.where(documents: { locale: nil })
+      else
+        links = links.where(documents: { locale: [nil, locale] })
+      end
+
       links = links.where(link_type: allowed_link_types) if allowed_link_types
 
       links = links
         .where.not(target_content_id: parent_content_ids + [content_id])
         .order(link_type: :asc, position: :asc)
-        .pluck(:link_type, :target_content_id, :content_store, :locale)
-
-      # these checks have to happen outside of the SQL as the queries only
-      # apply to edition-level links
-      links.select! { |item| item[2] != "draft" } unless with_drafts
-      links.select! { |item| (locales + [nil]).include?(item[3]) } if locales.present?
+        .pluck(:link_type, :target_content_id)
 
       grouped = links
         .group_by(&:first)
--- app/queries/links_to.rb
+++ app/queries/links_to.rb
@@ -1,6 +1,6 @@
 module Queries
   class LinksTo
-    def self.call(content_id, with_drafts:, allowed_link_types: nil, parent_content_ids: [])
+    def self.call(content_id, with_drafts:, locale:, allowed_link_types: nil, parent_content_ids: [])
       return {} if allowed_link_types && allowed_link_types.empty?
 
       links = Link
@@ -8,14 +8,37 @@ module Queries
         .left_outer_joins(edition: :document)
         .where(target_content_id: content_id)
 
+      # here we query on state rather than content_store because of the join
+      # which means that superseeded editions and documents would both have a
+      # content_store of NULL
+      if with_drafts
+        has_draft = "EXISTS (SELECT 1
+                             FROM editions AS e
+                             WHERE content_store = 'draft'
+                               AND e.document_id = documents.id)"
+        links = links.where("editions.state IS NULL
+                             OR CASE
+                               WHEN #{has_draft}
+                                 THEN editions.state = 'draft'
+                               ELSE editions.state IN ('published', 'unpublished')
+                             END")
+      else
+        links = links.where(editions: { state: [nil, "unpublished", "published"] })
+      end
+
+      if locale.nil?
+        links = links.where(documents: { locale: nil })
+      else
+        links = links.where(documents: { locale: [nil, locale] })
+      end
+
       links = links.where(link_type: allowed_link_types) if allowed_link_types
 
       links = links
         .where.not(target_content_id: parent_content_ids)
         .order(link_type: :asc, position: :asc)
-        .pluck(:link_type, "COALESCE(link_sets.content_id, documents.content_id)", :content_store)
-
-      links.select! { |item| item.last != "draft" } unless with_drafts
+        .pluck(:link_type,
+               "COALESCE(link_sets.content_id, documents.content_id)")
 
       grouped = links
         .group_by(&:first)
