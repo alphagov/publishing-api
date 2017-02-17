e86b8c86abc79c439babd1dc4e7d36b0461b9bd3
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-02-17 11:18:39 +0000

    Update Link Expansion documentation for Edition Links
    Also includes information on common problems and debugging.

--- lib/link_expansion.rb
+++ lib/link_expansion.rb
@@ -1,3 +1,9 @@
+#
+# This is the core class of Link Expansion which is a complicated concept
+# in the Publishing API
+#
+# The concept is documented in /doc/link-expansion.md
+#
 class LinkExpansion
   attr_reader :content_id, :locale, :with_drafts
 
