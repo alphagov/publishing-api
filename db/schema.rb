ed5325fa5d8921c0d7193b43e136e4847ee81258
Graham Pengelly <graham@three-tier.com>
2017-02-27 17:21:23 +0000

    Delete some corrupt content items
    This commit adds a migration to delete two content items that have
    become corrupt leading to Whitehall not being able to republish them.
    
    The associated Whitehall documents will be republished once this
    migration has run.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223160956) do
+ActiveRecord::Schema.define(version: 20170227130411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
