42e741a7e10e76b8aa161aaad4270d0f3b6328b9
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-03-02 15:06:19 +0000

    Refactor BasePathForState query

--- app/queries/base_path_for_state.rb
+++ app/queries/base_path_for_state.rb
@@ -1,41 +1,49 @@
 module Queries
   module BasePathForState
-    extend ArelHelpers
+    extend self
 
-    def self.conflict(edition_id, state, base_path)
-      return if state == "superseded"
-      return if state == "unpublished" && Unpublishing.is_substitute?(edition_id)
+    def conflict(edition_id, state, base_path)
+      return if has_no_conflicts?(state, edition_id)
 
-      documents_table = Document.arel_table
-      editions_table = Edition.arel_table
-      unpublishings_table = Unpublishing.arel_table
-
-      allowed_states = state == "draft" ? %w(draft) : %w(published unpublished)
-
-      scope = editions_table
-        .project(
-          editions_table[:id],
-          documents_table[:content_id],
-          documents_table[:locale],
+      id, content_id, locale = find_conflict(
+        limit_scope_to_unpublishing(
+          edition_scope(edition_id, state, base_path), state
         )
-        .where(editions_table[:id].not_eq(edition_id))
-        .where(editions_table[:state].in(allowed_states))
-        .where(editions_table[:base_path].eq(base_path))
-        .join(documents_table).on(documents_table[:id].eq(editions_table[:document_id]))
+      )
 
-      if %w(published unpublished).include?(state)
-        unpublished_state = editions_table[:state].eq("unpublished")
-        editions_join = editions_table[:id].eq(unpublishings_table[:edition_id])
-        nil_unpublishing = unpublishings_table[:type].eq(nil)
-        non_substitute = unpublishings_table[:type].not_eq("substitute")
+      { id: id, content_id: content_id, locale: locale } if id
+    end
 
-        scope = scope.outer_join(unpublishings_table)
-          .on(unpublished_state.and(editions_join))
-          .where(nil_unpublishing.or(non_substitute))
-      end
+    def find_conflict(scope)
+      scope
+        .order(created_at: :desc)
+        .pluck("editions.id", :content_id, :locale)
+        .first
+    end
 
-      scope = scope.order(editions_table[:created_at].desc).take(1)
-      get_rows(scope).first.try(:symbolize_keys)
+    def has_no_conflicts?(state, edition_id)
+      return true if state == "superseded"
+      return true if state == "unpublished" && Unpublishing.is_substitute?(edition_id)
+      false
+    end
+
+    def edition_scope(edition_id, state, base_path)
+      Edition
+        .with_document
+        .where.not(id: edition_id)
+        .where(state: allowed_states(state))
+        .where(base_path: base_path)
+    end
+
+    def allowed_states(state)
+      state == "draft" ? %w(draft) : %w(published unpublished)
+    end
+
+    def limit_scope_to_unpublishing(scope, state)
+      return scope unless %w(published unpublished).include?(state)
+      scope
+        .with_unpublishing
+        .where("unpublishings.type IS NULL OR unpublishings.type != 'substitute'")
     end
   end
 end
