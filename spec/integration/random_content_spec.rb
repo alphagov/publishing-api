1a55f8e68afa44908be54b55d2f4cb14bc186613
Tijmen Brommet <tijmen@gmail.com>
2017-03-08 18:29:03 +0000

    Explicitly test the API with random content
    This does 50 iterations with random content. This should flush out any
    issues.
    
    The number 50 is a bit arbitrary. It takes about 20 seconds, so doesn't
    slow down the tests too much.

---
+++ spec/integration/random_content_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "Randomised content" do
+  include RandomContentHelpers
+
+  50.times do |i|
+    it "it can publish randomly generated content #{i+1}/50" do
+      base_path = "/#{SecureRandom.hex}"
+      stub_content_store_calls(base_path)
+      edition = generate_random_edition(base_path)
+
+      put "/v2/content/#{content_id}", params: edition.to_json
+
+      expect(response).to be_ok, random_content_failure_message(response, edition)
+
+      post "/v2/content/#{content_id}/publish", params: { locale: edition["locale"], update_type: "major" }.to_json
+
+      expect(response).to be_ok, random_content_failure_message(response, edition)
+    end
+  end
+
+  def stub_content_store_calls(base_path)
+    stub_request(:put, "http://draft-content-store.dev.gov.uk/content#{base_path}")
+      .to_return(status: 200)
+    stub_request(:put, "http://content-store.dev.gov.uk/content#{base_path}")
+      .to_return(status: 200)
+  end
+end
