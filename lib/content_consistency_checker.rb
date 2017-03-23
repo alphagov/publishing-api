d4ed6e47b255531ccc559e639cf5eaec6c82bd71
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-03-23 14:09:22 +0000

    Perform consistency check based on content dump

--- lib/content_consistency_checker.rb
+++ lib/content_consistency_checker.rb
@@ -3,93 +3,127 @@ require 'gds_api/content_store'
 class ContentConsistencyChecker
   attr_reader :errors
 
-  def initialize(ignore_recent = false)
-    @ignore_recent = ignore_recent
-  end
-
-  def call(content_id, locale)
-    @content_id = content_id
-    @locale = locale
-    @errors = []
-
-    unless document
-      errors << "Document (#{content_id}, #{locale}) could not be found."
-      return errors
+  ContentItem = Struct.new(:base_path,
+                           :content_id,
+                           :locale,
+                           :document_type,
+                           :schema_name,
+                           :rendering_app,
+                           :publishing_app,
+                           :updated_at) do
+    def gone?
+      schema_name == "gone" && document_type == "gone"
     end
 
-    check_edition("Draft", draft, draft_content_store) if draft
-    check_edition("Live", live, live_content_store) if live
+    def redirect?
+      schema_name == "redirect" && document_type == "redirect"
+    end
+  end
 
-    errors
+  def initialize(content_store, content_dump)
+    @content_store = content_store
+    @content_dump = load_content_dump(content_dump)
+    @remaining_content = Set.new(@content_dump.keys)
+    @errors = Hash.new { |hash, key| hash[key] = [] }
+  end
+
+  def check_editions
+    editions_to_check.limit(10000).find_each do |edition|
+      check_edition(edition)
+    end
+  end
+
+  def check_content
+    @remaining_content.each do |path|
+      content_item = @content_dump.fetch(path)
+      next if content_item["content_id"].nil?
+      next if content_item["schema_name"] == "gone"
+      next if content_item["schema_name"].nil? || content_item["schema_name"].empty?
+
+      edition = Edition.find_by(
+        content_store: content_store,
+        base_path: content_item["base_path"]
+      )
+      next if edition
+
+      @errors[content_item["base_path"]] << "No edition available."
+    end
   end
 
 private
 
-  attr_reader :content_id, :locale, :ignore_recent
+  attr_reader :content_store
 
-  def check_edition(prefix, edition, content_store)
+  def editions_to_check
+    Edition
+      .with_document
+      .where(content_store: content_store)
+  end
+
+  def load_content_dump(filename)
+    content_dump = {}
+
+    Zlib::GzipReader.open(filename) do |file|
+      csv = CSV.new(file)
+      keys = csv.gets
+      csv.each do |row|
+        content_item_hash = Hash[keys.zip(row)].symbolize_keys
+        content_item = ContentItem.new(*content_item_hash.values_at(*ContentItem.members))
+        content_item.updated_at = Time.parse(content_item.updated_at)
+        base_path = content_item.base_path
+        content_dump[base_path.to_sym] = content_item
+      end
+    end
+
+    content_dump
+  end
+
+  def get_content_item(path)
+    begin
+      path = path.to_sym
+      content_item = @content_dump.fetch(path)
+      @remaining_content.delete?(path)
+      content_item
+    rescue KeyError
+      nil
+    end
+  end
+
+  def check_edition(edition)
     return unless edition.base_path
-
-    return if ignore_recent && edition.updated_at < 1.day.ago
 
     path = edition.base_path
 
-    begin
-      content_item = content_store.content_item(path).parsed_content
-    rescue GdsApi::HTTPForbidden
-      # nothing
-    rescue GdsApi::ContentStore::ItemNotFound
-      errors << "#{prefix} #{path} content is missing from the content store."
-    rescue GdsApi::HTTPGone
-      if edition.gone?
-        return
-      else
-        errors << "#{prefix} #{path} is gone in the content store."
+    content_item = get_content_item(edition.base_path)
+
+    unless content_item
+      unless edition.gone?
+        @errors[path] << "Content is missing from the content store."
       end
+
+      return
     end
 
-    return if content_item.nil?
+    return if edition.updated_at > content_item.updated_at
 
     if edition.redirect?
-      if content_item["document_type"] != "redirect" ||
-          content_item["schema_name"] != "redirect"
-        errors << "#{prefix} content is not a redirect in the content store."
+      unless content_item.redirect?
+        @errors[path] << "Content is not a redirect in the content store."
       end
     elsif edition.gone?
-      if content_item["document_type"] != "gone" ||
-          content_item["schema_name"] != "gone"
-        errors << "#{prefix} content is not gone in the content store."
+      unless content_item.gone?
+        @errors[path] << "Content is not gone in the content store."
       end
     else
-      fields = [:rendering_app, :publishing_app, :schema_name, :document_type]
+      fields = [:content_id, :locale, :rendering_app, :publishing_app, :schema_name, :document_type]
       fields.each do |field|
         edition_value = edition.send(field)
-        content_item_value = content_item[field.to_s]
+        content_item_value = content_item.send(field)
 
         if edition_value != content_item_value
-          errors << "#{prefix} edition #{field} (#{edition_value}) does not match content store (#{content_item_value})."
+          @errors[path] << "Edition #{field} (#{edition_value}) does not match content store (#{content_item_value})."
         end
       end
     end
-  end
-
-  def live
-    document.live
-  end
-
-  def draft
-    document.draft
-  end
-
-  def document
-    @document ||= Document.find_by(content_id: content_id, locale: locale)
-  end
-
-  def live_content_store
-    GdsApi::ContentStore.new(Plek.find("content-store"))
-  end
-
-  def draft_content_store
-    GdsApi::ContentStore.new(Plek.find("draft-content-store"))
   end
 end
--- lib/tasks/check_content_consistency.rake
+++ lib/tasks/check_content_consistency.rake
@@ -1,21 +1,29 @@
-namespace :check_content_consistency do
-def check_content(checker, ignore_recent)
-  errors = checker.call(content_id, locale)
+require "content_consistency_checker"
 
-  if errors.any?
-    puts "#{content_id} #{locale} 😱"
-    puts errors
+def report_errors(errors, content_store)
+  Airbrake.notify(
+    "Inconsistent #{content_store} documents",
+    parameters: {
+      errors: errors,
+    }
+  )
+
+  errors.each do |base_path, item_errors|
+    puts "#{base_path} 😱"
+    puts item_errors
   end
-
-  errors.none?
 end
 
 desc "Check all the documents for consistency with the router-api and content-store"
-task :check_content_consistency, [:ignore_recent] => [:environment] do |_, args|
-  checker = ContentConsistencyChecker.new(args.fetch(:ignore_recent, false) == "true")
-  documents = Document.pluck(:content_id, :locale)
-  failures = documents.reject do |content_id, locale|
-    check_content(checker, content_id, locale)
-  end
-  puts "Results: #{failures.count} failures out of #{documents.count}."
+task :check_content_consistency, [:content_store, :content_dump] => [:environment] do |_, args|
+  raise "Missing content store." unless args[:content_store]
+  raise "Missing content dump." unless args[:content_dump]
+  raise "Invalid content store." unless ["live", "draft"].include?(args[:content_store])
+
+  checker = ContentConsistencyChecker.new(args[:content_store], args[:content_dump])
+  checker.check_editions
+  checker.check_content
+
+  errors = checker.errors
+  report_errors(errors, args[:content_store]) if errors.any?
 end
