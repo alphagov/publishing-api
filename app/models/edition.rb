eae735af75c956dd2ea88d995747412c7ca2370d
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2017-04-20 07:58:17 +0000

    Stop validating description as WellFormedContentType
    Edition `description` is no longer JSON, it is now a string.  Therefore we no
    longer need it to be validated through the WellFormedContentTypesValidator.

--- app/models/edition.rb
+++ app/models/edition.rb
@@ -57,7 +57,6 @@ class Edition < ApplicationRecord
     in: %w(alpha beta live),
     message: 'must be either alpha, beta, or live'
   }
-  validates :description, well_formed_content_types: { must_include: "text/html" }
   validates :details, well_formed_content_types: { must_include_one_of: %w(text/html text/govspeak) }
 
   validate :user_facing_version_must_increase
--- spec/support/well_formed_content_types_validator.rb
+++ spec/support/well_formed_content_types_validator.rb
@@ -1,33 +1,5 @@
 RSpec.shared_examples_for WellFormedContentTypesValidator do
   describe "validations" do
-    describe "description" do
-      it "is valid when the content types are well-formed" do
-        subject.description = [
-          { content_type: "text/html", content: "<p>content</p>" },
-          { content_type: "text/plain", content: "content" },
-        ]
-
-        expect(subject).to be_valid
-      end
-
-      it "is invalid when the content types are not well-formed" do
-        subject.description = [
-          { content_type: "text/html" },
-          { content_type: "text/plain", content: "content" },
-        ]
-
-        expect(subject).to be_invalid
-      end
-
-      it "is invalid when the text/html content type is not included" do
-        subject.description = [
-          { content_type: "text/plain", content: "content" },
-        ]
-
-        expect(subject).to be_invalid
-      end
-    end
-
     describe "details" do
       it "is valid when the content types are well-formed" do
         subject.details = {
