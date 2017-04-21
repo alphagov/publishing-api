c98ba7e8620482f251f082be2dfbce67505c9dc4
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-04-21 15:26:51 +0100

    Class for a distributed lock
    This class is to set a lock to ensure that the same code cannot be
    executed at the same time, even when the application is running on
    multiple machines.

---
+++ lib/distributed_lock.rb
@@ -0,0 +1,35 @@
+class DistributedLock
+  attr_reader :name, :timeout_seconds, :has_run
+
+  def self.lock(name, timeout_seconds: 30, &block)
+    self.new(name, timeout_seconds: timeout_seconds).run(&block)
+  end
+
+  def initialize(name, timeout_seconds:)
+    @name = name
+    @timeout_seconds = timeout_seconds
+    @has_run = false
+  end
+
+  def run
+    @has_run = false
+    # Run this inside a transaction so lock can be automatically released
+    # when tranasction completes - seems a less deadlock prone situation than
+    # creating a lock and the releasing it later
+    result = ActiveRecord::Base.transaction do
+      ActiveRecord::Base.with_advisory_lock(
+        name,
+        timeout_seconds: timeout_seconds,
+        transaction: true
+      ) do
+        @has_run = true
+        yield
+      end
+    end
+
+    raise FailedToAcquireLock unless @has_run
+    result
+  end
+
+  class FailedToAcquireLock < StandardError; end
+end
