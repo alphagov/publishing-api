fcee612b995bfc4cbebf52cd299a3b1c85f74bc2
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-05-04 09:07:01 +0100

    Add a migration which fixes a nil public_updated_at
    This edition ended up with a nil public_updated_at which needs
    fixing.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503130640) do
+ActiveRecord::Schema.define(version: 20170504075801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
