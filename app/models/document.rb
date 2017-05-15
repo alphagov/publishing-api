9e63a300f06d413e8716c19111e072f429ce3cff
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2017-05-15 13:03:36 +0000

    Use string for class_name in has_one.
    Passing a class is deprecated.

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,13 +2,13 @@ class Document < ApplicationRecord
   include FindOrCreateLocked
 
   has_many :editions
-  has_one :draft, -> { where(content_store: "draft") }, class_name: Edition
-  has_one :live, -> { where(content_store: "live") }, class_name: Edition
+  has_one :draft, -> { where(content_store: "draft") }, class_name: 'Edition'
+  has_one :live, -> { where(content_store: "live") }, class_name: 'Edition'
 
   # Due to the scenario of unpublished type substitute we need a scope that
   # can access published / unpublished which isn't tied to live content store
   # FIXME - we should make this go away
-  has_one :published_or_unpublished, -> { where(state: %w(published unpublished)) }, class_name: Edition
+  has_one :published_or_unpublished, -> { where(state: %w(published unpublished)) }, class_name: 'Edition'
 
   validates :content_id, presence: true, uuid: true
 
