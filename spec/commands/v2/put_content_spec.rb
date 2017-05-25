941aed31f2e7960d3fd97494903d6441529aeeee
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2017-05-25 10:20:50 +0000

    Move pathless content tests to spec/integration
    These scenarios aren't really unit tests so create an integration test
    concerning pathless content.

--- spec/commands/v2/put_content_spec.rb
+++ spec/commands/v2/put_content_spec.rb
@@ -31,20 +31,6 @@ RSpec.describe Commands::V2::PutContent do
       }
     end
 
-    let(:pathless_payload) do
-      {
-        content_id: content_id,
-        title: "Some Title",
-        publishing_app: "publisher",
-        rendering_app: "frontend",
-        document_type: "contact",
-        details: { title: "Contact Title" },
-        schema_name: "contact",
-        locale: locale,
-        phase: "beta",
-      }
-    end
-
     it "validates the payload" do
       validator = double(:validator)
       expect(Commands::V2::PutContentValidator).to receive(:new)
@@ -677,50 +663,6 @@ RSpec.describe Commands::V2::PutContent do
       end
     end
 
-    context "without a base_path" do
-      before do
-        payload.delete(:base_path)
-      end
-
-      context "when schema requires a base_path" do
-        it "raises an error" do
-          expect {
-            described_class.call(payload)
-          }.to raise_error(CommandError, /Base path is not a valid absolute URL path/)
-        end
-      end
-
-      context "when schema does not require a base_path" do
-        before do
-          payload.merge!(schema_name: 'government', document_type: 'government').delete(:format)
-        end
-
-        it "does not raise an error" do
-          expect {
-            described_class.call(payload)
-          }.not_to raise_error
-        end
-
-        it "does not try to reserve a path" do
-          expect {
-            described_class.call(payload)
-          }.not_to change(PathReservation, :count)
-        end
-      end
-    end
-
-    context 'without a publishing_app' do
-      before do
-        payload.delete(:publishing_app)
-      end
-
-      it "raises an error" do
-        expect {
-          described_class.call(payload)
-        }.to raise_error(CommandError, /publishing_app is required/)
-      end
-    end
-
     it_behaves_like TransactionalCommand
 
     context "when the draft does not exist" do
@@ -795,130 +737,6 @@ RSpec.describe Commands::V2::PutContent do
 
           expect(edition.last_edited_at).to eq(old_last_edited_at)
         end
-      end
-    end
-
-    context "with a pathless edition payload" do
-      let(:payload) { pathless_payload }
-
-      it "saves the content as draft" do
-        expect {
-          described_class.call(payload)
-        }.to change(Edition, :count).by(1)
-      end
-
-      it "sends to the downstream draft worker" do
-        expect(DownstreamDraftWorker).to receive(:perform_async_in_queue)
-        described_class.call(payload)
-      end
-
-      context "for an existing draft edition" do
-        let!(:draft_edition) do
-          FactoryGirl.create(:draft_edition,
-            document: FactoryGirl.create(:document, content_id: content_id),
-            title: "Old Title"
-          )
-        end
-
-        it "updates the draft" do
-          described_class.call(payload)
-          expect(draft_edition.reload.title).to eq("Some Title")
-        end
-      end
-
-      context "for an existing live edition" do
-        let!(:live_edition) do
-          FactoryGirl.create(:live_edition,
-            document: FactoryGirl.create(:document, content_id: content_id),
-            title: "Old Title"
-          )
-        end
-
-        it "creates a new draft" do
-          expect {
-            described_class.call(payload)
-          }.to change(Edition, :count).by(1)
-        end
-      end
-    end
-
-    context "where a base_path is optional and supplied" do
-      let(:payload) do
-        pathless_payload.merge(
-          base_path: base_path,
-          routes: [{ path: base_path, type: "exact" }],
-        )
-      end
-
-      it "sends to the content-store" do
-        expect(DownstreamDraftWorker).to receive(:perform_async_in_queue)
-        described_class.call(payload)
-      end
-
-      # This covers a specific edge case where the edition uniqueness validator
-      # matched anything else with the same state, locale and version because it
-      # was previously ignoring the base path, now it should return without
-      # attempting to validate for pathless formats.
-      context "with other similar pathless items" do
-        before do
-          FactoryGirl.create(:draft_edition,
-            base_path: nil,
-            schema_name: "contact",
-            document_type: "contact",
-            user_facing_version: 3,
-          )
-        end
-
-        it "doesn't conflict" do
-          expect {
-            described_class.call(payload)
-          }.not_to raise_error
-        end
-      end
-
-      context "when there's a conflicting edition" do
-        before do
-          FactoryGirl.create(:draft_edition,
-            base_path: base_path,
-            schema_name: "contact",
-            document_type: "contact",
-            user_facing_version: 3,
-          )
-        end
-
-        it "conflicts" do
-          expect {
-            described_class.call(payload)
-          }.to raise_error(CommandError, /base path=\/vat-rates conflicts/)
-        end
-      end
-    end
-
-    context "schema validation fails" do
-      let(:errors) do
-        [{ schema: "a", fragment: "b", message: "c", failed_attribute: "d" }]
-      end
-      let(:validator) do
-        instance_double(SchemaValidator, valid?: false, errors: errors)
-      end
-      before do
-        allow(SchemaValidator).to receive(:new).and_return(validator)
-      end
-
-      it "raises command error and exits" do
-        expect(PathReservation).not_to receive(:reserve_base_path!)
-        expect { described_class.call(payload) }.to raise_error { |error|
-          expect(error).to be_a(CommandError)
-          expect(error.code).to eq 422
-          expect(error.error_details).to eq errors
-        }
-      end
-    end
-
-    context "schema validation passes" do
-      it "returns success" do
-        expect(PathReservation).to receive(:reserve_base_path!)
-        expect { described_class.call(payload) }.not_to raise_error
       end
     end
 
