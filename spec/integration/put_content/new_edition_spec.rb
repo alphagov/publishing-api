7a9c78577afa18a7de479362498d1023eccf09f4
Steve Laing <steve.laing@gmail.com>
2017-05-31 11:56:44 +0100

    Use RSpec subject instead of repeatedly using Edition.last

--- spec/integration/put_content/new_edition_spec.rb
+++ spec/integration/put_content/new_edition_spec.rb
@@ -1,38 +1,33 @@
 
 RSpec.describe "PUT /v2/content when the payload is for a brand new edition" do
-  include PutContentIntegrationHelper
+  include_context "PutContent call"
 
-  let(:payload) { default_payload }
+  subject { Edition.last }
 
   it "creates an edition" do
     put "/v2/content/#{content_id}", params: payload.to_json
 
-    edition = Edition.last
-
-    expect(edition).to be_present
-    expect(edition.document.content_id).to eq(content_id)
-    expect(edition.title).to eq("Some Title")
+    expect(subject).to be_present
+    expect(subject.document.content_id).to eq(content_id)
+    expect(subject.title).to eq("Some Title")
   end
 
   it "sets a draft state for the edition" do
     put "/v2/content/#{content_id}", params: payload.to_json
-    edition = Edition.last
 
-    expect(edition.state).to eq("draft")
+    expect(subject.state).to eq("draft")
   end
 
   it "sets a user-facing version of 1 for the edition" do
     put "/v2/content/#{content_id}", params: payload.to_json
-    edition = Edition.last
 
-    expect(edition.user_facing_version).to eq(1)
+    expect(subject.user_facing_version).to eq(1)
   end
 
   it "creates a lock version for the edition" do
     put "/v2/content/#{content_id}", params: payload.to_json
-    edition = Edition.last
 
-    expect(edition.document.stale_lock_version).to eq(1)
+    expect(subject.document.stale_lock_version).to eq(1)
   end
 
   shared_examples "creates a change note" do
