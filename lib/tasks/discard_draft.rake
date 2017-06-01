2cbf60658c3c4c96ab04ddde636bd2ba6c97dcdb
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-06-01 17:15:53 +0100

    Raise an error if content-id is not provided
    Rspec only runs rake tasks once test suite, so they have to be
    "reenabled" before the tests run.

--- lib/tasks/discard_draft.rake
+++ lib/tasks/discard_draft.rake
@@ -1,5 +1,7 @@
 desc "Discard all drafts for a content item"
 task :discard_draft, [:content_id] => :environment do |_, args|
+  raise 'Missing parameter: content_id' unless args.content_id
+
   payload = { content_id: args.content_id }
 
   Commands::V2::DiscardDraft.call(payload)
--- spec/lib/tasks/discard_draft_spec.rb
+++ spec/lib/tasks/discard_draft_spec.rb
@@ -1,10 +1,18 @@
 require "rails_helper"
 
 RSpec.describe "Discard draft rake task" do
+  before do
+    Rake::Task['discard_draft'].reenable
+  end
+
   it "runs the process to discard a draft" do
     payload = { content_id: "content_id" }
     expect(Commands::V2::DiscardDraft).to receive(:call).with(payload)
 
     Rake::Task['discard_draft'].invoke("content_id")
+  end
+
+  it "raises an error if a content id is not provided" do
+    expect { Rake::Task['discard_draft'].invoke }.to raise_error('Missing parameter: content_id')
   end
 end
