3236ea1c13709bbffda5f0cb1a6804fd383031e4
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-06-13 12:46:57 +0100

    Inject links to the different content in the change notes

--- app/controllers/content_api_prototype/content_items/content_controller.rb
+++ app/controllers/content_api_prototype/content_items/content_controller.rb
@@ -31,13 +31,43 @@ module ContentApiPrototype
           .new(edition, draft: edition.draft?)
           .for_content_store(0)
 
-        resolve_text_html(presented_edition)
+        presented_edition = resolve_text_html(presented_edition)
+
+        inject_links(presented_edition, edition)
       end
 
       def resolve_text_html(presented_edition)
         resolver = ContentTypeResolver.new("text/html")
         presented_edition[:details] = resolver.resolve(presented_edition[:details])
         presented_edition[:description] = resolver.resolve(presented_edition[:description])
+        presented_edition
+      end
+
+      def user_facing_version_for(edition, position)
+        return edition.user_facing_version + 1 if position == :next
+        return edition.user_facing_version - 1 if position == :prev
+        raise "Invalid position, must be :next or :prev"
+      end
+
+      def sibling_edition(edition, position)
+        version = user_facing_version_for(edition, position)
+        edition.document.editions
+          .where(user_facing_version: version, content_store: [nil, "live"])
+          .first
+      end
+
+      def link_to_sibling(edition, position)
+        other_edition = sibling_edition(edition, position)
+        return unless other_edition
+        "/content/#{other_edition.document.content_id}/#{other_edition.document.locale}/#{other_edition.user_facing_version}"
+      end
+
+      def inject_links(presented_edition, edition)
+        presented_edition[:historical_links] = {
+          next: link_to_sibling(edition, :next),
+          prev: link_to_sibling(edition, :prev),
+        }
+
         presented_edition
       end
     end
