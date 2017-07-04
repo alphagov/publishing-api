86427e61d536e879592cce66a540b120391e6ffa
David Henry <david@decoybecoy.com>
2017-07-04 16:48:13 +0100

    Allow lookup by base path filter criteria to be specified
    In order to migrate content out of router-data we need 
    to be able to find the content ID for unpublished
    redirects. 
    
    This change allow us to remove the filtering  required 
    by mainstream applications for unpublished.type and 
    document_type fields.
    
    We have inverted the existing logic around unpublishing_type
    filters based on the fact that content must be `published`
    or `unpublished` and only `unpublished` content will
    have an unpublishing record.

--- app/controllers/lookups_controller.rb
+++ app/controllers/lookups_controller.rb
@@ -5,13 +5,18 @@ class LookupsController < ApplicationController
     states = %w(published unpublished)
     base_paths = params.fetch(:base_paths)
 
-    base_paths_and_content_ids = Edition.with_document
-      .left_outer_joins(:unpublishing)
+    scope = Edition.left_outer_joins(:unpublishing)
+
+    # where not in (..) does not return records that where the field is null
+    scope = scope.where(unpublishings: { type: nil }).or(
+      scope.where.not(unpublishings: { type: params.fetch(:exclude_unpublishing_types, %w{vanish redirect gone}) })
+    )
+
+    scope = scope.with_document
       .where(state: states, base_path: base_paths)
-      .where("state = 'published' OR unpublishings.type = 'withdrawal'")
-      .where("document_type NOT IN ('gone', 'redirect')")
-      .pluck(:base_path, 'documents.content_id')
-      .uniq
+      .where.not(document_type: params.fetch('exclude_document_types', %w{gone redirect}))
+
+    base_paths_and_content_ids = scope.distinct.pluck(:base_path, 'documents.content_id')
 
     response = Hash[base_paths_and_content_ids]
     render json: response
