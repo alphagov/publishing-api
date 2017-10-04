bb3069bb3019bf17ba5f205800d2a814b111f51f
David Henry <david@decoybecoy.com>
2017-10-04 10:41:50 +0100

    Preview of rabbit MQ message
    This is helpful when building consumers as otehrwise
    it is hard to reason about the actual message that will be received
    and setting the system up to capture the message is time consuming.
    
    This functionality has been added to the documentation.

---
+++ spec/lib/tasks/queue_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "Queue rake task" do
+  before do
+    Rake::Task['queue:preview_recent_message'].reenable
+  end
+
+  it "previews the rabbit MQ message for a document type" do
+    edition = FactoryGirl.create(:live_edition, base_path: '/ci1')
+    FactoryGirl.create(:event, id: 12)
+
+    expect_any_instance_of(Object).to receive(:pp).with(
+      hash_including(title: "VAT rates", payload_version: 12)
+    )
+    Rake::Task['queue:preview_recent_message'].invoke(edition.document_type)
+  end
+end
