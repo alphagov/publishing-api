936e78ce6b1ef5e264fdfab4fb42b17e88962cfe
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-03 18:07:05 +0000

    Fix TAP first published at dates
    This has had all the dates pulled out of TAP and put into a CSV file and
    then imported in. I added an extra entry for the index page which has
    the lowest timestamp of all the pages.
    
    Method used to extract data:
    
    ```
    irb(main):034:0> dates = Country.all.map { |c| time =
    TravelAdviceEdition.where(:country_slug => c.slug, :published_at.ne =>
    nil).sort(published_at: :asc).limit(1).pluck(:published_at).first;
    [c.content_id, c.slug, time ? time.to_datetime.rfc3339 : nil ] }.reject { |(_, _, date)| date.nil? }
    irb(main):048:0> File.open("/tmp/travel-advice-publisher.csv", "w") {
    |file| file.write(dates.map(&:to_csv).join("")) }
    ```

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030153415) do
+ActiveRecord::Schema.define(version: 20171103174319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
