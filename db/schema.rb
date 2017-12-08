8b309e51daef9d402f00651a9413798397b56d50
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-12-08 21:45:20 +0000

    Fix Specialist Publisher finder first_published_at values
    The data for this was generated by looping through the
    content_ids and pulling out the first_published at available for each
    one which always seemed to be different to the dreaded 29th February
    date
    
    ```
    content_ids.map { |cid| [cid, Edition.with_document.where("documents.content_id": cid).minimum(:first_published_at).rfc3339] }
    ```

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171208162436) do
+ActiveRecord::Schema.define(version: 20171208170032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
