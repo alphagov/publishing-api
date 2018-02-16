400f764d45c7963109d5ee519faa2d528b5aa560
Elena Tanasoiu <elena.tanasoiu@gmail.com>
2018-02-16 09:34:08 +0000

    Fixes change note assignment

--- db/migrate/20180215112954_fix_whitehall_remove_change_note.rb
+++ db/migrate/20180215112954_fix_whitehall_remove_change_note.rb
@@ -6,7 +6,7 @@ class FixWhitehallRemoveChangeNote < ActiveRecord::Migration[5.1]
     d = Document.where(content_id: "b3b96cb9-e7fa-493c-87a5-31d71d79c7a7").first
     if d.present?
       change_notes = d.editions.map(&:change_note).compact
-      change_notes.select{ |change_note| change_note.note == "Added funding allocations for individual LAs for 2018 to 2019."}
+      change_notes.select!{ |change_note| change_note.note == "Added funding allocations for individual LAs for 2018 to 2019."}
   
       # Get rid of change notes
       change_notes.map(&:destroy)
