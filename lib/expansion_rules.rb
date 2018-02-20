cd82337abef54e037d883fbd6f928f883911aa34
Tijmen Brommet <tijmen@gmail.com>
2018-02-20 16:40:34 +0000

    Always expose the "phase" of expanded taxons

--- lib/expansion_rules.rb
+++ lib/expansion_rules.rb
@@ -55,7 +55,7 @@ module ExpansionRules
     { document_type: :placeholder_topical_event,  fields: DEFAULT_FIELDS_WITH_DETAILS },
     { document_type: :organisation,               fields: DEFAULT_FIELDS_WITH_DETAILS },
     { document_type: :placeholder_organisation,   fields: DEFAULT_FIELDS_WITH_DETAILS },
-    { document_type: :taxon,                      fields: DEFAULT_FIELDS_WITH_DETAILS },
+    { document_type: :taxon,                      fields: DEFAULT_FIELDS_WITH_DETAILS + [:phase] },
     { document_type: :need,                       fields: DEFAULT_FIELDS_WITH_DETAILS },
     { document_type: :finder, link_type: :finder, fields: DEFAULT_FIELDS_WITH_DETAILS },
     { document_type: :travel_advice,              fields: DEFAULT_FIELDS + [[:details, :country], [:details, :change_description]] },
