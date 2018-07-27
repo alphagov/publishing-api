5ee1f88f56bc5bc664ca216c316707e85fbf314c
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2018-07-27 14:11:11 +0000

    Remove change note

--- db/migrate/20180724140319_remove_change_note.rb
+++ db/migrate/20180724140319_remove_change_note.rb
@@ -1,33 +1,8 @@
+# This has now been replaced by a newer migration: 20180727140643_remove_change_note_v2
+
 # Remove a public change note (dated 20 July 2018) for "VAT Notice 708: buildings and construction"
 # Ticket: https://govuk.zendesk.com/agent/tickets/2927623
 # Page: https://www.gov.uk/government/publications/vat-notice-708-buildings-and-construction
 #
 # Prior steps:
 # Queried `Document` for the `content_id: "5f623c6e-7631-11e4-a3cb-005056011aef"`
-
-class RemoveChangeNote < ActiveRecord::Migration[5.1]
-  disable_ddl_transaction!
-
-  def up
-    # Find document
-    document = Document.where(content_id: "5f623c6e-7631-11e4-a3cb-005056011aef").first
-
-    if document.present?
-      live_edition = document.live
-      details = live_edition.details
-      change_history = details[:change_history]
-
-      details[:change_history] = change_history.drop(1)
-
-      live_edition.save
-
-      if Rails.env.production?
-        Commands::V2::RepresentDownstream.new.call(document.content_id)
-      end
-    end
-  end
-
-  def down
-    # This migration is not reversible
-  end
-end
