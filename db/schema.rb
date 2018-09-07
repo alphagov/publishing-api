2d89640564cd3ad057b57f5561a5b11739b679dd
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-09-07 11:23:41 +0100

    Create a multi-column index on edition id and content_store
    There are quite a few examples of a query being run which uses
    both of these fields, for example:
    
    SELECT  "editions"."id"
    FROM "editions"
    INNER JOIN "documents" ON "documents"."id" = "editions"."document_id"
    WHERE "editions"."content_store" = 'live' AND ("editions"."id" > 1308589)
    ORDER BY "editions"."id" ASC LIMIT 1000
    
    I'm unable to find in the code where the query is coming from, but
    it is happening. It currently takes about 50 seconds to run cold
    because although there is an index on `content_store` it's not
    being used, instead the primary key index is used with a filter
    on the `content_store` field. By adding a multi-column index on
    both of those fields, it gets utilised in this query taking the
    speed down to under a second.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180801195938) do
+ActiveRecord::Schema.define(version: 20180907102237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 20180801195938) do
     t.index ["document_id"], name: "index_editions_on_document_id"
     t.index ["document_type", "state"], name: "index_editions_on_document_type_and_state"
     t.index ["document_type", "updated_at"], name: "index_editions_on_document_type_and_updated_at"
+    t.index ["id", "content_store"], name: "index_editions_on_id_and_content_store"
     t.index ["publishing_app"], name: "index_editions_on_publishing_app"
     t.index ["state", "base_path"], name: "index_editions_on_state_and_base_path"
     t.index ["updated_at", "id"], name: "index_editions_on_updated_at_and_id"
