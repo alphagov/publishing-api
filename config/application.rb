96d87906cb46b427ba2119276a32c29f8b6f34bc
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2019-03-28 15:06:28 +0000

    Add new locales
    We are adding support for:
    
    Croatian: hr
    Danish: da
    Dutch: nl
    Finish fi
    Icelandic is
    Irish gd
    Norwegian: no
    Maltese: mt
    Slovenian: sl
    Swedish: sv
    
    Add the locale codes to `config/application.rb` to allow support for
    them.

--- config/application.rb
+++ config/application.rb
@@ -31,10 +31,12 @@ module PublishingAPI
     config.eager_load_paths += Dir["#{config.root}/lib/**/"]
 
     config.i18n.available_locales = %i[
-      en ar az be bg bn cs cy de dr el es es-419
-      et fa fr he hi hu hy id it ja ka ko lt
-      lv ms pl ps pt ro ru si sk so sq sr sw ta th
-      tk tr uk ur uz vi zh zh-hk zh-tw
+      en ar az be bg bn cs cy da de dr el
+      es es-419 et fa fi fr gd he hi hr hu
+      hy id is it ja ka ko lt lv ms mt nl
+      no pl ps pt ro ru si sk sl so sq sr
+      sv sw ta th tk tr uk ur uz vi zh zh-hk
+      zh-tw
     ]
 
     config.s3_export = OpenStruct.new(
