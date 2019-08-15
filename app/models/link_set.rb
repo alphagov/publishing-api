d7f6395c0c3f47e385aba37d72dad3ff4799d693
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-08-15 08:26:54 +0100

    Add links_changed? to LinkSet
    This can be used to check if the links of a link set have changed by
    passing in an array of what the links used to be. We will use this in
    patch link set to avoid updating dependencies if the links haven't
    changed.

--- app/models/link_set.rb
+++ app/models/link_set.rb
@@ -2,4 +2,28 @@ class LinkSet < ApplicationRecord
   include FindOrCreateLocked
 
   has_many :links, -> { order(link_type: :asc, position: :asc) }, dependent: :destroy
+
+  # We could define the `==` method on `Link` to perform this check but that
+  # breaks the strict definition of a model instance being a representation of
+  # a specific row in the database as it would allow two different rows to
+  # equate.
+  ComparableLink = Struct.new(:link) do
+    delegate :target_content_id, :link_type, :position, to: :link
+
+    def hash
+      [target_content_id, link_type, position].hash
+    end
+
+    def eql?(other)
+      target_content_id == other.target_content_id &&
+        link_type == other.link_type &&
+        position == other.position
+    end
+  end
+
+  def links_changed?(other_links)
+    links_set = links.map { |link| ComparableLink.new(link) }.to_set
+    other_links_set = other_links.map { |link| ComparableLink.new(link) }.to_set
+    links_set != other_links_set
+  end
 end
---
+++ spec/models/link_set_spec.rb
@@ -0,0 +1,72 @@
+require "rails_helper"
+
+RSpec.describe LinkSet do
+  subject(:link_set) { create(:link_set) }
+
+  describe "links_changed?" do
+    let(:other_links) { [] }
+    subject(:links_changed?) { link_set.links_changed?(other_links) }
+
+    context "with no links" do
+      it { is_expected.to be false }
+    end
+
+    context "with only saved links" do
+      before { create(:link, link_set: link_set) }
+
+      it { is_expected.to be true }
+    end
+
+    context "with only other links" do
+      let(:other_links) { [build(:link, link_set: link_set)] }
+
+      it { is_expected.to be true }
+    end
+
+    context "with the same other links" do
+      let(:link) { create(:link, link_set: link_set) }
+      let(:other_links) { [build(:link, link_set: link_set, target_content_id: link.target_content_id)] }
+
+      it { is_expected.to be false }
+    end
+
+    context "with different other links" do
+      let(:target_content_id) { SecureRandom.uuid }
+      let(:link_type) { "link_type" }
+      let(:position) { 0 }
+
+      let(:link) do
+        create(
+          :link, link_set: link_set, target_content_id: target_content_id,
+          link_type: link_type, position: position
+        )
+      end
+      let(:other_link) { nil }
+      let(:other_links) { [other_link] }
+
+      context "different target_content_id" do
+        let(:other_link) do
+          build(:link, target_content_id: SecureRandom.uuid, link_type: link_type, position: position)
+        end
+
+        it { is_expected.to be true }
+      end
+
+      context "different link_type" do
+        let(:other_link) do
+          build(:link, target_content_id: target_content_id, link_type: "link_type2", position: position)
+        end
+
+        it { is_expected.to be true }
+      end
+
+      context "different position" do
+        let(:other_link) do
+          build(:link, target_content_id: target_content_id, link_type: link_type, position: 1)
+        end
+
+        it { is_expected.to be true }
+      end
+    end
+  end
+end
