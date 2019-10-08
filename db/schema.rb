5cb00e72f34ed29e3fbf4a064246c42095e0d8fd
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2019-10-08 09:54:49 +0100

    Add auth_bypass_ids to editions and add validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_19_071750) do
+ActiveRecord::Schema.define(version: 2019_10_07_162200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 2019_07_19_071750) do
     t.datetime "published_at"
     t.datetime "publishing_api_first_published_at"
     t.datetime "publishing_api_last_edited_at"
+    t.string "auth_bypass_ids", default: [], null: false, array: true
     t.index ["base_path", "content_store"], name: "index_editions_on_base_path_and_content_store", unique: true
     t.index ["document_id", "content_store"], name: "index_editions_on_document_id_and_content_store", unique: true
     t.index ["document_id", "state"], name: "index_editions_on_document_id_and_state"
