e07542367ff30c60e6cd416c930ad8eee53c29dd
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2019-10-10 10:48:48 +0100

    Disable ddl transaction for this migration
    This migration would lock the whole of the editions table and as it
    takes a while to run as updating every edition we need to disable the
    transaction.

--- db/migrate/20191007162200_add_auth_bypass_ids_to_editions.rb
+++ db/migrate/20191007162200_add_auth_bypass_ids_to_editions.rb
@@ -1,4 +1,6 @@
 class AddAuthBypassIdsToEditions < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
   def change
     add_column :editions, :auth_bypass_ids, :string, array: true, null: false, default: []
   end
