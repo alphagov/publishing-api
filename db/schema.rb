1ba89a0f64660bb5cd326cbe289fcde571d8d2df
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2019-10-10 10:47:06 +0100

    Copy auth_bypass_ids from AccessLimit to Edition
    This migration copies over all the auth_bypass_ids records from the
    access_limits table to it's corresponding edition.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_131917) do
+ActiveRecord::Schema.define(version: 2019_10_10_161856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
