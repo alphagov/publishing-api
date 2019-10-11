3543f525a1955e3a1f80934096b05301d2b5bc13
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2019-10-11 10:10:15 +0100

    Updates migration to not touch the updated_at column

--- db/migrate/20191010161856_copy_auth_bypass_ids_to_editions.rb
+++ db/migrate/20191010161856_copy_auth_bypass_ids_to_editions.rb
@@ -4,7 +4,7 @@ class CopyAuthBypassIdsToEditions < ActiveRecord::Migration[5.2]
   def up
     AccessLimit.includes(:edition).find_each do |access_limit|
       next unless access_limit.edition
-      access_limit.edition.update!(auth_bypass_ids: access_limit.auth_bypass_ids)
+      access_limit.edition.update_column(:auth_bypass_ids, access_limit.auth_bypass_ids)
     end
   end
 end
