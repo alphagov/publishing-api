2fceb0a9b75c3f8fc233679bed51a9ff1000cfff
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2020-03-19 17:30:41 +0000

    Review new framework defaults
    I've switched load defaults to 6.0 but getting this kind of error:
    
    `Zeitwerk::NameError:
      expected file /govuk/publishing-api/lib/dependency_resolution/link_reference.rb
    to define constant LinkReference, but didn't`
    
    and
    
    `NameError:
      uninitialized constant DataHygiene`
    
    I think this is due to the way the new Zeitwerk file loader works
    in Rails 6.

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ Bundler.require(*Rails.groups)
 module PublishingAPI
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.0
 
     config.api_only = true
 
