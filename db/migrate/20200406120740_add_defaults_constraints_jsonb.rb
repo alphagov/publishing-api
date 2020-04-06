344ae510734016766684bef39c22e6121bfb49b7
Murilo Dal Ri <murilo.dalri@digital.cabinet-office.gov.uk>
2020-04-06 13:09:35 +0100

    Add defaults and constraints to new columns
    This will lock the tables so doing it in a separate migration.

---
+++ db/migrate/20200406120740_add_defaults_constraints_jsonb.rb
@@ -0,0 +1,23 @@
+class AddDefaultsConstraintsJsonb < ActiveRecord::Migration[5.2]
+  def change
+    # Add defaults and constraints to new jsonb columns
+    change_table :access_limits, bulk: true do |t|
+      t.change_default :temp_users, from: nil, to: []
+      t.change_default :temp_organisations, from: nil, to: []
+    end
+
+    change_column_null :access_limits, :temp_users, false
+    change_column_null :access_limits, :temp_organisations, false
+
+    change_table :editions, bulk: true do |t|
+      t.change_default :temp_details, from: nil, to: {}
+      t.change_default :temp_routes, from: nil, to: []
+      t.change_default :temp_redirects, from: nil, to: []
+    end
+
+    change_column_default(:events, :temp_payload, from: nil, to: {})
+
+    change_column_default(:expanded_links, :temp_expanded_links, from: nil, to: {})
+    change_column_null :expanded_links, :temp_expanded_links, false, {}
+  end
+end
