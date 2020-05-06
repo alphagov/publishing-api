c87b58d43ba49ec0f316fba0316868b875eaad94
Murilo Dal Ri <murilo.dalri@digital.cabinet-office.gov.uk>
2020-05-06 16:35:21 +0100

    Migration to delete json columns
    This migration removes the now unused json columns

---
+++ db/migrate/20200506153224_remove_json_columns.rb
@@ -0,0 +1,17 @@
+class RemoveJsonColumns < ActiveRecord::Migration[6.0]
+  def up
+    change_table :access_limits, bulk: true do |t|
+      t.remove :temp_users, :temp_organisations
+    end
+
+    change_table :editions, bulk: true do |t|
+      t.remove :temp_details, :temp_routes, :temp_redirects
+    end
+
+    remove_column :events, :temp_payload, :json
+    remove_column :expanded_links, :temp_expanded_links, :json
+    remove_column :unpublishings, :temp_redirects, :json
+  end
+
+  def down; end
+end
