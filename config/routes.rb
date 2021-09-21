ccf2b8c70d144e84e9f5e5c9b17507500d98442c
Callum Knights <callum.knights@digital.cabinet-office.gov.uk>
2021-09-21 17:16:15 +0100

    Run `app:update` task

--- config/routes.rb
+++ config/routes.rb
@@ -70,4 +70,5 @@ Rails.application.routes.draw do
     require "sidekiq/web"
     mount Sidekiq::Web => "/sidekiq"
   end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
