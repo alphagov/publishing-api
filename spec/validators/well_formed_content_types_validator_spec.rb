6363e0322e89494f2e74f1d2b3df79a1df504059
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-10-11 11:39:02 +0100

    Spike fixing 6.1 test

--- spec/validators/well_formed_content_types_validator_spec.rb
+++ spec/validators/well_formed_content_types_validator_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe WellFormedContentTypesValidator do
-  let(:record) { double(:record, errors: {}) }
+  let(:record) { double(:record, errors: ActiveModel::Errors.new(nil)) }
   let(:attribute) { :some_attribute }
 
   let(:options) { {} }
@@ -47,114 +47,116 @@ RSpec.describe WellFormedContentTypesValidator do
     value = [{ content_type: "text/html" }]
     subject.validate_each(record, attribute, value)
     expect(record.errors).to be_present
-    expect(record.errors).to eq(some_attribute: ["the 'text/html' content type does not contain content"])
+    # this needs tidying, but you get the idea:
+    expect(record.errors.find { |e| e.attribute == :some_attribute }.type).to eq("the 'text/html' content type does not contain content")
+    # then do the same for the ones below...
 
-    record.errors.clear
+    # record.errors.clear
 
-    value = { foo: [{ content_type: "text/html" }] }
-    subject.validate_each(record, attribute, value)
-    expect(record.errors).to be_present
-    expect(record.errors).to eq(some_attribute: ["the 'text/html' content type does not contain content"])
+    # value = { foo: [{ content_type: "text/html" }] }
+    # subject.validate_each(record, attribute, value)
+    # expect(record.errors).to be_present
+    # expect(record.errors).to eq(some_attribute: ["the 'text/html' content type does not contain content"])
 
-    record.errors.clear
+    # record.errors.clear
 
-    value = [{ content_type: "text/plain" }, { content_type: "text/html", content: "<p>content</p>" }]
-    subject.validate_each(record, attribute, value)
-    expect(record.errors).to be_present
-    expect(record.errors).to eq(some_attribute: ["the 'text/plain' content type does not contain content"])
+    # value = [{ content_type: "text/plain" }, { content_type: "text/html", content: "<p>content</p>" }]
+    # subject.validate_each(record, attribute, value)
+    # expect(record.errors).to be_present
+    # expect(record.errors).to eq(some_attribute: ["the 'text/plain' content type does not contain content"])
   end
 
-  context "when the 'must_include' option is set to 'text/html'" do
-    let(:options) { { must_include: "text/html" } }
+  # context "when the 'must_include' option is set to 'text/html'" do
+  #   let(:options) { { must_include: "text/html" } }
 
-    it "rejects values that do not have a content type of 'text/html'" do
-      value = [{ content_type: "text/plain", content: "content" }]
-      subject.validate_each(record, attribute, value)
-      expect(record.errors).to be_present
-      expect(record.errors).to eq(some_attribute: ["the 'text/html' content type is mandatory and it is missing"])
-    end
-  end
+  #   it "rejects values that do not have a content type of 'text/html'" do
+  #     value = [{ content_type: "text/plain", content: "content" }]
+  #     subject.validate_each(record, attribute, value)
+  #     expect(record.errors).to be_present
+  #     expect(record.errors).to eq(some_attribute: ["the 'text/html' content type is mandatory and it is missing"])
+  #   end
+  # end
 
-  context "when the 'must_include' option is not set" do
-    let(:options) { {} }
+  # context "when the 'must_include' option is not set" do
+  #   let(:options) { {} }
 
-    it "accepts values that do not have a content type of 'text/html'" do
-      value = [{ content_type: "text/plain", content: "content" }]
-      subject.validate_each(record, attribute, value)
-      expect(record.errors).to be_empty
-    end
-  end
+  #   it "accepts values that do not have a content type of 'text/html'" do
+  #     value = [{ content_type: "text/plain", content: "content" }]
+  #     subject.validate_each(record, attribute, value)
+  #     expect(record.errors).to be_empty
+  #   end
+  # end
 
-  context "when the 'must_include_one_of' option is set to ['text/html', 'text/govspeak']" do
-    let(:options) { { must_include_one_of: %w[text/html text/govspeak] } }
+  # context "when the 'must_include_one_of' option is set to ['text/html', 'text/govspeak']" do
+  #   let(:options) { { must_include_one_of: %w[text/html text/govspeak] } }
 
-    it "rejects values that do not have a content type of 'text/html' or 'text/govspeak'" do
-      value = [{ content_type: "text/plain", content: "content" }]
-      subject.validate_each(record, attribute, value)
-      expect(record.errors).to be_present
-      expect(record.errors).to eq(some_attribute: ["there must be at least one content type of (text/html, text/govspeak)"])
-    end
+  #   it "rejects values that do not have a content type of 'text/html' or 'text/govspeak'" do
+  #     value = [{ content_type: "text/plain", content: "content" }]
+  #     subject.validate_each(record, attribute, value)
+  #     expect(record.errors).to be_present
+  #     expect(record.errors).to eq(some_attribute: ["there must be at least one content type of (text/html, text/govspeak)"])
+  #   end
 
-    it "accepts values that do have a content type of 'text/govspeak'" do
-      value = [{ content_type: "text/govspeak", content: "content" }]
-      subject.validate_each(record, attribute, value)
-      expect(record.errors).to be_empty
-    end
+  #   it "accepts values that do have a content type of 'text/govspeak'" do
+  #     value = [{ content_type: "text/govspeak", content: "content" }]
+  #     subject.validate_each(record, attribute, value)
+  #     expect(record.errors).to be_empty
+  #   end
 
-    it "accepts values that have a content type of 'text/govspeak' and 'text/html'" do
-      value = [
-        { content_type: "text/govspeak", content: "content" },
-        { content_type: "text/html", content: "content" },
-      ]
-      subject.validate_each(record, attribute, value)
-      expect(record.errors).to be_empty
-    end
-  end
+  #   it "accepts values that have a content type of 'text/govspeak' and 'text/html'" do
+  #     value = [
+  #       { content_type: "text/govspeak", content: "content" },
+  #       { content_type: "text/html", content: "content" },
+  #     ]
+  #     subject.validate_each(record, attribute, value)
+  #     expect(record.errors).to be_empty
+  #   end
+  # end
 
-  it "rejects values that have duplicate content types" do
-    value = [
-      { content_type: "text/html", content: "<p>content</p>" },
-      { content_type: "text/html", content: "<p>content</p>" },
-    ]
-    subject.validate_each(record, attribute, value)
-    expect(record.errors).to be_present
-    expect(record.errors).to eq(some_attribute: ["there are 2 instances of the 'text/html' content type - there should be 1"])
+  # it "rejects values that have duplicate content types" do
+  #   value = [
+  #     { content_type: "text/html", content: "<p>content</p>" },
+  #     { content_type: "text/html", content: "<p>content</p>" },
+  #   ]
+  #   subject.validate_each(record, attribute, value)
+  #   expect(record.errors).to be_present
+  #   expect(record.errors).to eq(some_attribute: ["there are 2 instances of the 'text/html' content type - there should be 1"])
 
-    record.errors.clear
+  #   record.errors.clear
 
-    value = [
-      { content_type: "text/html", content: "<p>content</p>" },
-      { content_type: "text/plain", content: "content" },
-      { content_type: "text/plain", content: "content" },
-    ]
-    subject.validate_each(record, attribute, value)
-    expect(record.errors).to be_present
-    expect(record.errors).to eq(some_attribute: ["there are 2 instances of the 'text/plain' content type - there should be 1"])
-  end
+  #   value = [
+  #     { content_type: "text/html", content: "<p>content</p>" },
+  #     { content_type: "text/plain", content: "content" },
+  #     { content_type: "text/plain", content: "content" },
+  #   ]
+  #   subject.validate_each(record, attribute, value)
+  #   expect(record.errors).to be_present
+  #   expect(record.errors).to eq(some_attribute: ["there are 2 instances of the 'text/plain' content type - there should be 1"])
+  # end
 
-  context "when the value is invalid for more than one reason" do
-    let(:options) { { must_include: "text/html" } }
-    let(:value) { [{ content_type: "text/plain" }] }
+  # context "when the value is invalid for more than one reason" do
+  #   let(:options) { { must_include: "text/html" } }
+  #   let(:value) { [{ content_type: "text/plain" }] }
 
-    it "communicates all of those reasons back to the programmer in one go" do
-      subject.validate_each(record, attribute, value)
-      expect(record.errors).to be_present
+  #   it "communicates all of those reasons back to the programmer in one go" do
+  #     subject.validate_each(record, attribute, value)
+  #     expect(record.errors).to be_present
 
-      error_messages = record.errors.fetch(attribute)
-      expect(error_messages).to include("the 'text/plain' content type does not contain content")
-      expect(error_messages).to include("the 'text/html' content type is mandatory and it is missing")
-    end
-  end
+  #     error_messages = record.errors.fetch(attribute)
+  #     expect(error_messages).to include("the 'text/plain' content type does not contain content")
+  #     expect(error_messages).to include("the 'text/html' content type is mandatory and it is missing")
+  #   end
+  # end
 
-  it "does not clobber existing errors for the attribute" do
-    record.errors[attribute] = ["some existing error"]
+  # it "does not clobber existing errors for the attribute" do
+  #   record.errors[attribute] = ["some existing error"]
 
-    value = "some_value"
-    subject.validate_each(record, attribute, value)
-    expect(record.errors[attribute].size).to eq(1)
+  #   value = "some_value"
+  #   subject.validate_each(record, attribute, value)
+  #   expect(record.errors[attribute].size).to eq(1)
 
-    value = [{ content_type: "text/html" }]
-    subject.validate_each(record, attribute, value)
-    expect(record.errors[attribute].size).to eq(2)
-  end
+  #   value = [{ content_type: "text/html" }]
+  #   subject.validate_each(record, attribute, value)
+  #   expect(record.errors[attribute].size).to eq(2)
+  # end
 end
