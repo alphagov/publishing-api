1d910ad2c4512e457ed81f9873ffa6a6cd98bb67
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2022-01-26 15:30:31 +0000

    remove connection mutex

--- lib/queue_publisher.rb
+++ lib/queue_publisher.rb
@@ -5,14 +5,14 @@ class QueuePublisher
 
     @exchange_name = options.fetch(:exchange)
     @options = options.except(:exchange)
-    @connection_mutex = Mutex.new
+    # @connection_mutex = Mutex.new
   end
 
   def connection
-    @connection_mutex.synchronize do
-      @connection ||= Bunny.new(ENV["RABBITMQ_URL"], @options)
-      @connection.start
-    end
+    # @connection_mutex.synchronize do
+    @connection ||= Bunny.new(ENV["RABBITMQ_URL"], @options)
+    @connection.start
+    # end
   end
 
   class PublishFailedError < StandardError
