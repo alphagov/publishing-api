e1d9f0d95a4f813bff2351f22829b94bed8e6389
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2022-03-15 10:19:45 +0000

    Update test to match error generated by Rails 7
    The 'did you mean' part of the error is no longer returned to the user,
    so we need to update the test accordingly.

--- spec/requests/lookups_spec.rb
+++ spec/requests/lookups_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "POST /lookup-by-base-path", type: :request do
   context "validating" do
     it "requires a base_paths param" do
-      expected_error_response = { "error" => { "code" => 422, "message" => "param is missing or the value is empty: base_paths\nDid you mean?  action\n               controller" } }
+      expected_error_response = { "error" => { "code" => 422, "message" => "param is missing or the value is empty: base_paths" } }
 
       post "/lookup-by-base-path"
 
