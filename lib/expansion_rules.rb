d1e2506811908bd2528fbb6c2544339aba85a6e1
Joseph Kempster <jkempster34@gmail.com>
2023-05-23 12:28:15 +0100

    wip

--- lib/expansion_rules.rb
+++ lib/expansion_rules.rb
@@ -48,6 +48,7 @@ module_function
     %i[ordered_special_representatives role_appointments role],
     %i[ordered_traffic_commissioners role_appointments role],
     %i[historical_accounts person],
+    %i[worldwide_organisation sponsoring_organisations],
   ].freeze
 
   REVERSE_LINKS = {
