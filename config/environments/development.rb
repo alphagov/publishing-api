f6a756cb31754006bb7450ee1cb23becf3c09d64
Korny Sietsma <korny.sietsma@digital.cabinet-office.gov.uk>
2023-05-24 18:00:41 +0100

    added timestamps to dev logs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,10 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # use logger with timestamp (default is simple logger)
+  config.log_formatter = ::Logger::Formatter.new
+  config.log_formatter.datetime_format = "%H:%M:%S"
+
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
