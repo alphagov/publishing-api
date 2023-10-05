41b01d45ae15780824d055d9a16e0f98f41f388b
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2023-10-05 09:22:50 +0100

    Reduce `lock_ttl` and make the value global
    In [1], we added an expiry time to the locking of downstream workers, as
    some locks are not being removed when the job executes. We are still
    unsure of the reason for this.
    
    Having examined the logs further, this affects all types of worker, not
    just the downstream workers.
    
    Therefore making the `lock_ttl` value global for all Sidekiq workers.
    
    Also reducing the expiry time to one hour, as all the workers we have
    should complete within that timescale.
    
    1: https://github.com/alphagov/publishing-api/pull/2515

--- app/workers/downstream_draft_worker.rb
+++ app/workers/downstream_draft_worker.rb
@@ -8,8 +8,7 @@ class DownstreamDraftWorker
   sidekiq_options queue: HIGH_QUEUE,
                   lock: :until_executing,
                   lock_args_method: :uniq_args,
-                  on_conflict: :log,
-                  lock_ttl: 1.day
+                  on_conflict: :log
 
   def self.uniq_args(args)
     [
