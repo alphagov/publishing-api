6eabc5d194293eb607bdb2f1869ebb7750e14417
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-02-17 19:26:59 +0000

    WIP absolute filth...

--- lib/fast_link_expansion.rb
+++ lib/fast_link_expansion.rb
@@ -1,4 +1,11 @@
-LinkExpansionResult = Data.define(:edition_id, :base_path, :content_id, :link_type, :content_id_path, :link_type_path) do
+LinkExpansionResult = Data.define(
+  :edition_id,
+  :base_path,
+  :content_id,
+  :link_type,
+  :content_id_path,
+  :link_type_path,
+) do
   def to_h
     {
       edition_id:,
@@ -29,37 +36,69 @@ class FastLinkExpansion
   end
 
   def links_with_content
-    # TODO - alias the results from root_links and level_1_links so they're consistent
+    # TODO: - alias the results from root_links and level_1_links so they're consistent
     # and can be joined with reverse links and edition links
 
     root_links = Link.joins(:link_set).where(link_sets: { content_id: })
-    root_linked_editions = linked_editions(root_links)
+    root_linked_editions = Edition
+      .with_document
+      .with(root_links:)
+      .joins("INNER JOIN root_links ON root_links.target_content_id = documents.content_id")
+      .where(state: "draft") # TODO: - more states
+      .where(documents: { locale: "en" }) # TODO: - more locales
+      .where.not(document_type: Edition::NON_RENDERABLE_FORMATS)
+      .pluck(
+        :id,
+        :base_path,
+        "documents.content_id",
+        "root_links.link_type",
+        Arel.sql('ARRAY["documents"."content_id"]'),
+        Arel.sql('ARRAY["root_links"."link_type"]'),
+      )
+      .map { |row| LinkExpansionResult.new(*row) }
 
-    level_1_condition = link_type_condition(root_linked_editions)
-    level_1_links = Link.joins(:link_set).where(level_1_condition)
-    # TODO - the problem here is that it's not possible to work out
-    # which content_id / link_type was the parent - because it's just
-    # one giant condition.
-    # Maybe instead of a where((link_type = 'a' and content_id in (b,c,d)) or ... number)
-    # we actually need to do a join table thing, where we could refer back to the parent
-    # ...
-    # ...
-    # in which case, would it actually be easier to go back to the full on SQL version?
-    # ...
-    # I was hoping that we'd be able to do this as a first step...
-    # Maybe something like:
-    # WITH prev(link_type, content_id, link_type_path, content_id_path) as (
-    #   VALUES ('parent', 'blah', link_type_path, content_id_path), ('document', 'blah', link_type_path, content_id_path)
-    # )
-    # SELECT *
-    # FROM links
-    # JOIN prev on links.link_type = prev.link_type AND documents.content_id = prev.content_id
-    # ...
-    # would work?
-    # In a sense, this is kinda similar to what postgres is likely to do anyway when it optimises our giant where clause
-    level_1_linked_editions = linked_editions(level_1_links)
+    link_type_condition(root_linked_editions)
 
-    # TODO - knit the level 1 links into the links: [] arrays in the root links
+    # TODO this is pretty filthy
+    values = Arel::Nodes::ValuesList.new(
+      root_linked_editions.map do |e|
+        [
+          Arel.sql("'#{e.content_id}'::uuid"),
+          e.link_type,
+          Arel.sql("Array[#{e.content_id_path.map { |x| "'#{x}'" }.join(',')}]"),
+          Arel.sql("Array[#{e.link_type_path.map { |x| "'#{x}'" }.join(',')}]"),
+        ]
+      end,
+    )
+    as = Arel::Nodes::As.new(
+      Arel::Nodes::Grouping.new(values),
+      Arel.sql("prev_values(content_id, link_type, content_id_path, link_type_path)"),
+    )
+    previous_links = Arel::Table.new(:prev_values).project(Arel.star).from(as)
+
+    level_1_links = Link
+      .with(previous_links:)
+      .joins(:link_set)
+      .joins("INNER JOIN previous_links ON previous_links.link_type = links.link_type AND previous_links.content_id = link_sets.content_id")
+
+    level_1_linked_editions = Edition
+       .with_document
+       .with(level_1_links:)
+       .joins("INNER JOIN level_1_links ON level_1_links.target_content_id = documents.content_id")
+       .where(state: "draft") # TODO: - more states
+       .where(documents: { locale: "en" }) # TODO: - more locales
+       .where.not(document_type: Edition::NON_RENDERABLE_FORMATS)
+       .pluck(
+         :id,
+         :base_path,
+         "documents.content_id",
+         "level_1_links.link_type",
+         Arel.sql('ARRAY["documents"."content_id"]'), # TODO: this needs to include more stuff
+         Arel.sql('ARRAY["level_1_links"."link_type"]'), # TODO: this needs to include more stuff
+       )
+       .map { |row| LinkExpansionResult.new(*row) }
+
+    # TODO: - knit the level 1 links into the links: [] arrays in the root links
     # instead of flattening them 👇
     (root_linked_editions + level_1_linked_editions).group_by(&:link_type)
       .transform_values { |g| g.map(&:to_h) }
@@ -78,26 +117,7 @@ private
     edition ? edition.content_id : options.fetch(:content_id)
   end
 
-  def linked_editions(level_links)
-    Edition
-      .with_document
-      .with(level_links:)
-      .joins("INNER JOIN level_links ON level_links.target_content_id = documents.content_id")
-      .where(state: "draft") # TODO - more states
-      .where(documents: { locale: "en" }) # TODO - more locales
-      .where.not(document_type: Edition::NON_RENDERABLE_FORMATS)
-      .pluck(
-        :id,
-        :base_path,
-        "documents.content_id",
-        "level_links.link_type",
-        Arel.sql('ARRAY["documents"."content_id"]'), # TODO this needs to include more stuff
-        Arel.sql('ARRAY["level_links"."link_type"]'), # TODO this needs to include more stuff
-      )
-      .map { |row| LinkExpansionResult.new(*row) }
-  end
-
-  # TODO factor this out and unit test it
+  # TODO: factor this out and unit test it
   def link_type_condition(linked_editions)
     link_type_col = Link.arel_table[:link_type]
     content_id_col = LinkSet.arel_table[:content_id]
