e60486f80904f2bcf753635df0c01c3d591fd12c
Callum Knights <callum.knights@digital.cabinet-office.gov.uk>
2024-02-29 15:02:58 +0000

    Add presenter and schemas for new substitute unpublishing type.
    Also added extra logging for unknown publishing types.

--- app/downstream_payload.rb
+++ app/downstream_payload.rb
@@ -60,6 +60,10 @@ private
     Presenters::VanishPresenter.from_edition(edition)
   end
 
+  def substitute_presenter
+    Presenters::SubstitutePresenter.from_edition(edition)
+  end
+
   def content_store_presenter
     if unpublishing
       return redirect_presenter if unpublishing.type == "redirect"
@@ -72,14 +76,20 @@ private
   end
 
   def message_queue_presenter
-    return redirect_presenter if edition.document_type == "redirect"
-    return content_presenter unless unpublished?
-
-    case unpublishing.type
-    when "redirect" then redirect_presenter
-    when "gone" then gone_presenter
-    when "vanish" then vanish_presenter
-    else content_presenter
+    if unpublished?
+      case unpublishing.type
+      when "redirect" then redirect_presenter
+      when "gone" then gone_presenter
+      when "vanish" then vanish_presenter
+      when "substitute" then substitute_presenter
+      else
+        logger.warn("Unexpected unpublishing type #{unpublishing.type}")
+        content_presenter
+      end
+    elsif edition.document_type == "redirect"
+      redirect_presenter
+    else
+      content_presenter
     end
   end
 end
