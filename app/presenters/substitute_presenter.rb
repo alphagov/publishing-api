556c29baf5c386a26d1ad3b7534778a456ad881c
Callum Knights <callum.knights@digital.cabinet-office.gov.uk>
2024-03-04 09:17:38 +0000

    Add new presenter for substitute

---
+++ app/presenters/substitute_presenter.rb
@@ -0,0 +1,30 @@
+class Presenters::SubstitutePresenter
+  def initialize(base_path:, content_id:, publishing_app:, locale:)
+    @base_path = base_path
+    @publishing_app = publishing_app
+    @content_id = content_id
+    @locale = locale
+  end
+
+  def self.from_edition(edition)
+    new(
+      base_path: edition.base_path,
+      content_id: edition.content_id,
+      locale: edition.locale,
+      publishing_app: edition.publishing_app,
+    )
+  end
+
+  def for_message_queue(payload_version)
+    {
+      document_type: "substitute",
+      schema_name: "substitute",
+      base_path: @base_path,
+      locale: @locale,
+      publishing_app: @publishing_app,
+      content_id: @content_id,
+      govuk_request_id: GdsApi::GovukHeaders.headers[:govuk_request_id],
+      payload_version:,
+    }
+  end
+end
