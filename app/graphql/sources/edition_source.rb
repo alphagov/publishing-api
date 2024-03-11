2b9d7536384a7521b3903c8cf854b10444caaadc
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-03-11 17:52:00 +0000

    Batch load editions using dataloader

--- app/graphql/sources/edition_source.rb
+++ app/graphql/sources/edition_source.rb
@@ -1,8 +1,17 @@
 module Sources
   class EditionSource < GraphQL::Dataloader::Source
     def fetch(content_ids)
+      editions = Edition
+        .with_document
+        .with_unpublishing
+        .where(documents: { content_id: content_ids, locale: "en" })
+        .where(content_store: :live, state: :published)
+        .where("unpublishings.type IS NULL OR unpublishings.type != 'substitute'")
+        .includes(:document)
+        .group_by { |edition| edition.content_id }
+
       content_ids.map do |content_id|
-        Queries::GetEditionForContentStore.call(content_id, "en")
+        editions.fetch(content_id, []).sort_by(&:user_facing_version).last
       end
     end
   end
