06f540aedc64d1319479847e91613dbd1957304b
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-03-11 09:23:06 +0000

    Implement basic person -> roles loading
    Only supports "current" roles at the moment (unlike our existing link expansion API, which doesn't filter).
    
    No batching, so this is likely to get inefficient. At some point, we'll need to play with the Dataloader concept. Maybe later a direct graphql -> sql transformation might eke out a bit more performance.

--- app/graphql/types/person_type.rb
+++ app/graphql/types/person_type.rb
@@ -11,7 +11,27 @@ module Types
     end
 
     def roles
-      raise "TODO"
+      # Find the role appointments that link to this person
+      role_appointment_content_ids = Queries::Links.to(
+        object.content_id,
+        allowed_link_types: %i[person],
+      )[:person].map{ _1[:content_id] }
+
+      # Select only the current role appointments
+      role_appointment_content_ids = role_appointment_content_ids.filter do |content_id|
+        edition = Queries::GetEditionForContentStore.call(content_id, "en")
+        edition.details.dig(:current)
+      end
+
+      # Find the roles that these role appointments link to
+      role_content_ids = role_appointment_content_ids.flat_map do |content_id|
+        Queries::Links.from(content_id, allowed_link_types: %i[role])[:role].map{ _1[:content_id] }
+      end
+
+      # Get the latest edition for each role
+      role_content_ids.map do |content_id|
+        Queries::GetEditionForContentStore.call(content_id, "en")
+      end
     end
   end
 end
--- app/graphql/types/role_type.rb
+++ app/graphql/types/role_type.rb
@@ -5,13 +5,18 @@ module Types
     description "A role"
     field :people, [PersonType]
     field :body, String
+    field :title, String
 
     def people
       raise "TODO"
     end
 
+    def title
+      object.title
+    end
+
     def body
-      object.details[:body]
+      object.details.dig(:body, 0, :content)
     end
   end
 end
