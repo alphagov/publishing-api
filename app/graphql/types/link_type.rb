753f87fd0b4e9d71fc07923c758f5bec85e422b0
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-03-12 12:35:24 +0000

    Add target edition to generic link type

--- app/graphql/types/link_type.rb
+++ app/graphql/types/link_type.rb
@@ -5,5 +5,10 @@ module Types
     description "A link"
     field :target_content_id, String
     field :link_type, String
+    field :target_edition, EditionType
+
+    def target_edition
+      dataloader.with(Sources::EditionSource).load(object.target_content_id)
+    end
   end
 end
