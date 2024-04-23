60e6e7d2c6b6167dcfcee027cfc0357c67537086
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-04-23 14:48:44 +0100

    Remove unused NodeType module
    ... and associated fields

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,26 +2,8 @@
 
 module Types
   class QueryType < Types::BaseObject
-    field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
-      argument :id, ID, required: true, description: "ID of the object."
-    end
-
-    def node(id:)
-      context.schema.object_from_id(id, context)
-    end
-
-    field :nodes, [Types::NodeType, null: true], null: true, description: "Fetches a list of objects given a list of IDs." do
-      argument :ids, [ID], required: true, description: "IDs of the objects."
-    end
-
-    def nodes(ids:)
-      ids.map { |id| context.schema.object_from_id(id, context) }
-    end
-
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
     # Collections of editions
+
     field :editions, Types::EditionType.connection_type, description: "Collection of editions" do
       argument :document_type, String, required: false
     end
