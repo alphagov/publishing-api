7b3c10e0c3dd8bf7022fa6744c343b5f9e11bd3b
pezholio <pezholio@gmail.com>
2024-06-11 10:54:30 +0100

    Add a service method to get a named schema
    This allows us to fetch a schema for a particular content type, throwing
    an error if there is no such schema found

--- app/services/schema_service.rb
+++ app/services/schema_service.rb
@@ -5,4 +5,11 @@ class SchemaService
                         .select { |k| k.ends_with?("schema.json") }
                         .transform_keys { |k| format_regex.match(k)[1] }
   end
+
+  def self.find_schema_by_name(name)
+    GovukSchemas::Schema.find(publisher_schema: name)
+  rescue Errno::ENOENT
+    message = "Could not find publisher schema with name: #{name}"
+    raise CommandError.new(code: 404, message:)
+  end
 end
--- spec/services/schema_service_spec.rb
+++ spec/services/schema_service_spec.rb
@@ -39,4 +39,38 @@ RSpec.describe SchemaService do
       expect(SchemaService.all_schemas).to eq(expected_schemas)
     end
   end
+
+  describe "find_schema_by_name" do
+    let(:schema) do
+      {
+        type: "other",
+        required: %w[b],
+        properties: {
+          b: { "type" => "string" },
+        },
+      }.with_indifferent_access
+    end
+
+    describe "when a schema exists" do
+      before do
+        allow(GovukSchemas::Schema).to receive(:find).with(publisher_schema: "other") { schema }
+      end
+
+      it "returns a schema" do
+        expect(SchemaService.find_schema_by_name("other")).to eq(schema)
+      end
+    end
+
+    describe "when a schema does not exist" do
+      before do
+        allow(GovukSchemas::Schema).to receive(:find).with(publisher_schema: "any").and_raise(Errno::ENOENT)
+      end
+
+      it "throws an error" do
+        expect { SchemaService.find_schema_by_name("any") }.to raise_error(
+          an_instance_of(CommandError).and(having_attributes(code: 404, message: "Could not find publisher schema with name: any")),
+        )
+      end
+    end
+  end
 end
