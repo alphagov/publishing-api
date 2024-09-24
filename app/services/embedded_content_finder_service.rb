f8091b1b1efabdb6b6ad948906bfa08227f09eb9
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2024-09-24 14:59:01 +0100

    WIP

--- app/services/embedded_content_finder_service.rb
+++ app/services/embedded_content_finder_service.rb
@@ -2,8 +2,8 @@ class EmbeddedContentFinderService
   ContentReference = Data.define(:document_type, :friendly_id, :embed_code)
 
   SUPPORTED_DOCUMENT_TYPES = %w[contact content_block_email_address].freeze
-  UUID_REGEX = /([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/
-  EMBED_REGEX = /({{embed:(#{SUPPORTED_DOCUMENT_TYPES.join('|')}):#{UUID_REGEX}}})/
+  # UUID_REGEX = /([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/
+  EMBED_REGEX = /({{embed:(#{SUPPORTED_DOCUMENT_TYPES.join('|')}):(.*?)}})/
 
   def fetch_linked_friendly_ids(body, locale)
     content_references = if body.is_a?(Array)
@@ -13,8 +13,7 @@ class EmbeddedContentFinderService
                          end
     return [] if content_references.empty?
 
-    check_all_references_exist(content_references, locale)
-    content_references.map(&:content_id)
+    get_content_ids_for_content_references(content_references, locale)
   end
 
   def find_content_references(body)
@@ -23,26 +22,26 @@ class EmbeddedContentFinderService
 
 private
 
-  def check_all_references_exist(content_references, locale)
-    found_editions = live_editions(content_references, locale)
-    if found_editions.count != content_references.count
-      not_found_content_ids = content_references.map(&:content_id) - found_editions.map(&:content_id)
-      raise CommandError.new(
-        code: 422,
-        message: "Could not find any live editions in locale #{locale} for: #{not_found_content_ids.join(', ')}, ",
-      )
-    end
-  end
-
-  def live_editions(content_references, locale)
+  def get_content_ids_for_content_references(content_references, locale)
+    puts content_references.inspect
     embedded_content_references = EmbeddedContentReference.where(friendly_id: content_references.map(&:friendly_id))
     content_ids = embedded_content_references.map(&:content_id)
 
-    Edition.with_document.where(
+    found_editions = Edition.with_document.where(
       state: "published",
       content_store: "live",
       document_type: content_references.map(&:document_type),
       documents: { content_id: content_ids, locale: },
     )
+
+    if found_editions.count != content_references.count
+      not_found_content_references = content_ids - found_editions.map(&:content_id)
+      raise CommandError.new(
+        code: 422,
+        message: "Could not find any live editions in locale #{locale} for: #{not_found_content_references.join(', ')}, ",
+      )
+    end
+
+    embedded_content_references.map(&:content_id)
   end
 end
