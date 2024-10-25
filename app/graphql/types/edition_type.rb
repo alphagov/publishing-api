35cff41dd2796c370773c588104462362ed28a3a
George Eaton <george@dxw.com>
2024-10-25 15:11:19 +0100

    Add additional edition type fields
    This expands the edition type to include all the top-level properties
    that a content item contains, and most of the nested data. The queries
    sent by frontend apps will be able to pare down the data that gets sent
    to them from the Publishing API, but we are able to get almost all the
    same data as presented by the current content item if required
    
    Some fields are not yet fully implemented:
    - `withdrawn_notice` is currently just a nullable String, but will be
      implemented more fully in an upcoming commit
    - `details` is currently a generic JSON type as it's an inherently
      flexible object that could have a wide range of fields. It may be
      worth defining more specific types and moving the `details` keys up to
      the top level on each type for each page being built in future. This
      would allow for reduced complexity in querying the required fields.
      We've used JSON to keep it flexible and to allow consumers to fall
      back on these fields if they need to.
    - `links` needs further work to query and present the data that is
      currently presented in the content item, so we've left the field off
      for now, rather than adding a non-so-useful placeholder. We'll need to
      add links when we add pages that use links like the ministers index
      page.  It would be good to figure out how we can reduce the need for
      full link expansion and move to a more minimal on-demand version when
      we tackle this piece of work.
    - `publishing_scheduled_at` and `scheduled_publishing_delay_seconds` are
      not currently available in Publishing API. They are generated in
      Content Store, so we just return `null` here. If we do need to serve
      these in future, we'd need to find a way of storing these in
      Publishing API, but that's definitely out of scope for now.
    
    Co-authored-by: Ynda Jas <yndajas@gmail.com>

--- app/graphql/types/edition_type.rb
+++ app/graphql/types/edition_type.rb
@@ -2,6 +2,33 @@
 
 module Types
   class EditionType < Types::BaseObject
-    field :title, String
+    field :analytics_identifier, String
+    field :base_path, String
+    field :content_id, ID
+    field :description, String
+    field :details, GraphQL::Types::JSON, null: false
+    field :document_type, String
+    field :first_published_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :locale, String, null: false
+    field :phase, String, null: false
+    field :public_updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :publishing_app, String
+    field :publishing_request_id, String
+    field :publishing_scheduled_at, GraphQL::Types::ISO8601DateTime
+    field :rendering_app, String
+    field :scheduled_publishing_delay_seconds, Int
+    field :schema_name, String
+    field :title, String, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime
+    field :withdrawn_notice, String
+
+    # Aliased by field methods for fields that are currently presented in the
+    # content item, but come from Content Store, so we can't provide them here
+    def not_stored_in_publishing_api
+      nil
+    end
+
+    alias_method :publishing_scheduled_at, :not_stored_in_publishing_api
+    alias_method :scheduled_publishing_delay_seconds, :not_stored_in_publishing_api
   end
 end
