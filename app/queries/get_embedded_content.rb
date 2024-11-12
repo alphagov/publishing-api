b70dfa128438293162b29996f084fba527f67fce
pezholio <pezholio@gmail.com>
2024-11-12 08:56:50 +0000

    Use Arel to generate SQL
    This will make it easier to chain queries onto the SQL without having to
    write it “raw”

--- app/queries/get_embedded_content.rb
+++ app/queries/get_embedded_content.rb
@@ -14,51 +14,79 @@ module Queries
       :unique_pageviews,
     )
 
-    SQL = <<-SQL.freeze
-        SELECT
-            editions.id,
-            editions.title,
-            editions.base_path,
-            editions.document_type,
-            editions.publishing_app,
-            editions.last_edited_by_editor_id,
-            editions.last_edited_at,
-            primary_links.target_content_id AS primary_publishing_organisation_content_id,
-            org_editions.title AS primary_publishing_organisation_title,
-            org_editions.base_path AS primary_publishing_organisation_base_path,
-            statistics_caches.unique_pageviews AS unique_pageviews
-          FROM "editions"
-            INNER JOIN "links" ON "links"."edition_id" = "editions"."id"
-            INNER JOIN "documents" ON "documents"."id" = "editions"."document_id"
-            LEFT JOIN links AS primary_links ON primary_links.edition_id = editions.id AND primary_links.link_type = 'primary_publishing_organisation'
-            LEFT JOIN documents AS org_documents ON org_documents.content_id = primary_links.target_content_id
-            LEFT JOIN editions AS org_editions ON org_editions.document_id = org_documents.id AND org_editions.state = 'published'
-            LEFT JOIN statistics_caches ON statistics_caches.document_id = documents.id
-            WHERE "editions"."state" = $1 AND "links"."link_type" = $2 AND "links"."target_content_id" = $3
-    SQL
 
     attr_reader :target_content_id, :state
 
     def initialize(target_content_id)
+    TABLES = {
+      editions: Edition.arel_table,
+      documents: Document.arel_table,
+      links: Link.arel_table,
+      primary_links: Link.arel_table.alias(:primary_links),
+      org_editions: Edition.arel_table.alias(:org_editions),
+      org_documents: Document.arel_table.alias(:org_documents),
+      statistics_caches: StatisticsCache.arel_table,
+    }.freeze
+
+    FIELDS = [
+      TABLES[:editions][:id],
+      TABLES[:editions][:title],
+      TABLES[:editions][:base_path],
+      TABLES[:editions][:document_type],
+      TABLES[:editions][:publishing_app],
+      TABLES[:editions][:last_edited_by_editor_id],
+      TABLES[:editions][:last_edited_at],
+      TABLES[:primary_links][:target_content_id].as("primary_publishing_organisation_content_id"),
+      TABLES[:org_editions][:title].as("primary_publishing_organisation_title"),
+      TABLES[:org_editions][:base_path].as("primary_publishing_organisation_base_path"),
+      TABLES[:statistics_caches][:unique_pageviews],
+    ].freeze
+
       @target_content_id = target_content_id
       @state = "published"
     end
 
     def call
-      # Prepare params to bind to the SQL query - ActiveRecord requires params to be sent as QueryAttributes,
-      # see https://api.rubyonrails.org/classes/ActiveRecord/ConnectionAdapters/DatabaseStatements.html#method-i-select_all
-      bind_params = [
-        ActiveRecord::Relation::QueryAttribute.new("state", state, ActiveRecord::Type::Text.new),
-        ActiveRecord::Relation::QueryAttribute.new("embedded_link_type", embedded_link_type, ActiveRecord::Type::Text.new),
-        ActiveRecord::Relation::QueryAttribute.new("target_content_id", target_content_id, ActiveRecord::Type::Text.new),
-      ]
-      results = ActiveRecord::Base.connection.select_all(SQL, "SQL", bind_params).to_a
+      results = ActiveRecord::Base.connection.select_all(arel_query).to_a
       results.map do |row|
         Result.new(**row)
       end
     end
 
   private
+
+    def arel_query
+      arel_joins.where(
+        TABLES[:editions][:state].eq(state)
+                                 .and(TABLES[:links][:link_type].eq(embedded_link_type))
+                                 .and(TABLES[:links][:target_content_id].eq(target_content_id)),
+      )
+    end
+
+    def arel_joins
+      TABLES[:editions]
+        .project(FIELDS)
+        .join(TABLES[:links]).on(
+          TABLES[:links][:edition_id].eq(TABLES[:editions][:id]),
+        )
+        .join(TABLES[:documents]).on(
+          TABLES[:documents][:id].eq(TABLES[:editions][:document_id]),
+        )
+        .join(TABLES[:primary_links], Arel::Nodes::OuterJoin).on(
+          TABLES[:primary_links][:edition_id].eq(TABLES[:editions][:id]),
+          TABLES[:primary_links][:link_type].eq("primary_publishing_organisation"),
+        )
+        .join(TABLES[:org_documents], Arel::Nodes::OuterJoin).on(
+          TABLES[:org_documents][:content_id].eq(TABLES[:primary_links][:target_content_id]),
+        )
+        .join(TABLES[:org_editions], Arel::Nodes::OuterJoin).on(
+          TABLES[:org_editions][:document_id].eq(TABLES[:org_documents][:id]),
+          TABLES[:org_editions][:state].eq("published"),
+        )
+        .join(TABLES[:statistics_caches], Arel::Nodes::OuterJoin).on(
+          TABLES[:statistics_caches][:document_id].eq(TABLES[:documents][:id]),
+        )
+    end
 
     def embedded_link_type
       "embed"
