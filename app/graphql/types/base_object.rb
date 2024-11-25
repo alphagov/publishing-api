693c6e3ab2f75a88804e5b517048cb365fe5188a
Mike Patrick <mike.patrick2@digital.cabinet-office.gov.uk>
2024-11-25 17:04:13 +0000

    Common links_field method for BaseObject
    We did say that we would avoid thinking about database queries until
    after we'd finished defining our first few GraphQL types. That included
    de-duplicating the Active Record queries. But After copying-and-pasting
    this same code 8 times for the new Ministers Index type, it seemed
    overdue to just refactor this one thing.

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -5,5 +5,21 @@ module Types
     edge_type_class(Types::BaseEdge)
     connection_type_class(Types::BaseConnection)
     field_class Types::BaseField
+
+    def self.links_field(field_name_and_link_type, graphql_field_type)
+      field(field_name_and_link_type.to_sym, graphql_field_type)
+
+      define_method(field_name_and_link_type.to_sym) do
+        Edition
+          .live
+          .joins(document: { reverse_links: :link_set })
+          .where(
+            document: { locale: "en" },
+            link_set: { content_id: object.content_id },
+            reverse_links: { link_type: field_name_and_link_type.to_s },
+          )
+          .order(reverse_links: { position: :asc })
+      end
+    end
   end
 end
