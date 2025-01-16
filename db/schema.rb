2c2f72d1450da98f985aa92f6cc5143525f9c3bf
Ynda Jas <yndajas@gmail.com>
2025-01-16 17:15:40 +0000

    Update schema to Rails 8.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_06_115835) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_06_115835) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "access_limits", id: :serial, force: :cascade do |t|
     t.datetime "created_at", precision: nil, null: false
