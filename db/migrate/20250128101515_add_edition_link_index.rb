f7f1635592b1c40c550f435fd43a6a4a3eab2542
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2025-01-30 11:54:28 +0000

    Make migrations concurrent
    The migrations to add new indexes were previously locking all writes to
    the editions table at the time of running.
    
    As one of the migrations is slow (several minutes), this caused degraded
    performance of the application.
    
    Making them concurrent to remove the need for the tables to be locked.
    
    Concurrent indexes must be created outside of a transaction [1].
    
    1: https://thoughtbot.com/blog/how-to-create-postgres-indexes-concurrently-in

--- db/migrate/20250128101515_add_edition_link_index.rb
+++ db/migrate/20250128101515_add_edition_link_index.rb
@@ -1,5 +1,7 @@
 class AddEditionLinkIndex < ActiveRecord::Migration[8.0]
+  disable_ddl_transaction!
+
   def change
-    add_index :links, %i[edition_id link_type]
+    add_index :links, %i[edition_id link_type], algorithm: :concurrently
   end
 end
