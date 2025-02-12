6eef5bb59d2f28dcb0eab9882ad56ccb08fd770d
Mike Patrick <mike.patrick2@digital.cabinet-office.gov.uk>
2025-02-12 15:45:51 +0000

    Only SELECT the root Edition's columns we need
    We're hoping to speed up our GraphQL responses by reducing the amount of
    data retrieved from the database. By design we already only send the
    client the data they've requested, but until now that meant that we've
    been fetching a lot of data from the database that we don't actually
    make use of in serving the response to the client.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,10 +5,34 @@ module Types
     field :edition, EditionTypeOrSubtype, description: "An edition or one of its subtypes" do
       argument :base_path, String
       argument :content_store, String, required: false, default_value: "live"
+
+      extras [:lookahead]
     end
 
-    def edition(base_path:, content_store:)
-      Edition.where(content_store:).find_by(base_path:)
+    def edition(base_path:, content_store:, lookahead:)
+      selections = {}
+      selections[:editions] = convert_edition_selections(lookahead:)
+
+      selections[:editions] << :document_type
+
+      if lookahead.selects?(:links)
+        if lookahead.selections.find { _1.name == :links }.selects?(:available_translations)
+          selections[:editions] << :state
+        end
+
+        selections[:documents] = %i[content_id locale]
+      end
+
+      selections[:editions] = selections[:editions].to_a
+
+      if lookahead.selects?(:links)
+        Edition
+          .joins(:document)
+          .select(selections).where(content_store:).find_by(base_path:)
+      else
+        Edition
+          .select(selections).where(content_store:).find_by(base_path:)
+      end
     end
   end
 end
