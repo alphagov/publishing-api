355ff3b15b70d330573871f9286a4eae1ca52d8c
Mike Patrick <mike.patrick2@digital.cabinet-office.gov.uk>
2025-02-12 15:45:51 +0000

    WIP Use lookahead to select fewer root attributes

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,10 +5,23 @@ module Types
     field :edition, EditionTypeOrSubtype, description: "An edition or one of its subtypes" do
       argument :base_path, String
       argument :content_store, String, required: false, default_value: "live"
+
+      extras [:lookahead]
     end
 
-    def edition(base_path:, content_store:)
-      Edition.where(content_store:).find_by(base_path:)
+    def edition(base_path:, content_store:, lookahead:)
+      all_selections = lookahead.selections.map(&:name)
+
+      all_selections.delete(:links)
+
+      attributes = all_selections
+      attributes += %i{id document_type content_store document_id}
+      # id for edition link queries,
+      # document_type for EditionTypeOrSubtype
+      # content_store for BaseObject#links_field #reverse_links_field
+      # document_id for getting Document and its content_id
+
+      Edition.select(attributes).where(content_store:).find_by(base_path:)
     end
   end
 end
