d8b3317b611a95070d37bdf70e4f00b299a52841
Mike Patrick <mike.patrick2@digital.cabinet-office.gov.uk>
2025-02-14 10:39:04 +0000

    Copy ALL_EDITION_COLUMNS Set from Richard's repo

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,6 +1,39 @@
 # frozen_string_literal: true
 
 module Types
+  ALL_EDITION_COLUMNS = Set.new(%i[
+    analytics_identifier
+    auth_bypass_ids
+    base_path
+    content_store
+    created_at
+    description
+    details
+    document_id
+    document_type
+    first_published_at
+    id
+    last_edited_at
+    last_edited_by_editor_id
+    major_published_at
+    phase
+    public_updated_at
+    published_at
+    publishing_api_first_published_at
+    publishing_api_last_edited_at
+    publishing_app
+    publishing_request_id
+    redirects
+    rendering_app
+    routes
+    schema_name
+    state
+    title
+    update_type
+    updated_at
+    user_facing_version
+  ]).freeze
+
   class QueryType < Types::BaseObject
     field :edition, EditionTypeOrSubtype, description: "An edition or one of its subtypes" do
       argument :base_path, String
