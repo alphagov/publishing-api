d5fcfc2077858b277632a3c7aeed957edceeccd7
Mike Patrick <mike.patrick2@digital.cabinet-office.gov.uk>
2025-02-18 13:22:15 +0000

    Select a subset of linked-to-editions' columns

--- app/graphql/sources/linked_to_editions_source.rb
+++ app/graphql/sources/linked_to_editions_source.rb
@@ -11,32 +11,46 @@ module Sources
       edition_id_tuples = editions_and_link_types.map { |edition, link_type| "(#{edition.id},'#{link_type}')" }.join(",")
 
       edition_links = Link
-        .joins(:edition, { target_documents: @content_store })
-        .includes(:edition, { target_documents: @content_store })
+        .joins(edition: :document)
         .where('("editions"."id", "links"."link_type") IN (?)', Arel.sql(edition_id_tuples))
-        .where(target_documents: { locale: "en" })
         .order(link_type: :asc, position: :asc)
+        .select("link_type", "position", "target_content_id", "editions.id", "documents.content_id")
 
       link_set_links = Link
-        .joins(:link_set, { target_documents: @content_store })
-        .includes(:link_set, { target_documents: @content_store })
+        .joins(:link_set)
         .where('("link_sets"."content_id", "links"."link_type") IN (?)', Arel.sql(content_id_tuples))
-        .where(target_documents: { locale: "en" })
         .order(link_type: :asc, position: :asc)
+        .select("link_type", "position", "target_content_id", "link_sets.content_id")
 
       all_links = edition_links + link_set_links
 
+      editions = Edition
+        .joins(:document)
+        .where(
+          document: {
+            locale: "en",
+            content_id: all_links.map(&:target_content_id),
+          },
+          content_store: @content_store,
+        )
+        .select(
+          "title",
+          "base_path",
+          "details",
+          "content_store",
+          "document_id",
+          "document.content_id",
+          "document.locale",
+        )
+
+      editions_map = editions.each_with_object({}) do |e, hash|
+        hash[e.content_id] = e
+      end
       link_types_map = editions_and_link_types.map { [_1.content_id, _2] }.index_with { [] }
 
       all_links.each_with_object(link_types_map) { |link, hash|
-        hash[[(link.link_set || link.edition).content_id, link.link_type]].concat(editions_for_link(link))
+        hash[[link.content_id, link.link_type]] << editions_map[link.target_content_id]
       }.values
-    end
-
-  private
-
-    def editions_for_link(link)
-      link.target_documents.map { |document| document.send(@content_store) }
     end
   end
 end
