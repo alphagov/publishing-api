22a55cb9e22119cf12cad2b8866dd9d10381944c
Mike Patrick <mike.patrick2@digital.cabinet-office.gov.uk>
2025-02-18 13:22:15 +0000

    Only SELECT the linked-to-Editions columns we need
    Following on from the changes we made to the QueryType's database query
    for the root edition, this change aims to speed up GraphQL queries
    involving generic links by reducing the amount of data retrieved from
    the database.

--- spec/graphql/sources/linked_to_editions_source_spec.rb
+++ spec/graphql/sources/linked_to_editions_source_spec.rb
@@ -10,7 +10,14 @@ RSpec.describe Sources::LinkedToEditionsSource do
     create(:link, link_set: link_set, target_content_id: target_edition_3.content_id, link_type: "test_link")
 
     GraphQL::Dataloader.with_dataloading do |dataloader|
-      request = dataloader.with(described_class, content_store: source_edition.content_store).request([source_edition, "test_link"])
+      request = dataloader.with(
+        described_class,
+        content_store: source_edition.content_store,
+      ).request([
+        source_edition,
+        "test_link",
+        %i[id base_path title document_id],
+      ])
 
       expect(request.load).to match_array([target_edition_1, target_edition_3])
     end
@@ -28,7 +35,14 @@ RSpec.describe Sources::LinkedToEditionsSource do
                             })
 
     GraphQL::Dataloader.with_dataloading do |dataloader|
-      request = dataloader.with(described_class, content_store: source_edition.content_store).request([source_edition, "test_link"])
+      request = dataloader.with(
+        described_class,
+        content_store: source_edition.content_store,
+      ).request([
+        source_edition,
+        "test_link",
+        %i[id base_path title document_id],
+      ])
 
       expect(request.load).to match_array([target_edition_1, target_edition_3])
     end
@@ -49,7 +63,14 @@ RSpec.describe Sources::LinkedToEditionsSource do
     create(:link, link_set: link_set, target_content_id: target_edition_3.content_id, link_type: "test_link")
 
     GraphQL::Dataloader.with_dataloading do |dataloader|
-      request = dataloader.with(described_class, content_store: source_edition.content_store).request([source_edition, "test_link"])
+      request = dataloader.with(
+        described_class,
+        content_store: source_edition.content_store,
+      ).request([
+        source_edition,
+        "test_link",
+        %i[id base_path title document_id],
+      ])
 
       expect(request.load).to match_array([target_edition_1, target_edition_3])
     end
@@ -72,7 +93,14 @@ RSpec.describe Sources::LinkedToEditionsSource do
     create(:link, link_set:, target_content_id: target_edition_4.content_id, link_type: "test_link")
 
     GraphQL::Dataloader.with_dataloading do |dataloader|
-      request = dataloader.with(described_class, content_store: source_edition.content_store).request([source_edition, "test_link"])
+      request = dataloader.with(
+        described_class,
+        content_store: source_edition.content_store,
+      ).request([
+        source_edition,
+        "test_link",
+        %i[id base_path title document_id],
+      ])
 
       expect(request.load).to match_array([target_edition_3, target_edition_4])
     end
