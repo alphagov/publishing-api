8c7674108c22cc907b23eb71ea891ceb7f24df37
Mike Patrick <mike.patrick2@digital.cabinet-office.gov.uk>
2025-02-24 16:46:03 +0000

    Rename variable to be shorter and sweeter
    I originally named this variable that way because I was having trouble
    remembering what a "link type" even is and what its significance might
    be. We've been knee-deep in these Publishing API concepts for a couple
    of months now and as a result both of those things have become second
    nature and this variable name has become cumbersome.
    
    (I'm not sure whether the naming for reverse_links_field was ever quite
    accurate.)

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -44,21 +44,21 @@ module Types
     connection_type_class(Types::BaseConnection)
     field_class Types::BaseField
 
-    def self.links_field(field_name_and_link_type, graphql_field_type)
-      field(field_name_and_link_type.to_sym, graphql_field_type)
+    def self.links_field(link_type, graphql_field_type)
+      field(link_type.to_sym, graphql_field_type)
 
-      define_method(field_name_and_link_type.to_sym) do
+      define_method(link_type.to_sym) do
         dataloader.with(Sources::LinkedToEditionsSource, content_store: object.content_store)
-          .load([object, field_name_and_link_type.to_s])
+          .load([object, link_type.to_s])
       end
     end
 
-    def self.reverse_links_field(field_name_and_link_type, belongs_to, graphql_field_type)
-      field(field_name_and_link_type.to_sym, graphql_field_type)
+    def self.reverse_links_field(field_name, link_type, graphql_field_type)
+      field(field_name.to_sym, graphql_field_type)
 
-      define_method(field_name_and_link_type.to_sym) do
+      define_method(field_name.to_sym) do
         dataloader.with(Sources::ReverseLinkedToEditionsSource, content_store: object.content_store)
-          .load([object, belongs_to.to_s])
+          .load([object, link_type.to_s])
       end
     end
 
