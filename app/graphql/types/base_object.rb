57eceeb8d31fb06bf1ac8dab9127cca6437511e4
Mike Patrick <mike.patrick2@digital.cabinet-office.gov.uk>
2025-02-25 13:35:41 +0000

    Only SELECT the reverse-linked columns we need
    Just like the changes we've just made to the database queries for
    linked-to-editions, this reduces the amount of data retrieved from the
    database when resolving reverse-linked editions in an attempt to speed
    up response times.
    
    Co-authored-by: Ynda Jas <yndajas@gmail.com>

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -20,11 +20,15 @@ module Types
     end
 
     def self.reverse_links_field(field_name, link_type, graphql_field_type)
-      field(field_name.to_sym, graphql_field_type)
+      field(field_name.to_sym, graphql_field_type, extras: [:lookahead])
 
-      define_method(field_name.to_sym) do
+      define_method(field_name.to_sym) do |lookahead:|
+        selections = GraphqlSelections.with_edition_fields(
+          lookahead.selections.map(&:name),
+        )
+
         dataloader.with(Sources::ReverseLinkedToEditionsSource, content_store: object.content_store)
-          .load([object, link_type.to_s])
+          .load([object, link_type.to_s, selections])
       end
     end
   end
