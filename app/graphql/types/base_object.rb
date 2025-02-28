860cc43ccc0d3973d435e39da57f4454da280730
Mike Patrick <mike.patrick2@digital.cabinet-office.gov.uk>
2025-02-28 14:02:47 +0000

    Introduce BaseObject#convert_edition_selections
    This converts the GraphQL query's edition selections to database column
    names. We include a list of valid database columns and a hash which maps
    GraphQL fields not in the list to the database columns they rely on

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -2,6 +2,44 @@
 
 module Types
   class BaseObject < GraphQL::Schema::Object
+    ALL_EDITION_COLUMNS = Set.new(%i[
+      analytics_identifier
+      auth_bypass_ids
+      base_path
+      content_store
+      created_at
+      description
+      details
+      document_id
+      document_type
+      first_published_at
+      id
+      last_edited_at
+      last_edited_by_editor_id
+      major_published_at
+      phase
+      public_updated_at
+      published_at
+      publishing_api_first_published_at
+      publishing_api_last_edited_at
+      publishing_app
+      publishing_request_id
+      redirects
+      rendering_app
+      routes
+      schema_name
+      state
+      title
+      update_type
+      updated_at
+      user_facing_version
+    ]).freeze
+
+    FIELDS_TO_EDITION_COLUMNS = {
+      links: %i[id content_store],
+      web_url: :base_path,
+    }.freeze
+
     edge_type_class(Types::BaseEdge)
     connection_type_class(Types::BaseConnection)
     field_class Types::BaseField
@@ -22,6 +60,14 @@ module Types
         dataloader.with(Sources::ReverseLinkedToEditionsSource, content_store: object.content_store)
           .load([object, belongs_to.to_s])
       end
+    end
+
+  private
+
+    def convert_edition_selections(lookahead:)
+      selections = lookahead.selections.map(&:name)
+      mapped_columns = selections.flat_map { FIELDS_TO_EDITION_COLUMNS[_1] }
+      ALL_EDITION_COLUMNS & (mapped_columns + selections)
     end
   end
 end
