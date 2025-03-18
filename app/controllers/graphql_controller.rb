10d9ecf5c1659ef8cd04156beecb23dac1ba1f77
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2025-03-18 11:48:27 +0000

    Do not authenticate GraphQL requests
    Publishing API is only available internally within our infrastructure
    and can only return  data we make available through that endpoint.
    
    Therefore removing the authentication of read requests for GraphQL only.
    This matches the same behaviour we have in Content Store for returning
    content items [1].
    
    With regards to performance of the read replica, authenticating every
    request will result in data being read/written to the primary
    (non-replica) database for each request (e.g. user permissions).
    
    1: https://github.com/alphagov/content-store/blob/2a77ee211152ff44b38613d6f61194ce93b01062/app/controllers/content_items_controller.rb#L2

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -6,6 +6,8 @@ class GraphqlController < ApplicationController
   # but you'll have to authenticate your user separately
   # protect_from_forgery with: :null_session
 
+  skip_before_action :authenticate_user!, only: [:execute]
+
   def execute
     variables = prepare_variables(params[:variables])
     query = params[:query]
