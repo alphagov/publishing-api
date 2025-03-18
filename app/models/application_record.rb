bdb982dcff3712b743b41726466f3b042c9d405d
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2025-03-18 08:31:30 +0000

    Add configuration for read replica
    The read replica is a separate deployment of this application, which
    connects to both the primary database (e.g. for writing user records
    after authentication) and the replica database (for all reads).
    
    Toggling of the application is done by changing the `RAILS_ENV`
    environment variable from `production` to `production_replica`.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  if Rails.env.production_replica?
+    connects_to database: {
+      writing: :primary,
+      reading: :primary_replica,
+    }
+  end
 end
