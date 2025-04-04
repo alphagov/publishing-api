734d6b9947e3f13ba4d15d1d81a2b4c036b12c72
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-04-04 16:35:44 +0100

    Only validate new migrations with strong_migrations
    All the old migrations have already been applied. We only need to check
    whether new migrations are dangerous.

--- config/initializers/strong_migrations.rb
+++ config/initializers/strong_migrations.rb
@@ -1,2 +1,5 @@
+# rubocop:disable Style/NumericLiterals
+StrongMigrations.start_after = 2025_03_26_15_54_06
+# rubocop:enable Style/NumericLiterals
 StrongMigrations.lock_timeout = 10.seconds
 StrongMigrations.statement_timeout = 1.hour
