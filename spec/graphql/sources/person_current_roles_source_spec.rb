e241b9df64d8d054453e9d89eea57f23f3fc1ef2
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2025-04-17 11:55:36 +0100

    Only return published role appointments on ministers index page
    We are currently returning unpublished role appointments attached to a
    published role on the ministers index page (when rendered using
    GraphQL), resulting in ministers appearing to have roles that they do
    not hold.
    
    For example, Angela Rayner has a current but unpublished role
    appointment which we are currently returning:
    
    ```
     #<Edition:0x0000ffff8f3c62e0
      id: 14474060,
      title: "The Rt Hon Angela Rayner MP - Secretary of State f...",
      public_updated_at: "2024-07-05 14:39:57.000000000 +0000",
      publishing_app: "whitehall",
      update_type: "major",
      phase: "live",
      analytics_identifier: nil,
      created_at: "2024-07-05 14:39:57.381491000 +0000",
      updated_at: "2024-07-10 12:33:27.619460000 +0000",
      document_type: "role_appointment",
      schema_name: "role_appointment",
      first_published_at: "2024-07-05 14:39:57.433063000 +0000",
      last_edited_at: "2024-07-05 14:39:57.386136000 +0000",
      state: "unpublished",
      user_facing_version: 1,
      base_path: nil,
      content_store: "live",
      document_id: 1602112,
      description: nil,
      publishing_request_id: "21-1720190397.253-10.13.22.63-2739",
      major_published_at: "2024-07-05 14:39:57.433063000 +0000",
      published_at: "2024-07-05 14:39:57.433063000 +0000",
      publishing_api_first_published_at: "2024-07-05 14:39:57.433063000 +0000",
      publishing_api_last_edited_at: "2024-07-05 14:39:57.386136000 +0000",
      auth_bypass_ids: [],
      details: {"current"=>true, "started_on"=>"2024-07-05T00:00:00+01:00", "person_appointment_order"=>1},
      routes: [],
      redirects: [],
      last_edited_by_editor_id: nil,
      rendering_app: nil>]
    ```
    
    Adding an extra constraint here to ensure we only retrieve the published
    role appointments.

--- spec/graphql/sources/person_current_roles_source_spec.rb
+++ spec/graphql/sources/person_current_roles_source_spec.rb
@@ -16,4 +16,36 @@ RSpec.describe Sources::PersonCurrentRolesSource do
       expect(request.load).to eq([role_1, role_3])
     end
   end
+
+  it "does not return draft but current roles for a person" do
+    person_1 = create_person("Person 1")
+    role_1 = create_role("Role 1")
+    role_2 = create_role("Role 2")
+    role_3 = create_role("Role 3")
+    appoint_person_to_role(person_1, role_1)
+    appoint_person_to_role(person_1, role_2, current: false)
+    appoint_person_to_role(person_1, role_3, state: "draft")
+
+    GraphQL::Dataloader.with_dataloading do |dataloader|
+      request = dataloader.with(described_class).request(person_1.content_id)
+
+      expect(request.load).to eq([role_1])
+    end
+  end
+
+  it "does not return unpublished but current roles for a person" do
+    person_1 = create_person("Person 1")
+    role_1 = create_role("Role 1")
+    role_2 = create_role("Role 2")
+    role_3 = create_role("Role 3")
+    appoint_person_to_role(person_1, role_1)
+    appoint_person_to_role(person_1, role_2, current: false)
+    appoint_person_to_role(person_1, role_3, state: "unpublished")
+
+    GraphQL::Dataloader.with_dataloading do |dataloader|
+      request = dataloader.with(described_class).request(person_1.content_id)
+
+      expect(request.load).to eq([role_1])
+    end
+  end
 end
