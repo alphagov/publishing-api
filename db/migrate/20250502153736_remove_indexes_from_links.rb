c92ef08bbd4798da497bebf6dc31cc05c503ca92
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-05-02 17:30:51 +0100

    Specify columns so that remove_index is reversible
    Note that we also need to specify algorithm: :concurrently otherwise if
    we _did_ need to roll this migration back it would add the indexes
    non-concurrently, which would lock the links table.

--- db/migrate/20250502153736_remove_indexes_from_links.rb
+++ db/migrate/20250502153736_remove_indexes_from_links.rb
@@ -1,7 +1,9 @@
 class RemoveIndexesFromLinks < ActiveRecord::Migration[8.0]
+  disable_ddl_transaction!
+
   def change
-    remove_index :links, name: "index_links_on_link_set_id"
-    remove_index :links, name: "index_links_on_link_set_id_and_target_content_id"
-    remove_index :links, name: "index_links_on_link_set_id_and_link_type"
+    remove_index :links, column: [:link_set_id], name: "index_links_on_link_set_id", algorithm: :concurrently
+    remove_index :links, column: %i[link_set_id target_content_id], name: "index_links_on_link_set_id_and_target_content_id", algorithm: :concurrently
+    remove_index :links, column: %i[link_set_id link_type], name: "index_links_on_link_set_id_and_link_type", algorithm: :concurrently
   end
 end
