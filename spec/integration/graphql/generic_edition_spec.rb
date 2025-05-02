cc2041cc337de7bb478197cbe3389f7d976a45a5
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2025-05-02 13:56:22 +0100

    Use `let` blocks instead of `before`
    This means we don't need to hardcode the base paths in the tests and
    instead use those created by the factory.

--- spec/integration/graphql/generic_edition_spec.rb
+++ spec/integration/graphql/generic_edition_spec.rb
@@ -1,12 +1,11 @@
 RSpec.describe "GraphQL" do
   describe "generic edition" do
-    before do
-      document = create(:document, content_id: "d53db33f-d4ac-4eb3-839a-d415174eb906")
-      @edition = create(
+    let(:document) { create(:document, content_id: "d53db33f-d4ac-4eb3-839a-d415174eb906") }
+    let(:edition) do
+      create(
         :live_edition,
         document:,
         document_type: "generic_type",
-        base_path: "/my/generic/edition",
         first_published_at: Time.utc(2025, 1, 1, 0, 0, 0),
         public_updated_at: Time.utc(2025, 4, 1, 0, 0, 0),
         updated_at: Time.utc(2025, 4, 1, 0, 0, 0),
@@ -17,7 +16,7 @@ RSpec.describe "GraphQL" do
       post "/graphql", params: {
         query:
           "{
-            edition(base_path: \"/my/generic/edition\") {
+            edition(base_path: \"#{edition.base_path}\") {
               ... on Edition {
                 title
                 analytics_identifier
@@ -51,25 +50,25 @@ RSpec.describe "GraphQL" do
       expected = {
         "data": {
           "edition": {
-            "analytics_identifier": @edition.analytics_identifier,
-            "base_path": @edition.base_path,
-            "content_id": @edition.content_id,
-            "description": @edition.description,
+            "analytics_identifier": edition.analytics_identifier,
+            "base_path": edition.base_path,
+            "content_id": edition.content_id,
+            "description": edition.description,
             "details": {
-              "body": @edition.details[:body],
+              "body": edition.details[:body],
             },
-            "document_type": @edition.document_type,
+            "document_type": edition.document_type,
             "first_published_at": "2025-01-01T00:00:00+00:00",
-            "locale": @edition.locale,
-            "phase": @edition.phase,
+            "locale": edition.locale,
+            "phase": edition.phase,
             "public_updated_at": "2025-04-01T01:00:00+01:00",
-            "publishing_app": @edition.publishing_app,
-            "publishing_request_id": @edition.publishing_request_id,
+            "publishing_app": edition.publishing_app,
+            "publishing_request_id": edition.publishing_request_id,
             "publishing_scheduled_at": nil,
-            "rendering_app": @edition.rendering_app,
+            "rendering_app": edition.rendering_app,
             "scheduled_publishing_delay_seconds": nil,
-            "schema_name": @edition.schema_name,
-            "title": @edition.title,
+            "schema_name": edition.schema_name,
+            "title": edition.title,
             "updated_at": "2025-04-01T01:00:00+01:00",
             "withdrawn_notice": nil,
           },
@@ -85,7 +84,7 @@ RSpec.describe "GraphQL" do
       post "/graphql", params: {
         query:
           "{
-            edition(base_path: \"/my/generic/edition\") {
+            edition(base_path: \"#{edition.base_path}\") {
               ... on Edition {
                 title
                 document_type
@@ -95,15 +94,14 @@ RSpec.describe "GraphQL" do
           }",
       }
 
-      expect(request.env["govuk.prometheus_labels"][:document_type]).to eq(@edition.document_type)
-      expect(request.env["govuk.prometheus_labels"][:schema_name]).to eq(@edition.schema_name)
+      expect(request.env["govuk.prometheus_labels"][:document_type]).to eq(edition.document_type)
+      expect(request.env["govuk.prometheus_labels"][:schema_name]).to eq(edition.schema_name)
     end
 
     context "when there is a withdrawn notice" do
-      before do
+      let(:edition) do
         create(
           :withdrawn_unpublished_edition,
-          base_path: "/my/withdrawn/edition",
           explanation: "for integration testing",
           document_type: "generic_type",
           unpublished_at: Time.utc(2024, 10, 28, 17, 0, 0),
@@ -114,7 +112,7 @@ RSpec.describe "GraphQL" do
         post "/graphql", params: {
           query:
             "{
-              edition(base_path: \"/my/withdrawn/edition\") {
+              edition(base_path: \"#{edition.base_path}\") {
                 ... on Edition {
                   withdrawn_notice {
                     explanation
