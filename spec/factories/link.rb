e158231e968ba14e6f9353aeeff76259415ba0c6
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-05-06 09:11:10 +0100

    ADR009 do not set link_set_id fkey
    We're using link_set_content_id everywhere in publishing-api's codebase
    now, so we no longer need to set link_set_id on newly created links.
    
    This will be a breaking change for govsearch, as it operates on a dump
    of publishing-api's database. We should make sure govsearch is updated
    to use link_set_content_id before deploying this.
    
    The link_set_id column will be removed entirely in a future commit.

--- spec/factories/link.rb
+++ spec/factories/link.rb
@@ -3,12 +3,5 @@ FactoryBot.define do
     link_set          { create(:link_set) unless edition }
     target_content_id { SecureRandom.uuid }
     link_type         { "organisations" }
-
-    # TODO: ADR-009 - remove when we remove link_set_id
-    after(:build) do |link,|
-      if link.link_set.present?
-        link.link_set_id = link.link_set.attributes["id"]
-      end
-    end
   end
 end
