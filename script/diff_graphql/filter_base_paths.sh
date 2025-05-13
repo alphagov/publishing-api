b55b6b6a94bd9158277d1bd5c951a76fd69febe3
Ynda Jas <yndajas@gmail.com>
2025-05-13 09:49:33 +0100

    Add script to filter base paths for GraphQL differ
    This will take a list of base paths, e.g. one downloaded from an Athena
    Trino SQL query, and filter them by one or more schema names, ready for
    use in the bulk diff script

---
+++ script/diff_graphql/filter_base_paths.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+# Filter a list of base paths by one or more given schema names.
+
+data_dir='tmp/diff_graphql'
+schema_names=()
+help_text="script/diff_graphql/bulk.sh
+
+DESCRIPTION
+     Filter a list of base paths by one or more given schema names.
+
+ARGUMENTS
+     --schema-names (required)
+             A list of schema names to filter by (e.g. role or news_article).
+
+     --data-dir
+             The directory in which you've stored the "unfiltered_base_paths"
+             file and where the "filtered_base_paths" file will be written. By
+             default this is tmp/diff_graphql"
+usage_text="
+usage: script/diff_graphql/filter_base_paths.sh [--data-dir path/to/data/dir]
+                                                --schema_names schema_name_1
+                                                [scema_name_2...]"
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    --data-dir) data_dir=$2; shift 2;;
+    --help) echo "$help_text"; exit;;
+    --schema-names)
+      shift
+
+      while [[ $# -gt 0 && $1 != --* ]]; do
+        schema_names+=($1)
+        shift
+      done
+
+      ;;
+    *) echo "$usage_text"; exit 1;;
+  esac
+done
+
+if [ ! -r $data_dir/unfiltered_base_paths ]; then
+  echo "Error: $data_dir/unfiltered_base_paths not found"
+  exit 1
+fi
+
+if [ ${#schema_names[@]} -eq 0 ]; then
+  echo "$usage_text"
+  exit 1
+fi
+
+sed -i -r -e '/^"url_path"$/d' -e 's/"//g' $data_dir/unfiltered_base_paths
+
+echo "Unfiltered base paths: $(grep -c '^' $data_dir/unfiltered_base_paths)"
+
+cp $data_dir/unfiltered_base_paths script/diff_graphql/unfiltered_base_paths
+
+govuk-docker-run bundle exec rails runner script/diff_graphql/filter_base_paths.rb ${schema_names[*]}
+
+mv script/diff_graphql/filtered_base_paths $data_dir/filtered_base_paths
+rm script/diff_graphql/unfiltered_base_paths
+
+echo -e "\nFiltered base paths written to $data_dir/filtered_base_paths"
