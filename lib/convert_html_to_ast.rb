dd4ad379ffa46d669e88e608bf4d89da15d5b299
Mike Patrick <mike.patrick2@digital.cabinet-office.gov.uk>
2025-05-22 14:00:23 +0100

    WIP Naively convert HTML content into a syntax tree

--- lib/convert_html_to_ast.rb
+++ lib/convert_html_to_ast.rb
@@ -1,5 +1,26 @@
 class ConvertHtmlToAst
   def self.call(html)
-    {}
+    html_node_to_ast_node(Nokogiri.parse(html).children.first)
+  end
+
+private
+
+  def self.html_node_to_ast_node(node)
+    result = {type: node.name}
+    attributes = {}
+
+    case node.name
+    when "text"
+      result[:content] = node.content
+    else
+      node.attributes.each do |_, attr|
+        attributes[attr.name] = attr.value
+      end
+      result[:attributes] = attributes unless attributes.empty?
+
+      result[:children] = node.children.map(&method(:html_node_to_ast_node))
+    end
+
+    result
   end
 end
