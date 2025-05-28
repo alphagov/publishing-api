3af85ef086ff8a342432c1ea8b370d77b58b9118
Mike Patrick <mike.patrick2@digital.cabinet-office.gov.uk>
2025-05-28 13:50:30 +0100

    Break up some long lines

--- spec/graphql/sources/linked_to_editions_source_spec.rb
+++ spec/graphql/sources/linked_to_editions_source_spec.rb
@@ -10,7 +10,10 @@ RSpec.describe Sources::LinkedToEditionsSource do
     create(:link, link_set: link_set, target_content_id: target_edition_3.content_id, link_type: "test_link")
 
     GraphQL::Dataloader.with_dataloading do |dataloader|
-      request = dataloader.with(described_class, content_store: source_edition.content_store).request([source_edition, "test_link"])
+      request = dataloader.with(
+        described_class,
+        content_store: source_edition.content_store,
+      ).request([source_edition, "test_link"])
 
       expect(request.load).to match_array([target_edition_1, target_edition_3])
     end
@@ -28,7 +31,10 @@ RSpec.describe Sources::LinkedToEditionsSource do
                             })
 
     GraphQL::Dataloader.with_dataloading do |dataloader|
-      request = dataloader.with(described_class, content_store: source_edition.content_store).request([source_edition, "test_link"])
+      request = dataloader.with(
+        described_class,
+        content_store: source_edition.content_store,
+      ).request([source_edition, "test_link"])
 
       expect(request.load).to match_array([target_edition_1, target_edition_3])
     end
@@ -49,7 +55,10 @@ RSpec.describe Sources::LinkedToEditionsSource do
     create(:link, link_set: link_set, target_content_id: target_edition_3.content_id, link_type: "test_link")
 
     GraphQL::Dataloader.with_dataloading do |dataloader|
-      request = dataloader.with(described_class, content_store: source_edition.content_store).request([source_edition, "test_link"])
+      request = dataloader.with(
+        described_class,
+        content_store: source_edition.content_store,
+      ).request([source_edition, "test_link"])
 
       expect(request.load).to match_array([target_edition_1, target_edition_3])
     end
@@ -72,7 +81,10 @@ RSpec.describe Sources::LinkedToEditionsSource do
     create(:link, link_set:, target_content_id: target_edition_4.content_id, link_type: "test_link")
 
     GraphQL::Dataloader.with_dataloading do |dataloader|
-      request = dataloader.with(described_class, content_store: source_edition.content_store).request([source_edition, "test_link"])
+      request = dataloader.with(
+        described_class,
+        content_store: source_edition.content_store,
+      ).request([source_edition, "test_link"])
 
       expect(request.load).to match_array([target_edition_3, target_edition_4])
     end
@@ -125,7 +137,10 @@ RSpec.describe Sources::LinkedToEditionsSource do
         create(:link, link_set:, target_content_id: withdrawn_link_set_linked_edition.content_id, link_type:)
 
         GraphQL::Dataloader.with_dataloading do |dataloader|
-          request = dataloader.with(described_class, content_store: source_edition.content_store).request([source_edition, link_type])
+          request = dataloader.with(
+            described_class,
+            content_store: source_edition.content_store,
+          ).request([source_edition, link_type])
 
           expect(request.load).to match_array([edition_linked_edition, withdrawn_edition_linked_edition, link_set_linked_edition, withdrawn_link_set_linked_edition])
         end
@@ -149,7 +164,10 @@ RSpec.describe Sources::LinkedToEditionsSource do
       create(:link, link_set:, target_content_id: withdrawn_link_set_linked_edition.content_id, link_type: "test_link")
 
       GraphQL::Dataloader.with_dataloading do |dataloader|
-        request = dataloader.with(described_class, content_store: source_edition.content_store).request([source_edition, "test_link"])
+        request = dataloader.with(
+          described_class,
+          content_store: source_edition.content_store,
+        ).request([source_edition, "test_link"])
 
         expect(request.load).to match_array([edition_linked_edition, link_set_linked_edition])
       end
