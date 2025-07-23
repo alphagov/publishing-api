e25e7ea5c50597743127b8b45878b9cdf8b3159a
Bruce Bolt <bruce.bolt@dsit.gov.uk>
2025-07-23 08:05:03 +0100

    Update GraphQL content endpoint to behave more like Content Store

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,8 @@ Rails.application.routes.draw do
 
     post "/lookup-by-base-path", to: "lookups#by_base_path"
 
+    get "/graphql/content/*path_without_root" => "graphql#content"
     post "/graphql", to: "graphql#execute"
-    get "/graphqlcontent/*path_without_root" => "graphql#content"
 
     namespace :v2 do
       get "/content", to: "content_items#index"
