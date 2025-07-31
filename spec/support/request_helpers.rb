ee8f39065bf940e1a6e8468d70f7be92651b9b3e
Bruce Bolt <bruce.bolt@dsit.gov.uk>
2025-07-31 10:25:21 +0100

    Add `rack-cache` gem
    This will be used to replicate the responses currently provided by
    Content Store, when we create a Content Store-like endpoint powered by
    GraphQL in later commits.

--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -1,4 +1,8 @@
 module RequestHelpers
+  def cache_control
+    Rack::Cache::CacheControl.new(response["Cache-Control"])
+  end
+
   # Use in request and controller specs to access the response.
   def parsed_response
     JSON.parse(response.body)
