ee70469d1bd7fe8230cb2a682db56c4cb50e0be8
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-09-12 16:20:19 +0100

    WIP compact details and empty links

--- lib/graphql/content_item_compactor.rb
+++ lib/graphql/content_item_compactor.rb
@@ -3,12 +3,40 @@ class Graphql::ContentItemCompactor
     @schema = schema
   end
 
-  # TODO - details
-  # TODO - links with empty arrays
-
   def compact(graphql_response)
+    details = graphql_response["details"]
+    if details
+      details_required_fields = @schema.dig("definitions", "details", "required") || []
+      graphql_response["details"] = compact_by_schema(
+        details,
+        details_required_fields,
+        "#/properties/details/properties"
+      )
+    end
     required_fields = @schema.fetch("required", [])
-    graphql_response.filter do |key, value|
+    result = compact_by_schema(graphql_response, required_fields, "#/properties")
+    compact_empty_links!(result)
+    result
+  end
+
+private
+
+  def compact_empty_links!(item)
+    links = item["links"]
+    if links.present?
+      item["links"] = links.reject { |_key, linked_items| linked_items == [] }
+
+      # Recurse through each of the linked items, compacting their links too
+      item["links"].each do |_key, linked_items|
+        linked_items.each do |linked_item|
+          compact_empty_links!(linked_item)
+        end
+      end
+    end
+  end
+
+  def compact_by_schema(hash, required_fields, fragment)
+    hash.filter do |key, value|
       # Include all fields with non-nil values
       next true unless value.nil?
 
@@ -16,7 +44,7 @@ class Graphql::ContentItemCompactor
         # TODO check the performance cost of doing this for every required nil field
         begin
           # Check if nil is a valid value for this property
-          JSON::Validator.validate!(@schema, nil, fragment: "#/properties/#{key}")
+          JSON::Validator.validate!(@schema, nil, fragment: "#{fragment}/#{key}")
         rescue JSON::Schema::ValidationError
           Rails.logger.warn("field #{key} is required, and null is not an valid value, but null is all we have")
         end
