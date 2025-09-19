98d7be5edc47c7ab7e2cf21755e81a079bf11e70
Mike Patrick <mike.patrick2@digital.cabinet-office.gov.uk>
2025-09-19 16:58:46 +0100

    Add missing test coverage to dataloaders
    Co-authored-by: Ynda Jas <yndajas@gmail.com>

--- spec/graphql/sources/person_current_roles_source_spec.rb
+++ spec/graphql/sources/person_current_roles_source_spec.rb
@@ -17,6 +17,49 @@ RSpec.describe Sources::PersonCurrentRolesSource do
     end
   end
 
+  it "returns roles ordered by the `position` of the role_appointment->role links" do
+    person = create_person("Person")
+    role_0 = create_role("Role 0")
+    role_1 = create_role("Role 1")
+    role_2 = create_role("Role 2")
+
+    # appoint person to role
+    role_appointment = create(
+      :live_edition,
+      document_type: "role_appointment",
+      details: { current: true },
+    )
+    link_set = create(:link_set, content_id: role_appointment.content_id)
+    create(:link, link_type: "person", target_content_id: person.content_id, link_set:)
+    create(:link, position: 1, link_type: "role", target_content_id: role_1.content_id, link_set:)
+
+    # appoint person to role
+    role_appointment = create(
+      :live_edition,
+      document_type: "role_appointment",
+      details: { current: true },
+    )
+    link_set = create(:link_set, content_id: role_appointment.content_id)
+    create(:link, link_type: "person", target_content_id: person.content_id, link_set:)
+    create(:link, position: 0, link_type: "role", target_content_id: role_0.content_id, link_set:)
+
+    # appoint person to role
+    role_appointment = create(
+      :live_edition,
+      document_type: "role_appointment",
+      details: { current: true },
+    )
+    link_set = create(:link_set, content_id: role_appointment.content_id)
+    create(:link, link_type: "person", target_content_id: person.content_id, link_set:)
+    create(:link, position: 2, link_type: "role", target_content_id: role_2.content_id, link_set:)
+
+    GraphQL::Dataloader.with_dataloading do |dataloader|
+      request = dataloader.with(described_class).request(person.content_id)
+
+      expect(request.load).to eq([role_0, role_1, role_2])
+    end
+  end
+
   it "does not return draft roles for a person" do
     person_1 = create_person("Person 1")
     published_role = create_role("Role")
