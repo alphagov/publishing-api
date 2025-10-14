b38c9fcfab4f9a216b923bd936314831f0442184
Bruce Bolt <bruce.bolt@dsit.gov.uk>
2025-10-14 10:26:49 +0100

    Add missing test case
    There is a possibility of `include_edition_change_history` being false
    and no embed links existing.
    
    Therefore adding a test case to cover this scenario and wrapping the
    case of `include_edition_change_history` being true in a context block.

--- spec/presenters/queries/change_history_spec.rb
+++ spec/presenters/queries/change_history_spec.rb
@@ -5,28 +5,39 @@ RSpec.describe Presenters::Queries::ChangeHistory do
     create(:edition, document:, state: "published", user_facing_version: "2")
   end
 
-  let(:include_edition_change_history) { true }
   let(:subject) { described_class.new(edition, include_edition_change_history:).call }
 
   describe "when there are no embed links" do
-    it "constructs content history from change notes" do
-      2.times do |i|
-        create(:change_note, edition:, note: i.to_s, public_timestamp: Time.zone.now.utc)
+    context "when include_edition_change_history is true" do
+      let(:include_edition_change_history) { true }
+
+      it "constructs content history from change notes" do
+        2.times do |i|
+          create(:change_note, edition:, note: i.to_s, public_timestamp: Time.zone.now.utc)
+        end
+        expect(subject.map(&:note)).to eq %w[0 1]
       end
-      expect(subject.map(&:note)).to eq %w[0 1]
+
+      it "orders change notes by public_timestamp (ascending)" do
+        [1, 3, 2].to_a.each do |i|
+          create(:change_note, edition:, note: i.to_s, public_timestamp: i.days.ago)
+        end
+        expect(subject.map(&:note)).to eq %w[3 2 1]
+      end
+
+      it "omits change notes that don't have a public timestamp" do
+        create(:change_note, edition:, note: "with-timestamp", public_timestamp: 1.day.ago)
+        create(:change_note, edition:, note: "without-timestamp", public_timestamp: nil)
+        expect(subject.map(&:note)).to eq %w[with-timestamp]
+      end
     end
 
-    it "orders change notes by public_timestamp (ascending)" do
-      [1, 3, 2].to_a.each do |i|
-        create(:change_note, edition:, note: i.to_s, public_timestamp: i.days.ago)
-      end
-      expect(subject.map(&:note)).to eq %w[3 2 1]
-    end
+    context "when include_edition_change_history is false" do
+      let(:include_edition_change_history) { false }
 
-    it "omits change notes that don't have a public timestamp" do
-      create(:change_note, edition:, note: "with-timestamp", public_timestamp: 1.day.ago)
-      create(:change_note, edition:, note: "without-timestamp", public_timestamp: nil)
-      expect(subject.map(&:note)).to eq %w[with-timestamp]
+      it "should return no change notes" do
+        expect(subject.map(&:note)).to eq []
+      end
     end
   end
 
@@ -64,9 +75,12 @@ RSpec.describe Presenters::Queries::ChangeHistory do
       create(:change_note, edition: older_edition, note: "note-2", public_timestamp: 3.days.ago)
       create(:change_note, edition:, note: "note-1", public_timestamp: 1.hour.ago)
     end
+    context "when include_edition_change_history is true" do
+      let(:include_edition_change_history) { true }
 
-    it "should include change notes that were created after the link was created" do
-      expect(subject.map(&:note)).to eq %w[note-2 linked-edition-note-1 linked-edition-note-2 note-1]
+      it "should include change notes that were created after the link was created" do
+        expect(subject.map(&:note)).to eq %w[note-2 linked-edition-note-1 linked-edition-note-2 note-1]
+      end
     end
 
     context "when include_edition_change_history is false" do
