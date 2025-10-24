930c8e2b5fee08d5620efaf3879389d535eb2db2
Ynda Jas <yndajas@gmail.com>
2025-10-24 13:18:51 +0100

    Update GraphQL diff exclusions
    The aria-labelledby etc changes both simplify the sed expression and
    account for instances where there are two IDs in the string, which
    appear once in some (if not all) transactions

--- script/live_content/diff_frontend_functions.sh
+++ script/live_content/diff_frontend_functions.sh
@@ -31,9 +31,10 @@ function curl_and_strip_hashes() {
   echo "$response" | sed -r \
     -e 's/\?graphql=(true|false)//g' \
     -e 's/nonce="[^"]{22}=="/nonce="HASH=="/g' \
-    -e 's/ (aria-labelledby|data-controls|for|id)="([^"]+)-[a-z0-9]{8}"/ \1="\2-HASH"/g' \
+    -e 's/ (aria-labelledby="[^ ]+-)[a-z0-9]{8}( [^"]+-)[a-z0-9]{8}"/ \1HASH\2HASH"/g' \
+    -e 's/ (aria-labelledby|data-controls|for|id)(="[^"]+-)[a-z0-9]{8}"/ \1\2HASH"/g' \
     -e 's/<(meta name="govuk:updated-at" content=)"[^"]+">/<\1"TIMESTAMP">/' \
-    -e '/<meta name="govuk:content-has-history" content=".*">/d' \
+    -e '/<meta name="(csrf-token|csp-nonce|govuk:content-has-history)" content=".*"( \/)?>/d' \
     -e 's/(This news article was withdrawn on &lt;time datetime=)"[^"]+"/\1"TIMESTAMP"/' \
     -e 's/(This news article was withdrawn on <time datetime=)"[^"]+"/\1"TIMESTAMP"/' \
     > "$output_path"
