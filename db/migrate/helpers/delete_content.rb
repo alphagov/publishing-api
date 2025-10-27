114615f019170c9f38f237c438ee3fb6d5f456a5
pezholio <pezholio@gmail.com>
2025-10-27 12:26:50 +0000

    Ensure ContentIdAliases for a document are deleted
    When deleting a document, we need to ensure all `ContentIdAlias`
    objects are deleted too.

--- db/migrate/helpers/delete_content.rb
+++ db/migrate/helpers/delete_content.rb
@@ -5,11 +5,16 @@ module Helpers
 
       Document.where(content_id: content_ids).find_each do |document|
         destroy_edition_supporting_objects(document.editions)
+        destroy_content_id_aliases_for_document(document)
         document.editions.destroy_all
         document.destroy!
       end
 
       destroy_links(content_ids)
+    end
+
+    def self.destroy_content_id_aliases_for_document(document)
+      ContentIdAlias.where(content_id: document.content_id).destroy_all
     end
 
     def self.destroy_edition_supporting_objects(editions)
