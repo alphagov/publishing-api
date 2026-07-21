e48621565d86e35a676d28560a7c1f6d145cacf8
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2026-07-21 13:43:09 +0100

    Update PutContentValidator with gem base_path validator
    - Adds the GdsApi::Validators::BasePathValidator to check base_path and reject.
    - Content types that do not have a base path are excluded from the check.
    - Redirects are excluded from the check, because they're allowed to have invalid
      characters per RFC-192.

--- spec/commands/v2/put_content_validator_spec.rb
+++ spec/commands/v2/put_content_validator_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe Commands::V2::PutContentValidator do
-  let(:payload) { { publishing_app: "foo" } }
+  let(:payload) { { publishing_app: "foo", base_path: "/hello" } }
   let(:command) { instance_double(Commands::V2::PutContent) }
   subject { described_class.new(payload, command) }
 
@@ -41,6 +41,24 @@ RSpec.describe Commands::V2::PutContentValidator do
           expect {
             subject.validate
           }.to raise_error(CommandError, /publishing_app is required/)
+        end
+      end
+
+      context "with an invalid base_path" do
+        let(:payload) { { publishing_app: "foo", base_path: "/Hello", schema_name: "guide" } }
+
+        it "raises an error" do
+          expect {
+            subject.validate
+          }.to raise_error(CommandError, /base_path did not conform to standard/)
+        end
+
+        context "when the content item is a redirect" do
+          let(:payload) { { publishing_app: "foo", base_path: "/Hello", schema_name: "redirect" } }
+
+          it "doesn't raise anything" do
+            expect { subject.validate }.not_to raise_error
+          end
         end
       end
     end
